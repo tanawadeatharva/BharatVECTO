2ddda12d7b52a85aceb7c108bd5f0ac80873929c
Dana Nurse <Dana.Nurse@ricardo-aea.com>
2014-09-12 10:04:29 +0000

    Added HnacMap tests
    git-tfs-id: [http://tfs00.element.root.com:8080/tfs/TFSCollection]$/VECTO;C1600

--- VECTOAux/VectoAuxiliaries/Hvac/HVACMap.vb
+++ VECTOAux/VectoAuxiliaries/Hvac/HVACMap.vb
@@ -1,3 +1,5 @@
+Imports System.IO
+
 Namespace Hvac
     Public Class HVACMap
         'Some sort of multi-dimensional map implemented here
@@ -15,23 +17,81 @@ Namespace Hvac
 
         'could define the list of inputs based on the supplied map
 
+        ''' <summary>
+        ''' Path to HVAC map csv
+        ''' </summary>
+        ''' <remarks></remarks>
+        Private ReadOnly filePath As String
+
+        ''' <summary>
+        ''' Dictionary of values keyed by input value combinations
+        ''' </summary>
+        ''' <remarks></remarks>
         Private map As Dictionary(Of InputValues, OutputValues)
 
-        Public Sub New()
-            map = New Dictionary(Of InputValues, OutputValues)()
-
-            For i As Integer = 25 To 50
-                For j As Integer = 25 To 50
-                    Dim input As New InputValues(i, j)
-                    Dim output As New OutputValues(i * i * j, i * i * j * 5)
-                    map.Add(input, output)
-                Next
-            Next
+        ''' <summary>
+        ''' Creates a new instance of and HVACMap class
+        ''' </summary>
+        ''' <remarks></remarks>
+        Public Sub New(ByVal path As String)
+            filePath = path
         End Sub
 
+        Public Function Initialise() As Boolean
+            If (File.Exists(filePath)) Then
+                Using sr As StreamReader = New StreamReader(filePath)
+                    'get array of lines from csv
+                    Dim lines() As String = sr.ReadToEnd().Split(CType(Environment.NewLine, Char()), StringSplitOptions.RemoveEmptyEntries)
+
+                    map = New Dictionary(Of InputValues, OutputValues)()
+                    Dim firstline As Boolean = True
+
+                    For Each line As String In lines
+                        If Not firstline Then
+                            'split the line
+                            Dim elements() As String = line.Split(New Char() {","}, StringSplitOptions.RemoveEmptyEntries)
+                            '4 entries per line required
+                            If (elements.Length <> 4) Then Throw New ArgumentException("Incorrect number of values in csv file")
+                            'add values to map
+                            Dim region As String = elements(0)
+                            Dim season As String = elements(1)
+                            Dim input As New InputValues(region, season)
+                            Dim electricalDemand As String = elements(2)
+                            Dim mechanicalDemand As String = elements(3)
+                            Dim output As New OutputValues(electricalDemand, mechanicalDemand)
+                            map.Add(input, output)
+                        Else
+                            firstline = False
+                        End If
+                    Next
+                End Using
+                Return True
+            Else
+                Throw New ArgumentException("supplied input file does not exist")
+            End If
+        End Function
+
+
+        Public Function GetMechanicalDemand(ByVal region As Integer, ByVal season As Integer) As Integer
+            Dim key As InputValues = New InputValues(region, season)
+            Dim val As OutputValues = map(key)
+            Return val.MechanicalDemand
+        End Function
+
+        Public Function GetElectricalDemand(ByVal region As Integer, ByVal season As Integer) As Integer
+            Dim key As InputValues = New InputValues(region, season)
+            If (map.ContainsKey(key)) Then
+                Dim val As OutputValues = map(key)
+                Return val.ElectricalDemand
+            Else
+                Throw New ArgumentException("Key was not present")
+            End If
+        End Function
+
+#Region "Nested Structures"
         Private Structure InputValues
-            Public ReadOnly Region As Integer
-            Public ReadOnly Season As Integer
+            Private ReadOnly region As Integer
+            Private ReadOnly season As Integer
 
             Public Sub New(ByVal region As Integer, ByVal season As Integer)
                 Me.Region = region
@@ -40,27 +100,15 @@ Namespace Hvac
         End Structure
 
         Private Structure OutputValues
-            Public ReadOnly MechanicalDemand As Integer
-            Public ReadOnly ElectricalDemand As Integer
+            Public ReadOnly MechanicalDemand As Single
+            Public ReadOnly ElectricalDemand As Single
 
-            Public Sub New(ByVal mechanicalDemand As Integer, ByVal electricalDemand As Integer)
+            Public Sub New(ByVal electricalDemand As Single, ByVal mechanicalDemand As Single)
                 Me.MechanicalDemand = mechanicalDemand
                 Me.ElectricalDemand = electricalDemand
             End Sub
         End Structure
+#End Region
 
-        Public Function GetMechanicalDemand(ByVal region As Integer, ByVal season As Integer) As Integer
-            Dim key As InputValues = New InputValues(region, season)
-            Dim val As OutputValues = map(key)
-            Return val.MechanicalDemand
-        End Function
-
-
-
-        Public Function GetElectricalDemand(ByVal region As Integer, ByVal season As Integer) As Integer
-            Dim key As InputValues = New InputValues(region, season)
-            Dim val As OutputValues = map(key)
-            Return val.ElectricalDemand
-        End Function
     End Class
 End Namespace
---
+++ VECTOAux/VectoAuxiliariesTests/UnitTests/HVACMapTests.vb
@@ -0,0 +1,95 @@
+﻿Imports NUnit.Framework
+Imports VectoAuxiliaries.Hvac
+
+
+Namespace UnitTests
+
+    <TestFixture()> Public Class HVACMapTests
+#Region "Test Values"
+        Dim goodMap As String = "TestFiles\TestHvacMap.csv"
+        Const badMapWrongColumns As String = "TestFiles\testHvacMapWrongColumns.csv"
+        Const badMapInvalidRows As String = "TestFiles\testHvacMapInvalidRowData.csv"
+        Const badMapFileNotExist As String = "TestFiles\fileNotExist.csv"
+
+        Private Function GetGoodMap() As HVACMap
+            Return New HVACMap(goodMap)
+        End Function
+
+        Private Function GetInitialisedMap() As HVACMap
+            Dim map As HVACMap = GetGoodMap()
+            map.Initialise()
+            Return map
+        End Function
+
+#End Region
+
+        <Test()>
+        Public Sub CreateNewTest()
+            Dim target As HVACMap = GetGoodMap()
+            Assert.IsNotNull(target)
+        End Sub
+
+        <Test()>
+        Public Sub InitialiseTest()
+            Dim target As HVACMap = GetGoodMap()
+            Assert.IsTrue(target.Initialise())
+        End Sub
+
+        <TestCase(badMapWrongColumns)>
+        <ExpectedException("System.ArgumentException")>
+        Public Sub InitialiseInvalidColumnCountTest(ByVal path As String)
+            Dim target As HVACMap = New HVACMap(path)
+            Assert.IsTrue(target.Initialise())
+        End Sub
+
+        <TestCase(badMapInvalidRows)>
+        <ExpectedException("System.InvalidCastException")>
+        Public Sub InitialiseInvalidRowDataTest(ByVal path As String)
+            Dim target As HVACMap = New HVACMap(path)
+            Assert.IsTrue(target.Initialise())
+        End Sub
+
+        <TestCase(badMapFileNotExist)>
+        <ExpectedException("System.ArgumentException")>
+        Public Sub InitialiseFileNotExistTest(ByVal path As String)
+            Dim target As HVACMap = New HVACMap(path)
+            Assert.IsTrue(target.Initialise())
+        End Sub
+
+
+        <TestCase(3, 1, 24)>
+        <TestCase(3, 2, 192)>
+        <TestCase(3, 3, 24)>
+        <TestCase(3, 4, 192)>
+        Public Sub GetMechanicalDemandTest(ByVal region As Integer, ByVal season As Integer, result As Integer)
+            Dim target As HVACMap = GetInitialisedMap()
+            Dim expected As Integer = result
+            Dim actual As Integer = target.GetMechanicalDemand(region, season)
+            Assert.AreEqual(expected, actual)
+        End Sub
+
+        <TestCase(3, 1, 12)>
+        <TestCase(3, 2, 48)>
+        <TestCase(3, 3, 12)>
+        <TestCase(3, 4, 48)>
+        Public Sub GetElectricalDemandTest(ByVal region As Integer, ByVal season As Integer, result As Integer)
+            Dim target As HVACMap = GetInitialisedMap()
+            Dim expected As Integer = result
+            Dim actual As Integer = target.GetElectricalDemand(region, season)
+            Assert.AreEqual(expected, actual)
+        End Sub
+
+        <TestCase(3, 6)> _
+        <TestCase(6, 3)> _
+        <ExpectedException("System.ArgumentException")>
+        Public Sub GetElectricalDemandKeyNotPresentTest(ByVal region As Integer, ByVal season As Integer)
+            Dim target As HVACMap = GetInitialisedMap()
+            Dim actual As Integer = target.GetElectricalDemand(region, season)
+        End Sub
+
+    End Class
+
+
+End Namespace
+
+
