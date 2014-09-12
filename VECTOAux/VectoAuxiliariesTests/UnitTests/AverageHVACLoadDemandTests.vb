adf40afb32bd15663b394002ab85d4e641767158
Dana Nurse <Dana.Nurse@ricardo-aea.com>
2014-09-12 13:25:48 +0000

    git-tfs-id: [http://tfs00.element.root.com:8080/tfs/TFSCollection]$/VECTO;C1602

---
+++ VECTOAux/VectoAuxiliariesTests/UnitTests/AverageHVACLoadDemandTests.vb
@@ -0,0 +1,30 @@
+﻿Imports NUnit.Framework
+
+Namespace UnitTests
+    <TestFixture()> Public Class AverageHVACLoadDemandTests
+
+        <Test()> Public Sub NewTest()
+            Assert.Fail()
+        End Sub
+
+        <Test()> Public Sub InitialiseTest()
+            Assert.Fail()
+        End Sub
+
+        <Test()> Public Sub AverageMechanicalPowerAtCrankTest()
+            Assert.Fail()
+        End Sub
+
+        <Test()> Public Sub AverageElectricalPowerAtAlternatorTest()
+            Assert.Fail()
+        End Sub
+
+        <Test()> Public Sub AverageElectricalPowerAtCrankTest()
+            Assert.Fail()
+        End Sub
+    End Class
+
+
+End Namespace
+
+
