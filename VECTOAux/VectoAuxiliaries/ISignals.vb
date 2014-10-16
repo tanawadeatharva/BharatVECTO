8e7c2014f1e3546b1aeef7412475c7b2d2d8ae24
Burns, Terry <Terry.Burns@ricardo.com>
2014-10-16 15:15:03 +0000

    Pre-SignalsRefactoring
    git-tfs-id: [http://tfs00.element.root.com:8080/tfs/TFSCollection]$/VECTO;C1640

---
+++ VECTOAux/VectoAuxiliaries/ISignals.vb
@@ -0,0 +1,17 @@
+﻿Public Interface ISignals
+
+
+
+
+Property EngineMotoringPower As single
+property EngineDrivelinePower as single
+property SmartElectrics As Boolean
+Property ClutchEngaged As Boolean
+Property EngineSpeed as integer
+Property SmartPneumatics As Boolean
+Property TotalCycleTimeSeconds As Integer
+property EngineDrivelineTorque as single
+
+
+
+End Interface
---
+++ VECTOAux/VectoAuxiliaries/Signals.vb
@@ -0,0 +1,24 @@
+﻿
+Public Class Signals
+Implements ISignals
+
+    Public Property ClutchEngaged As Boolean Implements ISignals.ClutchEngaged
+
+    Public Property EngineDrivelinePower As Single Implements ISignals.EngineDrivelinePower
+
+    Public Property EngineDrivelineTorque As Single Implements ISignals.EngineDrivelineTorque
+
+    Public Property EngineMotoringPower As Single Implements ISignals.EngineMotoringPower
+
+    Public Property EngineSpeed As Integer Implements ISignals.EngineSpeed
+
+    Public Property SmartElectrics As Boolean Implements ISignals.SmartElectrics
+
+    Public Property SmartPneumatics As Boolean Implements ISignals.SmartPneumatics
+
+    Public Property TotalCycleTimeSeconds As Integer Implements ISignals.TotalCycleTimeSeconds
+
+End Class
+
+
+
