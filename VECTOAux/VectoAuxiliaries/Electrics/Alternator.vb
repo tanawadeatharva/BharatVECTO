09e5b18b1e9e470251ce16d3fc0637eec01fd53c
Dana Nurse <Dana.Nurse@ricardo-aea.com>
2015-04-29 08:36:29 +0000

    Another change to Combined Alternators Schematic Update to Row4 Amps calculation
    git-tfs-id: [http://tfs00.element.root.com:8080/tfs/TFSCollection]$/VECTO;C1910

--- VECTOAux/VectoAuxiliaries/Electrics/Alternator.vb
+++ VECTOAux/VectoAuxiliaries/Electrics/Alternator.vb
@@ -4,314 +4,318 @@
 Namespace Electrics
 
 
-Public Class Table4Row
+    Public Class Table4Row
 
-  Public RPM As Single
-  Public Efficiency As Single
+        Public RPM As Single
+        Public Efficiency As Single
 
-  Public Sub new ( rpm As single, eff  As single)
+        Public Sub New(rpm As Single, eff As Single)
 
-   Me.rpm= rpm
-   Me.Efficiency = eff
-
-
-  End Sub
+            Me.rpm = rpm
+            Me.Efficiency = eff
 
 
-End Class
+        End Sub
 
 
-Public Class Alternator
- Implements IAlternator
+    End Class
+
+
+    Public Class Alternator
+        Implements IAlternator
 
 
 
- Public Property AlternatorName As String Implements IAlternator.AlternatorName
- Public Property  PulleyRatio As Single Implements IAlternator.PulleyRatio
+        Public Property AlternatorName As String Implements IAlternator.AlternatorName
+        Public Property PulleyRatio As Single Implements IAlternator.PulleyRatio
 
- Public Property InputTable2000 As  New List(Of AltUserInput) Implements IAlternator.InputTable2000  
- Public Property InputTable4000 As  New List(Of AltUserInput) Implements IAlternator.InputTable4000
- Public Property InputTable6000 As  New List(Of AltUserInput) Implements IAlternator.InputTable6000
- Public Property RangeTable     As  New List(Of Table4Row)    Implements IAlternator.RangeTable
+        Public Property InputTable2000 As New List(Of AltUserInput) Implements IAlternator.InputTable2000
+        Public Property InputTable4000 As New List(Of AltUserInput) Implements IAlternator.InputTable4000
+        Public Property InputTable6000 As New List(Of AltUserInput) Implements IAlternator.InputTable6000
+        Public Property RangeTable As New List(Of Table4Row) Implements IAlternator.RangeTable
 
- Private signals As ICombinedAlternatorSignals 
-
-
- Public  Sub  Clone( other As IAlternator) Implements IAlternator.Clone
+        Private signals As ICombinedAlternatorSignals
 
 
-   
- End Sub
- Public ReadOnly Property Efficiency As Double Implements IAlternator.Efficiency
+        Public Sub Clone(other As IAlternator) Implements IAlternator.Clone
+
+
+
+        End Sub
+        Public ReadOnly Property Efficiency As Double Implements IAlternator.Efficiency
 
 
             Get
-               'First build RangeTable, table 4
+                'First build RangeTable, table 4
 
-               InitialiseRangeTable()
-               CalculateRangeTable()
+                InitialiseRangeTable()
+                CalculateRangeTable()
 
-               'Calculate ( Interpolate ) Efficiency
+                'Calculate ( Interpolate ) Efficiency
 
-               Dim range as List(Of AltUserInput) = RangeTable.Select( Function(s) New AltUserInput(s.RPM,s.Efficiency)).ToList()
+                Dim range As List(Of AltUserInput) = RangeTable.Select(Function(s) New AltUserInput(s.RPM, s.Efficiency)).ToList()
 
-               Dim v As Single =  Alternator.Iterpolate( range,Convert.ToSingle( SpindleSpeed))
+                Dim v As Single = Alternator.Iterpolate(range, Convert.ToSingle(SpindleSpeed))
 
-               Return v
+                Return v
 
             End Get
 
 
 
         End Property
- Public ReadOnly Property SpindleSpeed As Double Implements IAlternator.SpindleSpeed
+        Public ReadOnly Property SpindleSpeed As Double Implements IAlternator.SpindleSpeed
             Get
-               Return signals.CrankRPM * PulleyRatio
+                Return signals.CrankRPM * PulleyRatio
             End Get
         End Property
 
-         
- 'Constructors
- Sub new()
-   
- End Sub
- Sub new( isignals As ICombinedAlternatorSignals, inputs as List(Of ICombinedAlternatorMapRow))
+
+        'Constructors
+        Sub New()
+
+        End Sub
+        Sub New(isignals As ICombinedAlternatorSignals, inputs As List(Of ICombinedAlternatorMapRow))
 
 
-     If isignals is Nothing then Throw New ArgumentException("Alternator - ISignals supplied is nothing")
-     signals = isignals
+            If isignals Is Nothing Then Throw New ArgumentException("Alternator - ISignals supplied is nothing")
+            signals = isignals
 
-     Me.AlternatorName= inputs.First().AlternatorName
-     Me.PulleyRatio = inputs.First().PulleyRatio
+            Me.AlternatorName = inputs.First().AlternatorName
+            Me.PulleyRatio = inputs.First().PulleyRatio
 
-     Dim values2k As  Dictionary(Of single,single) =  inputs.where( function(x) x.RPM=2000).Select( function(x) new KeyValuePair(of single,single)(x.Amps,x.Efficiency)).ToDictionary( Function(x) x.Key, Function(x) x.Value)
-     Dim values4k As  Dictionary(Of single,single) =  inputs.where( function(x) x.RPM=4000).Select( function(x) new KeyValuePair(of single,single)(x.Amps,x.Efficiency)).ToDictionary( Function(x) x.Key, Function(x) x.Value)
-     Dim values6k As  Dictionary(Of single,single) =  inputs.where( function(x) x.RPM=6000).Select( function(x) new KeyValuePair(of single,single)(x.Amps,x.Efficiency)).ToDictionary( Function(x) x.Key, Function(x) x.Value)
-
-    
-     BuildInputTable( values2k, InputTable2000)
-     BuildInputTable( values4k, InputTable4000)
-     BuildInputTable( values6k, InputTable6000)
+            Dim values2k As Dictionary(Of Single, Single) = inputs.where(Function(x) x.RPM = 2000).Select(Function(x) New KeyValuePair(Of Single, Single)(x.Amps, x.Efficiency)).ToDictionary(Function(x) x.Key, Function(x) x.Value)
+            Dim values4k As Dictionary(Of Single, Single) = inputs.where(Function(x) x.RPM = 4000).Select(Function(x) New KeyValuePair(Of Single, Single)(x.Amps, x.Efficiency)).ToDictionary(Function(x) x.Key, Function(x) x.Value)
+            Dim values6k As Dictionary(Of Single, Single) = inputs.where(Function(x) x.RPM = 6000).Select(Function(x) New KeyValuePair(Of Single, Single)(x.Amps, x.Efficiency)).ToDictionary(Function(x) x.Key, Function(x) x.Value)
 
 
-     CreateRangeTable()
-     'InitialiseRangeTable()
+            BuildInputTable(values2k, InputTable2000)
+            BuildInputTable(values4k, InputTable4000)
+            BuildInputTable(values6k, InputTable6000)
 
 
- End Sub
+            CreateRangeTable()
+            'InitialiseRangeTable()
 
 
- Public  shared Function Iterpolate( values As List(Of AltUserInput), x As single) As Single
-
-    Dim lowestX As single = values.Min( Function(m) m.Amps)
-    Dim highestX As Single = values.Max( Function(m) m.Amps)
-    Dim preKey, postKey ,preEff,postEff, EffSlope As single
-    Dim deltaX , deltaEff As single 
-
-    'Out of range, returns efficiency for lowest
-    If x< lowestX then Return values.First( Function(f) f.Amps= lowestX).Eff
-
-    'Out of range, efficiency for highest
-    If x> highestX then Return values.First( Function(f) f.Amps= highestX).Eff
-
-    'On Bounds check
-    If  values.Where( Function(w) w.Amps=x).Count=1 then Return values.First( Function(w) w.Amps=x).Eff
+        End Sub
 
 
-    'OK, we need to interpolate.
-    preKey   = values.Last(  Function(l)  l.Amps < x).Amps
-    postKey   = values.First( Function(l)  l.Amps > x).Amps
-    preEff = values.First( Function(f)  f.Amps=preKey).Eff
-    postEff = values.First( Function(f)  f.Amps=postKey).Eff
+        Public Shared Function Iterpolate(values As List(Of AltUserInput), x As Single) As Single
+
+            Dim lowestX As Single = values.Min(Function(m) m.Amps)
+            Dim highestX As Single = values.Max(Function(m) m.Amps)
+            Dim preKey, postKey, preEff, postEff, EffSlope As Single
+            Dim deltaX, deltaEff As Single
+
+            'Out of range, returns efficiency for lowest
+            If x < lowestX Then Return values.First(Function(f) f.Amps = lowestX).Eff
+
+            'Out of range, efficiency for highest
+            If x > highestX Then Return values.First(Function(f) f.Amps = highestX).Eff
+
+            'On Bounds check
+            If values.Where(Function(w) w.Amps = x).Count = 1 Then Return values.First(Function(w) w.Amps = x).Eff
 
 
-    deltaX = postKey-preKey
-    deltaEff  = postEff-preEff
-
-    'slopes
-    effSlope = deltaEff/deltaX
-
-
-    Dim retVal As Single =   ((x - preKey) * effSlope) + preEff
-
-    Return retVal
+            'OK, we need to interpolate.
+            preKey = values.Last(Function(l) l.Amps < x).Amps
+            postKey = values.First(Function(l) l.Amps > x).Amps
+            preEff = values.First(Function(f) f.Amps = preKey).Eff
+            postEff = values.First(Function(f) f.Amps = postKey).Eff
 
 
- End Function
+            deltaX = postKey - preKey
+            deltaEff = postEff - preEff
+
+            'slopes
+            effSlope = deltaEff / deltaX
 
 
- Private Sub CalculateRangeTable()
+            Dim retVal As Single = ((x - preKey) * effSlope) + preEff
 
-    'M10=Row0-Rpm - N10=Row0-Eff
-    'M11=Row1-Rpm - N11=Row1-Eff
-    'M12=Row2-Rpm - N12=Row2-Eff - 2000
-    'M13=Row3-Rpm - N13=Row3-Eff - 4000
-    'M14=Row4-Rpm - N14=Row4-Eff - 6000
-    'M15=Row5-Rpm - N15=Row5-Eff
-    'M16=Row6-Rpm - N16=Row6-Eff
-
-     Dim N10,N11,N12,N13,N14,N15,N16 As single
-     Dim M10,M11,M12,M13,M14,M15,M16 As single
-
-     'EFFICIENCY
-  
-    '2000
-     N12= Alternator.Iterpolate(InputTable2000,signals.CurrentDemandAmps)
-     RangeTable(2).Efficiency= N12
-    '4000
-     N13 = Alternator.Iterpolate(InputTable4000,signals.CurrentDemandAmps)
-     RangeTable(3).Efficiency= N13
-    '6000
-     N14 =Alternator.Iterpolate(InputTable6000,signals.CurrentDemandAmps)
-     RangeTable(4).Efficiency= N14
-
-    'Row0 & Row1 Efficiency  =IF(N13>N12,0,MAX(N12:N14)) - Example Alt 1 N13=
-     N11=IF(N13>N12,0,Math.Max(Math.MAX(N12,N13),N14))
-     RangeTable(1).Efficiency = N11
-     N10=N11
-     RangeTable(0).Efficiency = N10
+            Return retVal
 
 
-    'Row 5 Efficiency
-     N15 =IF(N13>N14,0,Math.Max(Math.MAX(N12,N13),N14))
-     RangeTable(5).Efficiency = N15
-    'Row 6 - Efficiency
-     N16 = N15
-     RangeTable(6).Efficiency = N16
+        End Function
 
-     'RPM
 
-     '2000 Row 2 - RPM
-      M12 =  2000
-      RangeTable(2).RPM = M12
+        Private Sub CalculateRangeTable()
 
-     '4000 Row 3 - RPM
-      M13 = 4000
-      RangeTable(3).RPM =  M13
+            'M10=Row0-Rpm - N10=Row0-Eff
+            'M11=Row1-Rpm - N11=Row1-Eff
+            'M12=Row2-Rpm - N12=Row2-Eff - 2000
+            'M13=Row3-Rpm - N13=Row3-Eff - 4000
+            'M14=Row4-Rpm - N14=Row4-Eff - 6000
+            'M15=Row5-Rpm - N15=Row5-Eff
+            'M16=Row6-Rpm - N16=Row6-Eff
 
-     '6000 Row 4 - RPM
-      M14 = 6000
-      RangeTable(4).RPM =  M14
+            Dim N10, N11, N12, N13, N14, N15, N16 As Single
+            Dim M10, M11, M12, M13, M14, M15, M16 As Single
+
+            'EFFICIENCY
+
+            '2000
+            N12 = Alternator.Iterpolate(InputTable2000, signals.CurrentDemandAmps)
+            RangeTable(2).Efficiency = N12
+            '4000
+            N13 = Alternator.Iterpolate(InputTable4000, signals.CurrentDemandAmps)
+            RangeTable(3).Efficiency = N13
+            '6000
+            N14 = Alternator.Iterpolate(InputTable6000, signals.CurrentDemandAmps)
+            RangeTable(4).Efficiency = N14
+
+            'Row0 & Row1 Efficiency  =IF(N13>N12,0,MAX(N12:N14)) - Example Alt 1 N13=
+            N11 = If(N13 > N12, 0, Math.Max(Math.MAX(N12, N13), N14))
+            RangeTable(1).Efficiency = N11
+            N10 = N11
+            RangeTable(0).Efficiency = N10
+
+
+            'Row 5 Efficiency
+            N15 = If(N13 > N14, 0, Math.Max(Math.MAX(N12, N13), N14))
+            RangeTable(5).Efficiency = N15
+            'Row 6 - Efficiency
+            N16 = N15
+            RangeTable(6).Efficiency = N16
+
+            'RPM
+
+            '2000 Row 2 - RPM
+            M12 = 2000
+            RangeTable(2).RPM = M12
+
+            '4000 Row 3 - RPM
+            M13 = 4000
+            RangeTable(3).RPM = M13
+
+            '6000 Row 4 - RPM
+            M14 = 6000
+            RangeTable(4).RPM = M14
 
             'Row 1 - RPM
             'NOTE: Update to reflect CombineALternatorSchematicV02 20150429
             'IF(M12=IF(N12>N13,M12-((M12-M13)/(N12-N13))*(N12-N11),M12-((M12-M13)/(N12-N13))*(N12-N11)), M12-0.01, IF(N12>N13,M12-((M12-M13)/(N12-N13))*(N12-N11),M12-((M12-M13)/(N12-N13))*(N12-N11)))
             M11 = Convert.ToSingle(If(N12 - N13 = 0, 0, If(M12 = If(N12 > N13, M12 - ((M12 - M13) / (N12 - N13)) * (N12 - N11), M12 - ((M12 - M13) / (N12 - N13)) * (N12 - N11)), M12 - 0.01, If(N12 > N13, M12 - ((M12 - M13) / (N12 - N13)) * (N12 - N11), M12 - ((M12 - M13) / (N12 - N13)) * (N12 - N11)))))
-      RangeTable(1).RPM =M11
+            RangeTable(1).RPM = M11
 
-      'Row 0 - RPM
-      M10 = IF(M11<1500,M11-1,1500)
-      RangeTable(0).RPM  = M10
+            'Row 0 - RPM
+            M10 = If(M11 < 1500, M11 - 1, 1500)
+            RangeTable(0).RPM = M10
 
-      'Row 5 - RPM
-      M15 =  Convert.ToSingle(IF(M14=IF((N14=0 OrElse N14=N13),M14+1,IF(N13>N14,((((M14-M13)/(N13-N14))*N14)+M14),((((M14-M13)/(N13-N14))*(N14-N15))+M14))),M14+0.01,IF((N14=0 OrElse N14=N13),M14+1,IF(N13>N14,((((M14-M13)/(N13-N14))*N14)+M14),((((M14-M13)/(N13-N14))*(N14-N15))+M14)))))
-      RangeTable(5).RPM = M15
-
-
-      'Row 6 - RPM
-      M16 =  IF(M15>10000,M15+1,10000)
-      RangeTable(6).RPM =  M16
-
- End Sub
+            'Row 5 - RPM
+            M15 = Convert.ToSingle(If(M14 = If((N14 = 0 OrElse N14 = N13), M14 + 1, If(N13 > N14, ((((M14 - M13) / (N13 - N14)) * N14) + M14), ((((M14 - M13) / (N13 - N14)) * (N14 - N15)) + M14))), M14 + 0.01, If((N14 = 0 OrElse N14 = N13), M14 + 1, If(N13 > N14, ((((M14 - M13) / (N13 - N14)) * N14) + M14), ((((M14 - M13) / (N13 - N14)) * (N14 - N15)) + M14)))))
+            RangeTable(5).RPM = M15
 
 
- Private sub InitialiseRangeTable()
+            'Row 6 - RPM
+            M16 = If(M15 > 10000, M15 + 1, 10000)
+            RangeTable(6).RPM = M16
 
-  RangeTable(0).RPM=0:RangeTable(0).Efficiency=0
-  RangeTable(1).RPM=0:RangeTable(0).Efficiency=0
-  RangeTable(2).RPM=2000:RangeTable(0).Efficiency=0
-  RangeTable(3).RPM=4000:RangeTable(0).Efficiency=0
-  RangeTable(4).RPM=6000:RangeTable(0).Efficiency=0
-  RangeTable(5).RPM=0:RangeTable(0).Efficiency=0
-  RangeTable(6).RPM=0:RangeTable(0).Efficiency=0
-
- End Sub
+        End Sub
 
 
- Private Sub CreateRangeTable()
+        Private Sub InitialiseRangeTable()
+
+            RangeTable(0).RPM = 0 : RangeTable(0).Efficiency = 0
+            RangeTable(1).RPM = 0 : RangeTable(0).Efficiency = 0
+            RangeTable(2).RPM = 2000 : RangeTable(0).Efficiency = 0
+            RangeTable(3).RPM = 4000 : RangeTable(0).Efficiency = 0
+            RangeTable(4).RPM = 6000 : RangeTable(0).Efficiency = 0
+            RangeTable(5).RPM = 0 : RangeTable(0).Efficiency = 0
+            RangeTable(6).RPM = 0 : RangeTable(0).Efficiency = 0
+
+        End Sub
 
 
-     RangeTable.Clear()
-
-     RangeTable.Add( New Table4Row(0,0))
-     RangeTable.Add( New Table4Row(0,0))
-     RangeTable.Add( New Table4Row(0,0))
-     RangeTable.Add( New Table4Row(0,0))
-     RangeTable.Add( New Table4Row(0,0))
-     RangeTable.Add( New Table4Row(0,0))
-     RangeTable.Add( New Table4Row(0,0))
+        Private Sub CreateRangeTable()
 
 
- End Sub
+            RangeTable.Clear()
 
- public Sub BuildInputTable(  inputs As Dictionary(of Single, single), targetTable As List (Of AltUserInput ))
-
-
-       Dim C11,C12,C13,C14,D11,D12,D13,D14 As single
-       Dim tmpAmp As single
-
-       targetTable.Clear()
-
-       'Row0
-       targetTable.Add( New AltUserInput(0,D14))
-
-       'Row1
-       targetTable.Add( New AltUserInput( 10, inputs(10)))
-
-       'Row2
-       targetTable.Add( New AltUserInput( 40, inputs(40)))
-
-       'Row3
-       targetTable.Add( New AltUserInput( 60, inputs(60)))
-
-       C11= targetTable(1).Amps : C12=targetTable(2).Amps : C13=targetTable(3).Amps
-       D11= targetTable(1).Eff  : D12=targetTable(2).Eff  : D13=targetTable(3).Eff
-       D14= IF(D12>D13,0, Math.Max(Math.MAX(D11,D12),D13))
+            RangeTable.Add(New Table4Row(0, 0))
+            RangeTable.Add(New Table4Row(0, 0))
+            RangeTable.Add(New Table4Row(0, 0))
+            RangeTable.Add(New Table4Row(0, 0))
+            RangeTable.Add(New Table4Row(0, 0))
+            RangeTable.Add(New Table4Row(0, 0))
+            RangeTable.Add(New Table4Row(0, 0))
 
 
-       'Row4  - Eff
-       targetTable.Add( new AltUserInput( 0 ,D14 ))
+        End Sub
+
+        Public Sub BuildInputTable(inputs As Dictionary(Of Single, Single), targetTable As List(Of AltUserInput))
 
 
-       'Row4  - Amps
-       tmpAmp =IF((D13=0 OrElse D13=D12),C13+1,IF(D12>D13,((((C13-C12)/(D12-D13))*D13)+C13),((((C13-C12)/(D12-D13))*(D13-D14))+C13)))
-       targetTable(4).Amps = tmpAmp
+            Dim C11, C12, C13, C14, D11, D12, D13, D14 As Single
+            Dim tmpAmp As Single
 
-       'Row5 
-       tmpAmp  =IF(C14>200,C14+1,200)
-       targetTable.Add( New AltUserInput(tmpAmp,D14))
+            targetTable.Clear()
 
-       'Row0
-       targetTable(0).Eff=D11
+            'Row0
+            targetTable.Add(New AltUserInput(0, D14))
 
+            'Row1
+            targetTable.Add(New AltUserInput(10, inputs(10)))
 
-  End Sub
+            'Row2
+            targetTable.Add(New AltUserInput(40, inputs(40)))
 
+            'Row3
+            targetTable.Add(New AltUserInput(60, inputs(60)))
 
-
- Public Function IsEqualTo(other As IAlternator) As Boolean Implements IAlternator.IsEqualTo
-
-     If Me.AlternatorName <> other.AlternatorName then Return False
-     If Me.PulleyRatio <> other.PulleyRatio then Return false
-     
-     Dim i As Integer =1
-
-     For i= 1 to 3
-
-       If Me.InputTable2000(i).Eff <> other.InputTable2000(i).Eff then Return False
-       If Me.InputTable4000(i).Eff <> other.InputTable4000(i).Eff then Return False
-       If Me.InputTable6000(i).Eff <> other.InputTable6000(i).Eff then Return False
+            C11 = targetTable(1).Amps : C12 = targetTable(2).Amps : C13 = targetTable(3).Amps
+            D11 = targetTable(1).Eff : D12 = targetTable(2).Eff : D13 = targetTable(3).Eff
+            D14 = If(D12 > D13, 0, Math.Max(Math.MAX(D11, D12), D13))
 
 
-     Next
+            'Row4  - Eff
+            targetTable.Add(New AltUserInput(0, D14))
 
-    Return True
-    
-  End Function
+
+            'Row4  - Amps
+            ' Should probably refactor this into some sort of helper/extension method
+            Dim numarray As Single() = {D11, D12, D13}
+            Dim maxD11_D13 As Single = numarray.Max()
+
+            tmpAmp = If((D13 = 0 OrElse D13 = D12 OrElse D13 = maxD11_D13), C13 + 1, If(D12 > D13, ((((C13 - C12) / (D12 - D13)) * D13) + C13), ((((C13 - C12) / (D12 - D13)) * (D13 - D14)) + C13)))
+            targetTable(4).Amps = tmpAmp
+
+            'Row5 
+            tmpAmp = If(C14 > 200, C14 + 1, 200)
+            targetTable.Add(New AltUserInput(tmpAmp, D14))
+
+            'Row0
+            targetTable(0).Eff = D11
+
+
+        End Sub
 
 
 
-End Class
+        Public Function IsEqualTo(other As IAlternator) As Boolean Implements IAlternator.IsEqualTo
+
+            If Me.AlternatorName <> other.AlternatorName Then Return False
+            If Me.PulleyRatio <> other.PulleyRatio Then Return False
+
+            Dim i As Integer = 1
+
+            For i = 1 To 3
+
+                If Me.InputTable2000(i).Eff <> other.InputTable2000(i).Eff Then Return False
+                If Me.InputTable4000(i).Eff <> other.InputTable4000(i).Eff Then Return False
+                If Me.InputTable6000(i).Eff <> other.InputTable6000(i).Eff Then Return False
+
+
+            Next
+
+            Return True
+
+        End Function
+
+
+
+    End Class
 
 
 
