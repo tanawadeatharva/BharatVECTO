82f449ffd71aa6b2902121162145fa3f469f1169
Michael Krisper <michael.krisper@tugraz.at>
2015-12-03 13:51:57 +0100

    added EUPL license header to all source files

--- VectoCore/Properties/Version.cs
+++ VectoCore/Properties/Version.cs
@@ -1,4 +1,20 @@
-﻿using System.Reflection;
+/*
+* Copyright 2015 European Union
+*
+* Licensed under the EUPL (the "Licence");
+* You may not use this work except in compliance with the Licence.
+* You may obtain a copy of the Licence at:
+*
+* http://ec.europa.eu/idabc/eupl5
+*
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the Licence is distributed on an "AS IS" basis,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the Licence for the specific language governing permissions and 
+* limitations under the Licence.
+*/
+
+using System.Reflection;
 // generated on 11.11.2015 13:31:44
 [assembly: AssemblyVersion("3.0.1.314")]
 [assembly: AssemblyFileVersion("3.0.1.314")]
