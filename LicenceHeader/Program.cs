dfab6d4eafb1cd5f12f7c3e285c1f20fca628e0d
Michael Krisper <michael.krisper@tugraz.at>
2015-12-03 13:36:27 +0100

    added project for adding licence headers to all cs files

---
+++ LicenceHeader/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace LicenceHeader
+{
+	/// <summary>
+	/// Updates/Adds the EUPL license header to every .cs file in the projects: VectoCore, VectoConsole, VectoCoreTest
+	/// </summary>
+	internal class Program
+	{
+		private const string SolutionRootDirectory = @"..\\..\\..";
+		private static readonly string[] ProjectDirectories = { "VectoCore", "VectoConsole", "VectoCoreTest" };
+
+		private static void Main()
+		{
+			var licence = File.ReadAllText("header.txt");
+
+			foreach (var dir in ProjectDirectories) {
+				foreach (
+					var file in Directory.GetFiles(Path.Combine(SolutionRootDirectory, dir), "*.cs", SearchOption.AllDirectories)) {
+					if (file.Contains("\\obj\\") || file.Contains("\\bin\\")) {
+						continue;
+					}
+
+					var re = new Regex("^.*?(?=using|namespace)");
+					var content = File.ReadAllText(file);
+					var updatedContent = re.Replace(content, licence);
+					if (updatedContent != content) {
+						File.WriteAllText(file, updatedContent);
+					}
+					Console.WriteLine(file);
+				}
+			}
+		}
+	}
+}
