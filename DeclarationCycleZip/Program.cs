53cf4e6d5b863272eb442965897e33fdeb739b5d
Markus Quaritsch <markus.quaritsch@tugraz.at>
2015-12-11 22:50:50 +0100

    removing unused uses

--- DeclarationCycleZip/Program.cs
+++ DeclarationCycleZip/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TUGraz.VectoCore.InputData.FileIO.Reader;
+using TUGraz.VectoCore.InputData.Reader;
 using TUGraz.VectoCore.Utils;
 
 namespace DeclarationCycleZip
