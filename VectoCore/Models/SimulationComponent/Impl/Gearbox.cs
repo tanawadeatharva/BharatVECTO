50ab739fa18e173403e2df07fa70126cb78de475
Michael Krisper <michael.krisper@tugraz.at>
2016-01-07 13:32:31 +0100

    corrected compile error

--- VectoCore/Models/SimulationComponent/Impl/Gearbox.cs
+++ VectoCore/Models/SimulationComponent/Impl/Gearbox.cs
@@ -390,6 +390,7 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 					// todo (MK, 2015-12-14): should we throw an interpolation error in EngineOnly Mode also?
 					Log.Warn("Gear {0} LossMap data was extrapolated: range for loss map is not sufficient.", Gear);
 					if (DataBus.ExecutionMode == ExecutionMode.Declaration) {
+						// todo (MK, 2016-01-07): add operating point and loss values for easier debugging
 						throw new VectoException(
 							"Gear {0} LossMap data was extrapolated in Declaration Mode: range for loss map is not sufficient.", Gear);
 					}
--- VectoCoreTest/Integration/Truck40tPowerTrain.cs
+++ VectoCoreTest/Integration/Truck40tPowerTrain.cs
@@ -66,7 +66,7 @@ namespace TUGraz.VectoCore.Tests.Integration
 		{
 			var fileWriter = new FileOutputWriter(modFileName, "");
 			var modData = new ModalDataContainer(modFileName, fileWriter);
-			var container = new VehicleContainer(modData, ExecutionMode.Engineering);
+			var container = new VehicleContainer(modData, executionMode: ExecutionMode.Engineering);
 
 			var engineData = MockSimulationDataFactory.CreateEngineDataFromFile(EngineFile);
 			var axleGearData = CreateAxleGearData();
