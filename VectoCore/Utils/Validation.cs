e1ea8d181f9eb15befbda6ea05b46e7b7abe0321
Michael Krisper <michael.krisper@tugraz.at>
2016-01-12 18:55:49 +0100

    added classes for validation functionality

---
+++ VectoCore/Utils/Validation.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace TUGraz.VectoCore.Utils
+{
+	public static class ValidationHelper
+	{
+		public static IList<ValidationResult> Validate<T>(this T entity)
+		{
+			var context = new ValidationContext(entity);
+			var results = new List<ValidationResult>();
+			Validator.TryValidateObject(entity, new ValidationContext(entity), results, true);
+
+			foreach (
+				var p in entity.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic)) {
+				var val = p.GetValue(entity);
+				var attrs = p.GetCustomAttributes(typeof(ValidationAttribute)).Cast<ValidationAttribute>();
+				context.MemberName = p.Name;
+				context.DisplayName = p.Name;
+				Validator.TryValidateValue(val, context, results, attrs);
+			}
+
+			foreach (
+				var f in
+					entity.GetType()
+						.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public)) {
+				var val = f.GetValue(entity);
+				var attrs = f.GetCustomAttributes(typeof(ValidationAttribute)).Cast<ValidationAttribute>();
+				context.MemberName = f.Name;
+				context.DisplayName = f.Name;
+				Validator.TryValidateValue(val, context, results, attrs);
+			}
+
+			return results;
+		}
+
+		public static bool IsValid<T>(this T entity)
+		{
+			return Validator.TryValidateObject(entity, new ValidationContext(entity), null, true);
+		}
+	}
+
+
+	public class ValidateObjectAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (value == null) {
+				return ValidationResult.Success;
+			}
+
+			var enumerable = value as IEnumerable;
+			if (enumerable != null) {
+				var i = 0;
+				foreach (var element in enumerable) {
+					var results = element.Validate();
+					if (results.Any()) {
+						return
+							new ValidationResult(
+								string.Format("Validation for Element {0} in {1} failed: {2}.", i, validationContext.DisplayName,
+									string.Join(" ", results)));
+					}
+					i++;
+				}
+			} else {
+				var results = value.Validate();
+				if (results.Any()) {
+					return new ValidationResult(
+						string.Format("Validation for {0} failed: {1}.", validationContext.DisplayName, string.Join(" ", results)));
+				}
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+
+	public class SIRangeAttribute : RangeAttribute
+	{
+		public SIRangeAttribute(int minimum, int maximum) : base(minimum, maximum) {}
+		public SIRangeAttribute(double minimum, double maximum) : base(minimum, maximum) {}
+		public SIRangeAttribute(SI minimum, SI maximum) : base(minimum.Value(), maximum.Value()) {}
+
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			return base.IsValid(((SI)value).Value(), validationContext);
+		}
+	}
+
+	public class PathAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (!File.Exists((string)value)) {
+				return new ValidationResult("File not found: " + (string)value);
+			}
+			return ValidationResult.Success;
+		}
+	}
+}
---
+++ VectoCoreTest/Models/SimulationComponentData/ValidationTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TUGraz.VectoCore.Models.Simulation.Data;
+using TUGraz.VectoCore.Models.Simulation.Impl;
+using TUGraz.VectoCore.Models.SimulationComponent.Data;
+using TUGraz.VectoCore.Models.SimulationComponent.Data.Engine;
+using TUGraz.VectoCore.Models.SimulationComponent.Data.Gearbox;
+using TUGraz.VectoCore.Utils;
+
+namespace TUGraz.VectoCore.Tests.Models.SimulationComponentData
+{
+	[TestClass]
+	public class CombustionEngineDataValidationTestClass
+	{
+		[TestMethod]
+		public void Validation_CombustionEngineData()
+		{
+			var fuelConsumption = new DataTable();
+			fuelConsumption.Columns.Add("");
+			fuelConsumption.Columns.Add("");
+			fuelConsumption.Columns.Add("");
+			fuelConsumption.Rows.Add("1", "1", "1");
+			fuelConsumption.Rows.Add("2", "2", "2");
+			fuelConsumption.Rows.Add("3", "3", "3");
+
+			var fullLoad = new DataTable();
+			fullLoad.Columns.Add("");
+			fullLoad.Columns.Add("");
+			fullLoad.Columns.Add("");
+			fullLoad.Columns.Add("");
+			fullLoad.Rows.Add("3", "3", "3", "3");
+			fullLoad.Rows.Add("3", "3", "3", "3");
+
+			var data = new CombustionEngineData {
+				ModelName = "asdf",
+				Displacement = 1.SI<CubicMeter>(),
+				IdleSpeed = 560.RPMtoRad(),
+				Inertia = 1.SI<KilogramSquareMeter>(),
+				WHTCUrban = 1.SI<KilogramPerWattSecond>(),
+				WHTCRural = 1.SI<KilogramPerWattSecond>(),
+				WHTCMotorway = 1.SI<KilogramPerWattSecond>(),
+				FullLoadCurve = EngineFullLoadCurve.Create(fullLoad),
+				ConsumptionMap = FuelConsumptionMap.Create(fuelConsumption)
+			};
+			var results = data.Validate();
+			Assert.IsFalse(results.Any(), "Validation Failed: " + string.Join("; ", results.Select(r => r.ErrorMessage)));
+			Assert.IsTrue(data.IsValid());
+		}
+
+		[TestMethod]
+		public void Validation_VectoRun()
+		{
+			var container = new VehicleContainer();
+			var data = new DistanceRun(container);
+			var engineData = new CombustionEngineData {
+				FullLoadCurve = EngineFullLoadCurve.ReadFromFile(@"TestData\Components\12t Delivery Truck.vfld"),
+				IdleSpeed = 560.RPMtoRad()
+			};
+
+			var gearboxData = new GearboxData();
+			gearboxData.Gears[1] = new GearData {
+				LossMap = TransmissionLossMap.ReadFromFile(@"TestData\Components\Direct Gear.vtlm", 1, "1"),
+				Ratio = 1
+			};
+
+			var axleGearData = new AxleGearData {
+				Ratio = 1,
+				LossMap = TransmissionLossMap.ReadFromFile(@"TestData\Components\limited.vtlm", 1, "1"),
+			};
+
+			container.RunData = new VectoRunData {
+				GearboxData = gearboxData,
+				EngineData = engineData,
+				AxleGearData = axleGearData
+			};
+
+			var results = data.Validate();
+			Assert.IsFalse(results.Any(), "Validation Failed: " + string.Join("; ", results.Select(r => r.ErrorMessage)));
+		}
+
+
+		[TestMethod]
+		public void Validation_Test()
+		{
+			var data = new Data();
+			data.lu = new deep();
+			var results = data.Validate();
+			Assert.IsFalse(results.Any(), "Validation Failed: " + string.Join(" ", results.Select(r => r.ErrorMessage)));
+		}
+
+
+		public class deep
+		{
+			[Required, Range(10, 16)] public int mah = 45;
+		}
+
+
+		public abstract class Muh
+		{
+			[Required, Range(6, 9)]
+			protected int higss { get; set; }
+
+			[Required, ValidateObject] public deep lu;
+		}
+
+
+		public class Data : Muh
+		{
+			[Required, Range(2, 5)] private int bla = 7;
+		}
+	}
+}
