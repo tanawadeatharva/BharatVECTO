601bda2eb34c6e769547dad6eef8df1ce4674dce
Michael Krisper <michael.krisper@tugraz.at>
2016-01-18 13:48:50 +0100

    first stubs for pwheel tests

---
+++ VectoCoreTest/Models/Simulation/PwheelModeTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TUGraz.VectoCore.Tests.Models.Simulation
+{
+	[TestClass]
+	public class PwheelModeTests
+	{
+		/// <summary>
+		/// Test if the cycle file can be read.
+		/// </summary>
+		/// <remarks>VECTO-177</remarks>
+		[TestMethod]
+		public void Pwheel_ReadCycle_Test()
+		{
+			Assert.Fail("Test not implemented");
+		}
+
+		/// <summary>
+		/// Tests if the powertrain can be created in Pwheel mode.
+		/// </summary>
+		/// <remarks>VECTO-177</remarks>
+		[TestMethod]
+		public void Pwheel_CreatePowertrain_Test()
+		{
+			Assert.Fail("Test not implemented");
+		}
+
+		/// <summary>
+		/// Tests if the simulation runs a Pwheel mode.
+		/// </summary>
+		/// <remarks>VECTO-177</remarks>
+		[TestMethod]
+		public void Pwheel_Simulate_Test()
+		{
+			Assert.Fail("Test not implemented");
+		}
+
+		/// <summary>
+		/// Tests if the modfile is correct in Pwheel mode.
+		/// </summary>
+		/// <remarks>VECTO-177</remarks>
+		[TestMethod]
+		public void Pwheel_Output_Modfile_Test()
+		{
+			Assert.Fail("Test not implemented");
+		}
+	}
+}
