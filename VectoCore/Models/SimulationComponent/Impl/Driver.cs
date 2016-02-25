980279f3a14f84a4545926ec14fc058f1a1d6960
Michael Krisper <michael.krisper@tugraz.at>
2016-02-25 17:42:11 +0100

    experimental search algorithm interpolate linear

--- VectoCore/Models/SimulationComponent/Impl/Driver.cs
+++ VectoCore/Models/SimulationComponent/Impl/Driver.cs
@@ -493,168 +493,96 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 			Log.Info("Disabling logging during search iterations");
 			LogManager.DisableLogging();
 
-			var debug = new List<dynamic>(); // only used while testing
+			var operatingPoint = ComputeTimeInterval(acceleration, ds);
+			var dt = operatingPoint.SimulationInterval;
+			Watt brakePower = null;
 
-			var operatingPoint = new OperatingPoint() { SimulationDistance = ds, Acceleration = acceleration };
-			Watt origDelta = null;
 			initialResponse.Switch().
-				Case<ResponseGearShift>(r => origDelta = r.GearboxPowerRequest).
-				Case<ResponseUnderload>(r => origDelta = DataBus.ClutchClosed(absTime)
+				Case<ResponseGearShift>(r => brakePower = r.GearboxPowerRequest).
+				Case<ResponseUnderload>(r => brakePower = DataBus.ClutchClosed(absTime)
 					? r.Delta
 					: r.GearboxPowerRequest).
 				Default(r => { throw new UnexpectedResponseException("cannot use response for searching braking power!", r); });
 
-			// braking power is in the range of the exceeding delta. set searching range to 2/3 so that 
-			// the target point is approximately in the center of the second interval
-			var searchInterval = origDelta.Abs() * 2 / 3;
+			try {
+				DataBus.BrakePower = SearchAlgorithm.Search(DataBus.BrakePower, brakePower, -brakePower,
+					getYValue: result => {
+						var response = (ResponseDryRun)result;
+						return DataBus.ClutchClosed(absTime) ? response.DeltaDragLoad : response.GearboxPowerRequest;
+					},
+					evaluateFunction: x => {
+						DataBus.BrakePower = x;
+						return NextComponent.Request(absTime, dt, acceleration, gradient, true);
+					},
+					criterion: result => {
+						var response = (ResponseDryRun)result;
+						var delta = DataBus.ClutchClosed(absTime) ? response.DeltaDragLoad : response.GearboxPowerRequest;
+						return delta.IsEqual(0.SI<Watt>(), Constants.SimulationSettings.EnginePowerSearchTolerance);
+					});
 
-			debug.Add(new { brakePower = 0.SI<Watt>(), searchInterval, delta = origDelta, operatingPoint });
+				NextComponent.Request(absTime, dt, acceleration, gradient);
 
-			var brakePower = searchInterval * -origDelta.Sign();
-
-			var modeBinarySearch = false;
-			var intervalFactor = 1.0;
-			var retryCount = 0;
-
-			do {
-				operatingPoint = ComputeTimeInterval(operatingPoint.Acceleration, ds);
-				DataBus.BrakePower = brakePower;
-				var response =
-					(ResponseDryRun)
-						NextComponent.Request(absTime, operatingPoint.SimulationInterval, operatingPoint.Acceleration, gradient, true);
-				var delta = DataBus.ClutchClosed(absTime) ? response.DeltaDragLoad : response.GearboxPowerRequest;
-
-				if (delta.IsEqual(0.SI<Watt>(), Constants.SimulationSettings.EnginePowerSearchTolerance)) {
-					LogManager.EnableLogging();
-					Log.Debug("found operating point in {0} iterations, delta: {1}", debug.Count, delta);
-					return operatingPoint;
-				}
-
-				debug.Add(new { brakePower, searchInterval, delta, operatingPoint });
-
-				// check if a correct searchInterval was found (when the delta changed signs, we stepped through the 0-point)
-				// from then on the searchInterval can be bisected.
-				if (origDelta.Sign() != delta.Sign()) {
-					intervalFactor = 0.5;
-					if (!modeBinarySearch) {
-						modeBinarySearch = true;
-						retryCount = 0; // again max. 100 iterations for the binary search...
-					}
-				}
-
-				searchInterval *= intervalFactor;
-				brakePower += searchInterval * -delta.Sign();
-			} while (retryCount++ < Constants.SimulationSettings.DriverSearchLoopThreshold);
-
-			LogManager.EnableLogging();
-			Log.Warn("Exceeded max iterations when searching for operating point!");
-			Log.Warn("exceeded: {0} ... {1}", ", ".Join(debug.Take(5)), ", ".Join(debug.Slice(-6)));
-			Log.Error("Failed to find operating point for breaking!");
-			throw new VectoSearchFailedException("Failed to find operating point for breaking!exceeded: {0} ... {1}",
-				", ".Join(debug.Take(5)), ", ".Join(debug.Slice(-6)));
+				return operatingPoint;
+			} catch (Exception) {
+				Log.Error("Failed to find operating point! absTime: {0}", absTime);
+				throw;
+			}
 		}
 
-
-		/// <summary>
-		/// search for the operating point where the engine's requested power is either on the full-load curve or  on the drag curve (parameter 'coasting').
-		/// before the search can  be performed either a normal request or a dry-run request has to be made and the response is passed to this method.
-		/// perform a binary search starting with the currentState's acceleration value.
-		/// while searching it might be necessary to reduce the simulation distance because the vehicle already stopped before reaching the given ds. However,
-		/// it for every new iteration of the search the original distance is used. The resulting distance is returned.
-		/// After the search operation a normal request has to be made by the caller of this method. The final acceleration and time interval is stored in CurrentState.
-		/// </summary>
-		/// <param name="absTime">absTime from the original request</param>
-		/// <param name="ds">ds from the original request</param>
-		/// <param name="gradient">gradient from the original request</param>
-		/// <param name="acceleration"></param>
-		/// <param name="initialResponse"></param>
-		/// <param name="coasting">if true approach the drag-load curve, otherwise full-load curve</param>
-		/// <returns></returns>
 		protected OperatingPoint SearchOperatingPoint(Second absTime, Meter ds, Radian gradient,
 			MeterPerSquareSecond acceleration, IResponse initialResponse, bool coasting = false)
 		{
 			Log.Info("Disabling logging during search iterations");
 			LogManager.DisableLogging();
 
-			var debug = new List<dynamic>();
-
 			var retVal = new OperatingPoint { Acceleration = acceleration, SimulationDistance = ds };
-
 			var actionRoll = !DataBus.ClutchClosed(absTime);
 
-			var searchInterval = Constants.SimulationSettings.OperatingPointInitialSearchIntervalAccelerating;
-			var intervalFactor = 1.0;
-
-			Watt origDelta = null;
+			var x = retVal.Acceleration;
+			Watt y = null;
 			if (actionRoll) {
 				initialResponse.Switch().
-					Case<ResponseDryRun>(r => origDelta = r.GearboxPowerRequest).
-					Case<ResponseFailTimeInterval>(r => origDelta = r.GearboxPowerRequest).
+					Case<ResponseDryRun>(r => y = r.GearboxPowerRequest).
+					Case<ResponseFailTimeInterval>(r => y = r.GearboxPowerRequest).
 					Default(r => { throw new UnexpectedResponseException("Unknown response type.", r); });
 			} else {
 				initialResponse.Switch().
-					Case<ResponseOverload>(r => origDelta = r.Delta). // search operating point in drive action after overload
-					Case<ResponseDryRun>(r => origDelta = coasting ? r.DeltaDragLoad : r.DeltaFullLoad).
+					Case<ResponseOverload>(r => y = r.Delta). // search operating point in drive action after overload
+					Case<ResponseDryRun>(r => y = coasting ? r.DeltaDragLoad : r.DeltaFullLoad).
 					Default(r => { throw new UnexpectedResponseException("Unknown response type.", r); });
 			}
-			var delta = origDelta;
 
-			var retryCount = 0;
-			do {
-				debug.Add(new { delta, acceleration = retVal.Acceleration, searchInterval });
+			try {
+				x = SearchAlgorithm.Search(x, y,
+					Constants.SimulationSettings.OperatingPointInitialSearchIntervalAccelerating,
+					getYValue: response => {
+						var r = (ResponseDryRun)response;
+						return actionRoll ? r.GearboxPowerRequest : (coasting ? r.DeltaDragLoad : r.DeltaFullLoad);
+					},
+					evaluateFunction:
+						acc => {
+							var tmp = ComputeTimeInterval(retVal.Acceleration, ds);
+							retVal.SimulationInterval = tmp.SimulationInterval;
+							retVal.SimulationDistance = tmp.SimulationDistance;
+							return NextComponent.Request(absTime, retVal.SimulationInterval, acc, gradient, true);
+						},
+					criterion: response => {
+						var r = (ResponseDryRun)response;
+						var d = actionRoll ? r.GearboxPowerRequest : (coasting ? r.DeltaDragLoad : r.DeltaFullLoad);
+						return d.IsEqual(0.SI<Watt>(), Constants.SimulationSettings.EnginePowerSearchTolerance);
+					});
 
-				// check if a correct searchInterval was found (when the delta changed signs, we stepped through the 0-point)
-				// from then on the searchInterval can be bisected.
-				if (origDelta.Sign() != delta.Sign()) {
-					intervalFactor = 0.5;
-				}
 
-				searchInterval *= intervalFactor;
-				retVal.Acceleration += searchInterval * -delta.Sign();
-
-				if (retVal.Acceleration < (10 * DriverData.AccelerationCurve.MaxDeceleration() - searchInterval)
-					|| retVal.Acceleration > (DriverData.AccelerationCurve.MaxAcceleration() + searchInterval)) {
-					LogManager.EnableLogging();
-					Log.Warn("Operating Point outside driver acceleration limits: a: {0}", retVal.Acceleration);
-					LogManager.DisableLogging();
-				}
-
-				var tmp = ComputeTimeInterval(retVal.Acceleration, ds);
-				retVal.SimulationInterval = tmp.SimulationInterval;
-				retVal.SimulationDistance = tmp.SimulationDistance;
-
-				var response =
-					(ResponseDryRun)NextComponent.Request(absTime, retVal.SimulationInterval, retVal.Acceleration, gradient, true);
-				delta = actionRoll ? response.GearboxPowerRequest : (coasting ? response.DeltaDragLoad : response.DeltaFullLoad);
-
-				if (response is ResponseEngineSpeedTooLow) {
-					LogManager.EnableLogging();
-					Log.Debug("Got EngineSpeedTooLow during SearchOperatingPoint. Aborting!");
-					throw new VectoSimulationException("EngineSpeed too low during search.");
-				}
-
-				if (delta.IsEqual(0.SI<Watt>(), Constants.SimulationSettings.EnginePowerSearchTolerance)) {
-					LogManager.EnableLogging();
-					Log.Debug("found operating point in {0} iterations. Engine Power req: {2}, Gearbox Power req: {3} delta: {1}",
-						debug.Count, delta, response.EnginePowerRequest, response.GearboxPowerRequest);
-					return retVal;
-				}
-			} while (retryCount++ < Constants.SimulationSettings.DriverSearchLoopThreshold);
-
-			LogManager.EnableLogging();
-			Log.Error("Exceeded max iterations when searching for operating point!");
-			Log.Error("acceleration: {0} ... {1}", ", ".Join(debug.Take(5).Select(x => x.acceleration)),
-				", ".Join(debug.Slice(-6).Select(x => x.acceleration)));
-			Log.Error("exceeded: {0} ... {1}", ", ".Join(debug.Take(5).Select(x => x.delta)),
-				", ".Join(debug.Slice(-6).Select(x => x.delta)));
-			// issue request once more for logging...
-			NextComponent.Request(absTime, retVal.SimulationInterval, retVal.Acceleration, gradient, true);
-			Log.Error("Failed to find operating point! absTime: {0}", absTime);
-			throw new VectoSearchFailedException("Failed to find operating point!  exceeded: {0} ... {1}",
-				", ".Join(debug.Take(5).Select(x => x.delta)),
-				", ".Join(debug.Slice(-6).Select(x => x.delta)));
+				return new OperatingPoint {
+					Acceleration = x,
+					SimulationInterval = retVal.SimulationInterval,
+					SimulationDistance = retVal.SimulationDistance
+				};
+			} catch (Exception) {
+				Log.Error("Failed to find operating point! absTime: {0}", absTime);
+				throw;
+			}
 		}
-
 
 		/// <summary>
 		/// compute the acceleration and time-interval such that the vehicle's velocity approaches the given target velocity
---
+++ VectoCore/Utils/SearchAlgorithm.cs
@@ -0,0 +1,143 @@
+﻿/*
+* This file is part of VECTO.
+*
+* Copyright © 2012-2016 European Union
+*
+* Developed by Graz University of Technology,
+*              Institute of Internal Combustion Engines and Thermodynamics,
+*              Institute of Technical Informatics
+*
+* VECTO is licensed under the EUPL, Version 1.1 or - as soon they will be approved
+* by the European Commission - subsequent versions of the EUPL (the "Licence");
+* You may not use VECTO except in compliance with the Licence.
+* You may obtain a copy of the Licence at:
+*
+* https://joinup.ec.europa.eu/community/eupl/og_page/eupl
+*
+* Unless required by applicable law or agreed to in writing, VECTO
+* distributed under the Licence is distributed on an "AS IS" basis,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the Licence for the specific language governing permissions and
+* limitations under the Licence.
+*
+* Authors:
+*   Stefan Hausberger, hausberger@ivt.tugraz.at, IVT, Graz University of Technology
+*   Christian Kreiner, christian.kreiner@tugraz.at, ITI, Graz University of Technology
+*   Michael Krisper, michael.krisper@tugraz.at, ITI, Graz University of Technology
+*   Raphael Luz, luz@ivt.tugraz.at, IVT, Graz University of Technology
+*   Markus Quaritsch, markus.quaritsch@tugraz.at, IVT, Graz University of Technology
+*   Martin Rexeis, rexeis@ivt.tugraz.at, IVT, Graz University of Technology
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NLog;
+using NLog.Fluent;
+using TUGraz.VectoCore.Exceptions;
+
+namespace TUGraz.VectoCore.Utils
+{
+	public static class SearchAlgorithm
+	{
+		public static T Search<T>(T x, SI y, T interval, Func<object, SI> getYValue,
+			Func<T, object> evaluateFunction, Func<object, bool> criterion) where T : SIBase<T>
+		{
+//#if DEBUG
+			var res = InterpolateLinear(x, y, interval, getYValue, evaluateFunction, criterion);
+//#else
+//			var res = SearchBinary(x, y, interval, getYValue, evaluateFunction, criterion);
+//#endif
+			return res;
+		}
+
+		/// <summary>
+		/// Line Search Algorithm. 
+		/// Phase 1: Linear Bracketing
+		/// Phase 2: Binary Sectioning
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="interval"></param>
+		/// <param name="getYValue"></param>
+		/// <param name="evaluateFunction"></param>
+		/// <param name="criterion"></param>
+		/// <returns></returns>
+		public static T SearchBinary<T>(T x, SI y, T interval, Func<object, SI> getYValue,
+			Func<T, object> evaluateFunction, Func<object, bool> criterion) where T : SIBase<T>
+		{
+			var log = LogManager.GetLogger(typeof(SearchAlgorithm).FullName);
+			LogManager.DisableLogging();
+			var intervalFactor = 1.0;
+			var origY = y;
+			var debug = new List<dynamic> { new { x, y } };
+
+			for (var iterationCount = 1; iterationCount < 100; iterationCount++) {
+				debug.Add(new { x, y });
+
+				if (origY.Sign() != y.Sign()) {
+					intervalFactor = 0.5;
+				}
+
+				interval *= intervalFactor;
+				x += interval * -y.Sign();
+
+				var result = evaluateFunction(x);
+				if (criterion(result)) {
+					log.Debug("InterpolateLinear found an operating point after {0} function calls.", iterationCount);
+					return x;
+				}
+				y = getYValue(result);
+			}
+
+			LogManager.EnableLogging();
+
+			log.Debug("SearchBinary could not find an operating point.");
+			log.Error("Exceeded max iterations when searching for operating point!");
+			log.Error("debug: {0} ... {1}", ", ".Join(debug.Take(5)), ", ".Join(debug.Slice(-6)));
+			throw new VectoSearchFailedException("Failed to find operating point! points: {0} ... {1}", ", ".Join(debug.Take(5)),
+				", ".Join(debug.Slice(-6)));
+		}
+
+		public static T InterpolateLinear<T>(T x1, SI y1, T interval, Func<object, SI> getYValue,
+			Func<T, object> evaluateFunction, Func<object, bool> criterion) where T : SIBase<T>
+		{
+			var log = LogManager.GetLogger(typeof(SearchAlgorithm).FullName);
+			LogManager.DisableLogging();
+			var debug = new List<dynamic> { new { x = x1, y = y1 } };
+
+			var x2 = x1 + interval;
+			var result = evaluateFunction(x2);
+			if (criterion(result)) {
+				log.Debug("InterpolateLinear found an operating point after 1 function call.");
+				return x2;
+			}
+
+			for (var iterationCount = 2; iterationCount < 10; iterationCount++) {
+				var y2 = getYValue(result);
+				debug.Add(new { x = x2, y = y2 });
+				var k = (y2 - y1) / (x2 - x1);
+				var d = y2 - k * x2;
+
+				x1 = x2;
+				x2 = (-d / k).Cast<T>();
+
+				result = evaluateFunction(x2);
+				if (criterion(result)) {
+					log.Debug("InterpolateLinear found an operating point after {0} function calls.", iterationCount);
+					return x2;
+				}
+
+				y1 = y2;
+			}
+
+			LogManager.EnableLogging();
+			log.Debug("InterpolateLinear could not find an operating point.");
+			log.Error("Exceeded max iterations when searching for operating point!");
+			log.Error("debug: {0} ... {1}", ", ".Join(debug.Take(5)), ", ".Join(debug.Slice(-6)));
+			throw new VectoSearchFailedException("Failed to find operating point! points: {0} ... {1}", ", ".Join(debug.Take(5)),
+				", ".Join(debug.Slice(-6)));
+		}
+	}
+}
