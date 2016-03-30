8b980dc8c85d20a263be904c67faa9f2d81eaeb9
Markus Quaritsch <markus.quaritsch@tugraz.at>
2016-03-30 16:51:31 +0200

    change cyclestep default argument, code formatting

--- VECTOAux/VectoAuxiliaries/DownstreamModules/IM10.vb
+++ VECTOAux/VectoAuxiliaries/DownstreamModules/IM10.vb
@@ -15,30 +15,25 @@ Imports VectoAuxiliaries.Hvac
 Imports VectoAuxiliaries.DownstreamModules
 
 Namespace DownstreamModules
+	Public Interface IM10
+		'AverageLoadsFuelConsumptionInterpolatedForPneumatics
+		ReadOnly Property AverageLoadsFuelConsumptionInterpolatedForPneumatics As Single
 
-Public Interface IM10
+		'Interpolated FC between points 2-3-1 Representing smart Pneumatics = Fuel consumption with smart Pneumatics and average electrical  power demand
+		ReadOnly Property FuelConsumptionSmartPneumaticsAndAverageElectricalPowerDemand As Single
 
-  'AverageLoadsFuelConsumptionInterpolatedForPneumatics
-  ReadOnly Property AverageLoadsFuelConsumptionInterpolatedForPneumatics As Single
-  
-  'Interpolated FC between points 2-3-1 Representing smart Pneumatics = Fuel consumption with smart Pneumatics and average electrical  power demand
-  ReadOnly Property FuelConsumptionSmartPneumaticsAndAverageElectricalPowerDemand As Single
-  
-   Sub CycleStep( Optional stepTimeInSeconds As Single = nothing)
-  
-  'Added for diagnostic inspection purposes only, does not materially affect the class function.
-  ReadOnly Property P1X      As single
-  ReadOnly Property P1Y      As single
-  ReadOnly Property P2X      As single
-  ReadOnly Property P2Y      As single
-  ReadOnly Property P3X      As single
-  ReadOnly Property P3Y      As single
-  ReadOnly Property XTAIN    As single
-  ReadOnly Property INTRP1   As single
-  ReadOnly Property INTRP2   As single
-  
-End Interface
+		Sub CycleStep(Optional stepTimeInSeconds As Single = 0.0)
 
-
+		'Added for diagnostic inspection purposes only, does not materially affect the class function.
+		ReadOnly Property P1X As Single
+		ReadOnly Property P1Y As Single
+		ReadOnly Property P2X As Single
+		ReadOnly Property P2Y As Single
+		ReadOnly Property P3X As Single
+		ReadOnly Property P3Y As Single
+		ReadOnly Property XTAIN As Single
+		ReadOnly Property INTRP1 As Single
+		ReadOnly Property INTRP2 As Single
+	End Interface
 End Namespace
 
