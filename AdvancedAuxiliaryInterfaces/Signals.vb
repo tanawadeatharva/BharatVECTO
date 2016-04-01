53b1ebed2c261825af96c0ddf1b92a1888765b6f
Markus Quaritsch <markus.quaritsch@tugraz.at>
2016-04-01 14:52:32 +0200

    changed cycletime to seconds

--- AdvancedAuxiliaryInterfaces/Signals.vb
+++ AdvancedAuxiliaryInterfaces/Signals.vb
@@ -11,50 +11,54 @@
 
 
 Public Class Signals
-Implements ISignals
+	Implements ISignals
 
+	'Backing variables
+	Private _WHTCCorretion As Single = 1
+	Private _smartElectrics As Boolean
 
-    'Backing variables
-    Private _WHTCCorretion As Single = 1
-    Private _smartElectrics As Boolean
+	Public Property ClutchEngaged As Boolean Implements ISignals.ClutchEngaged
+	Public Property EngineDrivelinePower As Single Implements ISignals.EngineDrivelinePower
+	Public Property EngineDrivelineTorque As Single Implements ISignals.EngineDrivelineTorque
+	Public Property EngineMotoringPower As Single Implements ISignals.EngineMotoringPower
+	Public Property EngineSpeed As Integer Implements ISignals.EngineSpeed
 
-    Public Property ClutchEngaged As Boolean Implements ISignals.ClutchEngaged
-    Public Property EngineDrivelinePower As Single Implements ISignals.EngineDrivelinePower
-    Public Property EngineDrivelineTorque As Single Implements ISignals.EngineDrivelineTorque
-    Public Property EngineMotoringPower As Single Implements ISignals.EngineMotoringPower
-    Public Property EngineSpeed As Integer Implements ISignals.EngineSpeed
-    Public Property SmartElectrics As Boolean Implements ISignals.SmartElectrics
-        Get
-            Return _smartElectrics
-        End Get
-        Set(value As Boolean)
-            _smartElectrics = value
-        End Set
-    End Property
-    Public Property SmartPneumatics As Boolean Implements ISignals.SmartPneumatics
-    Public Property TotalCycleTimeSeconds As Integer Implements ISignals.TotalCycleTimeSeconds
-    Public Property CurrentCycleTimeInSeconds As Integer Implements ISignals.CurrentCycleTimeInSeconds
-    Public Property PreExistingAuxPower As Single Implements ISignals.PreExistingAuxPower
-    Public Property Idle As Boolean Implements ISignals.Idle
-    Public Property InNeutral As Boolean Implements ISignals.InNeutral
-    Public Property AuxiliaryEventReportingLevel As AdvancedAuxiliaryMessageType Implements ISignals.AuxiliaryEventReportingLevel
-    Public Property EngineStopped As Boolean Implements ISignals.EngineStopped
-    Public Property DeclarationMode As Boolean Implements ISignals.DeclarationMode
-    Public Property WHTC As Single Implements ISignals.WHTC
-        Set(value As Single)
-            _WHTCCorretion = value
-        End Set
-        Get
-            Return _WHTCCorretion
-        End Get
-    End Property
-    Public Property EngineIdleSpeed As Single Implements ISignals.EngineIdleSpeed
-    Public Property PneumaticOverrunUtilisation As Single Implements ISignals.PneumaticOverrunUtilisation
-    Public Property StoredEnergyEfficiency As Single Implements ISignals.StoredEnergyEfficiency
-    Public Property RunningCalc As Boolean Implements ISignals.RunningCalc
-    Public Property Internal_Engine_Power As Single Implements ISignals.Internal_Engine_Power
+	Public Property SmartElectrics As Boolean Implements ISignals.SmartElectrics
+		Get
+			Return _smartElectrics
+		End Get
+		Set(value As Boolean)
+			_smartElectrics = value
+		End Set
+	End Property
+
+	Public Property SmartPneumatics As Boolean Implements ISignals.SmartPneumatics
+	Public Property TotalCycleTimeSeconds As Integer Implements ISignals.TotalCycleTimeSeconds
+	Public Property CurrentCycleTimeInSeconds As Double Implements ISignals.CurrentCycleTimeInSeconds
+	Public Property PreExistingAuxPower As Single Implements ISignals.PreExistingAuxPower
+	Public Property Idle As Boolean Implements ISignals.Idle
+	Public Property InNeutral As Boolean Implements ISignals.InNeutral
+
+	Public Property AuxiliaryEventReportingLevel As AdvancedAuxiliaryMessageType _
+		Implements ISignals.AuxiliaryEventReportingLevel
+
+	Public Property EngineStopped As Boolean Implements ISignals.EngineStopped
+	Public Property DeclarationMode As Boolean Implements ISignals.DeclarationMode
+
+	Public Property WHTC As Single Implements ISignals.WHTC
+		Set(value As Single)
+			_WHTCCorretion = value
+		End Set
+		Get
+			Return _WHTCCorretion
+		End Get
+	End Property
+
+	Public Property EngineIdleSpeed As Single Implements ISignals.EngineIdleSpeed
+	Public Property PneumaticOverrunUtilisation As Single Implements ISignals.PneumaticOverrunUtilisation
+	Public Property StoredEnergyEfficiency As Single Implements ISignals.StoredEnergyEfficiency
+	Public Property RunningCalc As Boolean Implements ISignals.RunningCalc
+	Public Property Internal_Engine_Power As Single Implements ISignals.Internal_Engine_Power
 End Class
-
-
 
 
