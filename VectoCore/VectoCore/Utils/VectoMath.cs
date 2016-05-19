94c5f376dbd061dc3a179da4c698c01b76419979
Michael Krisper <michael.krisper@tugraz.at>
2016-05-19 14:08:22 +0200

    SI performance

--- VectoCore/VectoCore/Utils/VectoMath.cs
+++ VectoCore/VectoCore/Utils/VectoMath.cs
@@ -56,20 +56,23 @@ namespace TUGraz.VectoCore.Utils
 		public static TResult Interpolate<T, TResult>(T x1, T x2, TResult y1, TResult y2, T xint) where T : SI
 			where TResult : SIBase<TResult>
 		{
-			return ((xint - x1) * (y2 - y1) / (x2 - x1) + y1).Cast<TResult>();
+			return Interpolate(x1.Value(), x2.Value(), y1.Value(), y2.Value(), xint.Value()).SI<TResult>();
 		}
 
-
-		public static double Interpolate<T>(T x1, T x2, double y1, double y2, T xint)
-			where T : SI
+		public static double Interpolate<T>(T x1, T x2, double y1, double y2, T xint) where T : SI
 		{
-			return (((xint - x1) * (y2 - y1) / (x2 - x1)).Cast<Scalar>() + y1).Value();
+			return Interpolate(x1.Value(), x2.Value(), y1, y2, xint.Value());
 		}
 
 		public static TResult Interpolate<TResult>(double x1, double x2, TResult y1, TResult y2, double xint)
 			where TResult : SIBase<TResult>
 		{
-			return ((xint - x1) * (y2 - y1) / (x2 - x1) + y1).Cast<TResult>();
+			return Interpolate(x1, x2, y1.Value(), y2.Value(), xint).SI<TResult>();
+		}
+
+		public static double Interpolate(Point p1, Point p2, double x)
+		{
+			return Interpolate(p1.X, p2.X, p1.Y, p2.Y, x);
 		}
 
 		/// <summary>
@@ -77,12 +80,7 @@ namespace TUGraz.VectoCore.Utils
 		/// </summary>
 		public static double Interpolate(double x1, double x2, double y1, double y2, double xint)
 		{
-			return ((xint - x1) * (y2 - y1) / (x2 - x1) + y1);
-		}
-
-		public static double Interpolate(Point p1, Point p2, double x)
-		{
-			return Interpolate(p1.X, p2.X, p1.Y, p2.Y, x);
+			return (xint - x1) * (y2 - y1) / (x2 - x1) + y1;
 		}
 
 		/// <summary>
@@ -112,7 +110,9 @@ namespace TUGraz.VectoCore.Utils
 		public static T Limit<T>(this T value, T lowerBound, T upperBound) where T : IComparable
 		{
 			if (lowerBound.CompareTo(upperBound) > 0) {
-				throw new VectoException("VectoMath.Limit: lowerBound must not be greater than upperBound. lowerBound: {0}, upperBound: {1}", lowerBound, upperBound);
+				throw new VectoException(
+					"VectoMath.Limit: lowerBound must not be greater than upperBound. lowerBound: {0}, upperBound: {1}", lowerBound,
+					upperBound);
 			}
 
 			if (value.CompareTo(upperBound) > 0) {
