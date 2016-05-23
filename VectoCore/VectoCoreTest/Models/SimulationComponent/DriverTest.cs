56fd1d13d899f45048a9d6aa25d373e648bebdad
Markus Quaritsch <markus.quaritsch@tugraz.at>
2016-05-23 09:12:31 +0200

    fixed testcases: null reference due to missing cycle

--- VectoCore/VectoCoreTest/Models/SimulationComponent/DriverTest.cs
+++ VectoCore/VectoCoreTest/Models/SimulationComponent/DriverTest.cs
@@ -71,6 +71,7 @@ namespace TUGraz.VectoCore.Tests.Models.SimulationComponent
 			var fileWriter = new FileOutputWriter("Coach_MinimalPowertrain_Coasting");
 			var modData = new ModalDataContainer("Coach_MinimalPowertrain_Coasting", fileWriter);
 			var vehicleContainer = new VehicleContainer(ExecutionMode.Engineering, modData);
+			var mockCycle = new MockDrivingCycle(vehicleContainer, null);
 
 			var driver = new Driver(vehicleContainer, driverData, new DefaultDriverStrategy());
 			var engine = new CombustionEngine(vehicleContainer, engineData);
@@ -126,6 +127,7 @@ namespace TUGraz.VectoCore.Tests.Models.SimulationComponent
 			var fileWriter = new FileOutputWriter("Coach_MinimalPowertrain_Coasting");
 			var modData = new ModalDataContainer("Coach_MinimalPowertrain_Coasting", fileWriter);
 			var vehicleContainer = new VehicleContainer(ExecutionMode.Engineering, modData);
+			var mockCycle = new MockDrivingCycle(vehicleContainer, null);
 
 			var driver = new Driver(vehicleContainer, driverData, new DefaultDriverStrategy());
 			var engine = new CombustionEngine(vehicleContainer, engineData);
@@ -386,7 +388,9 @@ namespace TUGraz.VectoCore.Tests.Models.SimulationComponent
 			};
 			return new VehicleData {
 				AxleConfiguration = AxleConfiguration.AxleConfig_4x2,
-				CrossWindCorrectionCurve = new CrosswindCorrectionCdxALookup(CrossWindCorrectionCurveReader.GetNoCorrectionCurve(3.2634.SI<SquareMeter>()), CrossWindCorrectionMode.NoCorrection),
+				CrossWindCorrectionCurve =
+					new CrosswindCorrectionCdxALookup(CrossWindCorrectionCurveReader.GetNoCorrectionCurve(3.2634.SI<SquareMeter>()),
+						CrossWindCorrectionMode.NoCorrection),
 				CurbWeight = 15700.SI<Kilogram>(),
 				CurbWeigthExtra = 0.SI<Kilogram>(),
 				Loading = loading,
