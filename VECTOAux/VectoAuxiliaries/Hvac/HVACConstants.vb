8e039c6ed6682c0e3096cd7ef17977635b006e2e
Markus Quaritsch <markus.quaritsch@tugraz.at>
2016-06-21 15:17:00 +0200

    use si class in hvac constants, make it serializable; extend tests for aaux

--- VECTOAux/VectoAuxiliaries/Hvac/HVACConstants.vb
+++ VECTOAux/VectoAuxiliaries/Hvac/HVACConstants.vb
@@ -8,37 +8,43 @@
 '   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 '
 ' See the LICENSE.txt for the specific language governing permissions and limitations.
+Imports Newtonsoft.Json
 Imports TUGraz.VectoCommon.Utils
 
 Namespace Hvac
 	Public Class HVACConstants
 		Implements IHVACConstants
 
-		Private _fuelDensity As KilogramPerCubicMeter
+		<JsonProperty("FuelDensity")> ReadOnly _fuelDensity As Double
+		<JsonProperty("DieselGCVJperGram")> ReadOnly _dieselGcvJperGram As Double = 44800
 
 		Public Sub New()
-			_fuelDensity = 835.SI(Of KilogramPerCubicMeter)()
+			_fuelDensity = 835 '.SI(Of KilogramPerCubicMeter)()
 		End Sub
 
 		Public Sub New(fuelDensitySingle As KilogramPerCubicMeter)
-			_fuelDensity = fuelDensitySingle
+			_fuelDensity = fuelDensitySingle.Value()
 		End Sub
 
+
+		<JsonIgnore>
 		Public ReadOnly Property DieselGCVJperGram As JoulePerKilogramm Implements IHVACConstants.DieselGCVJperGram
 			Get
-				Return 44800.SI().Joule.Per.Gramm.Cast(Of JoulePerKilogramm)()
+				Return _dieselGcvJperGram.SI().Joule.Per.Gramm.Cast(Of JoulePerKilogramm)()
 			End Get
 		End Property
 
+		<JsonIgnore()>
 		Public ReadOnly Property FuelDensity As KilogramPerCubicMeter Implements IHVACConstants.FuelDensity
 			Get
-				Return _fuelDensity
+				Return _fuelDensity.SI(Of KilogramPerCubicMeter)()
 			End Get
 		End Property
 
+		<JsonIgnore()>
 		Public ReadOnly Property FuelDensityAsGramPerLiter As Double Implements IHVACConstants.FuelDensityAsGramPerLiter
 			Get
-				Return _fuelDensity.Value() * 1000
+				Return _fuelDensity * 1000
 			End Get
 		End Property
 	End Class
