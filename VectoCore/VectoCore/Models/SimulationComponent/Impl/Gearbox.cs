562a302c4256fe6f7805ea2d2ab345fcaf9a5b5d
Markus Quaritsch <markus.quaritsch@tugraz.at>
2016-06-23 11:13:08 +0200

    gearbox: fix: inertia loss is 0 if angularvelocity is 0

--- VectoCore/VectoCore/Models/SimulationComponent/Impl/Gearbox.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Impl/Gearbox.cs
@@ -378,8 +378,9 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 					: 0.SI<NewtonMeter>();
 				inTorque += CurrentState.InertiaTorqueLossOut / ModelData.Gears[Gear].Ratio;
 
-				var inertiaTorqueLossIn =
-					Formulas.InertiaPower(outAngularVelocity, PreviousState.OutAngularVelocity, ModelData.Inertia, dt) /
+				var inertiaTorqueLossIn = avgOutAngularVelocity.IsEqual(0, 1e-9)
+					? 0.SI<NewtonMeter>()
+					: Formulas.InertiaPower(outAngularVelocity, PreviousState.OutAngularVelocity, ModelData.Inertia, dt) /
 					avgOutAngularVelocity / ModelData.Gears[Gear].Ratio;
 				var dryRunResponse = NextComponent.Request(absTime, dt, inTorque + inertiaTorqueLossIn, inAngularVelocity, true);
 				dryRunResponse.GearboxPowerRequest = outTorque * (PreviousState.OutAngularVelocity + outAngularVelocity) / 2.0;
