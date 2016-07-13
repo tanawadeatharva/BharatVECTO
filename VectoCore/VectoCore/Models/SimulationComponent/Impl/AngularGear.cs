43c77c384f2d64d5ff5da004d2f8efca61ee3511
Michael Krisper <michael.krisper@tugraz.at>
2016-07-13 13:41:24 +0200

    added MODFile fields for angular gear: P_angle_in, P_angle_loss

--- VectoCore/VectoCore/Models/SimulationComponent/Impl/AngularGear.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Impl/AngularGear.cs
@@ -1,14 +1,13 @@
 using TUGraz.VectoCommon.Models;
 using TUGraz.VectoCommon.Utils;
 using TUGraz.VectoCore.Models.Simulation;
+using TUGraz.VectoCore.Models.Simulation.Data;
 using TUGraz.VectoCore.Models.SimulationComponent.Data;
 using TUGraz.VectoCore.Models.SimulationComponent.Data.Gearbox;
 using TUGraz.VectoCore.OutputData;
 
 namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 {
-	// todo mk-2016-07-12: add dummy angular gear?
-
 	public class AngularGear : TransmissionComponent
 	{
 		public AngularGear(IVehicleContainer container, AngularGearData modelData) : base(container, modelData.AngularGear) {}
@@ -23,10 +22,10 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 
 		protected override void DoWriteModalResults(IModalDataContainer container)
 		{
-			//todo mk 2016-07-12: write angular gear modal results
 			var avgAngularVelocity = (PreviousState.InAngularVelocity + CurrentState.InAngularVelocity) / 2.0;
-			//container[ModalResultField.P_axle_loss] = (CurrentState.InTorque - CurrentState.OutTorque / ModelData.Ratio) *avgAngularVelocity;
-			//container[ModalResultField.P_axle_in] = CurrentState.InTorque * avgAngularVelocity;
+			container[ModalResultField.P_angle_loss] = (CurrentState.InTorque - CurrentState.OutTorque / ModelData.Ratio) *
+														avgAngularVelocity;
+			container[ModalResultField.P_angle_in] = CurrentState.InTorque * avgAngularVelocity;
 		}
 	}
 }
