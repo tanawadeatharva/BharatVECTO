bf2637dfa6dac5d7203f0361ab965bf9f8e22b4e
Michael Krisper <michael.krisper@tugraz.at>
2016-08-01 15:09:03 +0200

    faster whitespace removement in EnumHelper

--- VectoCommon/VectoCommon/Utils/EnumHelper.cs
+++ VectoCommon/VectoCommon/Utils/EnumHelper.cs
@@ -41,7 +41,7 @@ namespace TUGraz.VectoCommon.Utils
 	{
 		public static T ParseEnum<T>(this string s, bool ignoreCase = true)
 		{
-			return (T)Enum.Parse(typeof(T), Regex.Replace(s, @"\s+", ""), ignoreCase);
+			return (T)Enum.Parse(typeof(T), s.RemoveWhitespace(), ignoreCase);
 		}
 
 		public static IEnumerable<T> GetValues<T>()
