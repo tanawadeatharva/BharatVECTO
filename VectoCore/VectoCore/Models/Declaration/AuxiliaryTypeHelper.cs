faef01e5c355cab688e87c15af60289f92c8abbc
Michael Krisper <michael.krisper@tugraz.at>
2016-08-09 14:57:24 +0200

    added missing AuxiliaryTypeHelper

---
+++ VectoCore/VectoCore/Models/Declaration/AuxiliaryTypeHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TUGraz.VectoCommon.Models;
+using TUGraz.VectoCore.Configuration;
+
+namespace TUGraz.VectoCore.Models.Declaration
+{
+	public static class AuxiliaryTypeHelper
+	{
+		private static readonly Dictionary<AuxiliaryType, string> AuxToStr = new Dictionary<AuxiliaryType, string> {
+			{ AuxiliaryType.Fan, Constants.Auxiliaries.Names.Fan },
+			{ AuxiliaryType.SteeringPump, Constants.Auxiliaries.Names.SteeringPump },
+			{ AuxiliaryType.HVAC, Constants.Auxiliaries.Names.HeatingVentilationAirCondition },
+			{ AuxiliaryType.PneumaticSystem, Constants.Auxiliaries.Names.PneumaticSystem },
+			{ AuxiliaryType.ElectricSystem, Constants.Auxiliaries.Names.ElectricSystem },
+		};
+
+		private static readonly Dictionary<string, AuxiliaryType> StrToAux = AuxToStr.ToDictionary(kv => kv.Value,
+			kv => kv.Key);
+
+		public static AuxiliaryType Parse(string s)
+		{
+			AuxiliaryType aux;
+			if (StrToAux.TryGetValue(s, out aux))
+				return aux;
+
+			throw new ArgumentOutOfRangeException("s", s, "Could not parse auxiliary type string.");
+		}
+
+		public static string ToString(AuxiliaryType t)
+		{
+			return AuxToStr[t];
+		}
+	}
+}
