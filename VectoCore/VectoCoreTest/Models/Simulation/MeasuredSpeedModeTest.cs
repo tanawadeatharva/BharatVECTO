8c76ae3fa3ae17dd43ec2c210c6f306bf6bf86d2
Michael Krisper <michael.krisper@tugraz.at>
2016-08-09 10:26:12 +0200

    MeasuredSpeedMode Test: corrected error message

--- VectoCore/VectoCoreTest/Models/Simulation/MeasuredSpeedModeTest.cs
+++ VectoCore/VectoCoreTest/Models/Simulation/MeasuredSpeedModeTest.cs
@@ -29,26 +29,26 @@
 *   Martin Rexeis, rexeis@ivt.tugraz.at, IVT, Graz University of Technology
 */
 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using TUGraz.VectoCore.Utils;
-using System.Collections.Generic;
-using TUGraz.VectoCore.OutputData;
-using TUGraz.VectoCore.InputData.Reader;
-using TUGraz.VectoCore.OutputData.FileIO;
-using TUGraz.VectoCore.InputData.FileIO.JSON;
-using TUGraz.VectoCore.Models.Simulation.Data;
-using TUGraz.VectoCore.Models.Simulation.Impl;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TUGraz.VectoCommon.Exceptions;
 using TUGraz.VectoCommon.Models;
 using TUGraz.VectoCommon.Utils;
+using TUGraz.VectoCore.InputData.FileIO.JSON;
+using TUGraz.VectoCore.InputData.Reader;
 using TUGraz.VectoCore.Models.Declaration;
-using TUGraz.VectoCore.Models.SimulationComponent.Impl;
+using TUGraz.VectoCore.Models.Simulation.Data;
+using TUGraz.VectoCore.Models.Simulation.Impl;
 using TUGraz.VectoCore.Models.SimulationComponent.Data;
 using TUGraz.VectoCore.Models.SimulationComponent.Data.Engine;
 using TUGraz.VectoCore.Models.SimulationComponent.Data.Gearbox;
+using TUGraz.VectoCore.Models.SimulationComponent.Impl;
+using TUGraz.VectoCore.OutputData;
+using TUGraz.VectoCore.OutputData.FileIO;
 using TUGraz.VectoCore.Tests.Utils;
+using TUGraz.VectoCore.Utils;
 
 // ReSharper disable UnusedVariable
 
@@ -115,7 +115,7 @@ namespace TUGraz.VectoCore.Tests.Models.Simulation
 			inputData = @"<t>,<grad>,<Padd>,<n>,<gear>
 						  0  ,0";
 			AssertHelper.Exception<VectoException>(() => TestCycleRead(inputData, CycleType.MeasuredSpeedGear),
-				"Failed to read stream: Line 1: The number of values is not correct. Expected 5 Columns, Got 2 Columns");
+				"Line 1: The number of values is not correct. Expected 5 Columns, Got 2 Columns");
 		}
 
 		/// <summary>
@@ -172,7 +172,7 @@ namespace TUGraz.VectoCore.Tests.Models.Simulation
 			inputData = @"<t>,<v>,<grad>,<Padd>,<vair_res>,<vair_beta>,<Aux_Alt>
 						  0  ,0";
 			AssertHelper.Exception<VectoException>(() => TestCycleRead(inputData, CycleType.MeasuredSpeed),
-				"Failed to read stream: Line 1: The number of values is not correct. Expected 7 Columns, Got 2 Columns");
+				"Line 1: The number of values is not correct. Expected 7 Columns, Got 2 Columns");
 		}
 
 		private static void TestCycleRead(string inputData, CycleType cycleType, bool autoCycle = true,
