dac71c9911ad0f6d9de170e221e75e5b727784ea
Michael Krisper <michael.krisper@tugraz.at>
2016-08-10 16:51:14 +0200

    removed Once-Method (not needed)

--- VectoCommon/VectoCommon/Utils/EnumerableExtensionMethods.cs
+++ VectoCommon/VectoCommon/Utils/EnumerableExtensionMethods.cs
@@ -98,10 +98,6 @@ namespace TUGraz.VectoCommon.Utils
 		/// <summary>
 		/// Sums up the values of selector.
 		/// </summary>
-		/// <typeparam name="TU"></typeparam>
-		/// <typeparam name="TResult"></typeparam>
-		/// <param name="values"></param>
-		/// <param name="selector"></param>
 		/// <returns></returns>
 		public static TResult Sum<TU, TResult>(this IEnumerable<TU> values, Func<TU, TResult> selector)
 			where TResult : SIBase<TResult>
@@ -118,12 +114,6 @@ namespace TUGraz.VectoCommon.Utils
 		public static SI Sum(this IEnumerable<SI> values)
 		{
 			return values.DefaultIfEmpty().Aggregate((sum, current) => sum + current);
-		}
-
-		public static Func<bool> Once()
-		{
-			var once = 0;
-			return () => once++ == 0;
 		}
 
 		/// <summary>
