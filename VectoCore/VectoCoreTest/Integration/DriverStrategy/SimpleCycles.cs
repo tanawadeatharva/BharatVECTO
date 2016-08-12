d4dcd5e04fae141acd1125c1fecca36e4e2babaf
Markus Quaritsch <markus.quaritsch@tugraz.at>
2016-08-12 14:35:15 +0200

    adding testcases for at powertrain

--- VectoCore/VectoCoreTest/Integration/DriverStrategy/SimpleCycles.cs
+++ VectoCore/VectoCoreTest/Integration/DriverStrategy/SimpleCycles.cs
@@ -194,31 +194,44 @@ namespace TUGraz.VectoCore.Tests.Integration.DriverStrategy
 
 		[Category("ComparisonV2"),
 		TestCase(SimpleDrivingCycles.CycleDrive_80_Increasing_Slope,
-			"40t_Long_Haul_Truck_Cycle_Drive_80_Increasing_Slope.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleDrive_80_Increasing_Slope"),
+			"40t_Long_Haul_Truck_Cycle_Drive_80_Increasing_Slope.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleDrive_80_Increasing_Slope"),
 		TestCase(SimpleDrivingCycles.CycleDrive_50_Increasing_Slope,
-			"40t_Long_Haul_Truck_Cycle_Drive_50_Increasing_Slope.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleDrive_50_Increasing_Slope"),
+			"40t_Long_Haul_Truck_Cycle_Drive_50_Increasing_Slope.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleDrive_50_Increasing_Slope"),
 		TestCase(SimpleDrivingCycles.CycleDrive_30_Increasing_Slope,
-			"40t_Long_Haul_Truck_Cycle_Drive_30_Increasing_Slope.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleDrive_30_Increasing_Slope"),
+			"40t_Long_Haul_Truck_Cycle_Drive_30_Increasing_Slope.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleDrive_30_Increasing_Slope"),
 		TestCase(SimpleDrivingCycles.CycleDrive_80_Decreasing_Slope,
-			"40t_Long_Haul_Truck_Cycle_Drive_80_Decreasing_Slope.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleDrive_80_Decreasing_Slope"),
+			"40t_Long_Haul_Truck_Cycle_Drive_80_Decreasing_Slope.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleDrive_80_Decreasing_Slope"),
 		TestCase(SimpleDrivingCycles.CycleDrive_50_Decreasing_Slope,
-			"40t_Long_Haul_Truck_Cycle_Drive_50_Decreasing_Slope.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleDrive_50_Decreasing_Slope"),
+			"40t_Long_Haul_Truck_Cycle_Drive_50_Decreasing_Slope.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleDrive_50_Decreasing_Slope"),
 		TestCase(SimpleDrivingCycles.CycleDrive_30_Decreasing_Slope,
-			"40t_Long_Haul_Truck_Cycle_Drive_30_Decreasing_Slope.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleDrive_30_Decreasing_Slope"),
+			"40t_Long_Haul_Truck_Cycle_Drive_30_Decreasing_Slope.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleDrive_30_Decreasing_Slope"),
 		TestCase(SimpleDrivingCycles.CycleDrive_80_Dec_Increasing_Slope,
-			"40t_Long_Haul_Truck_Cycle_Drive_80_Dec_Increasing_Slope.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleDrive_80_Dec_Increasing_Slope"),
+			"40t_Long_Haul_Truck_Cycle_Drive_80_Dec_Increasing_Slope.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleDrive_80_Dec_Increasing_Slope"),
 		TestCase(SimpleDrivingCycles.CycleDrive_50_Dec_Increasing_Slope,
-			"40t_Long_Haul_Truck_Cycle_Drive_50_Dec_Increasing_Slope.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleDrive_50_Dec_Increasing_Slope"),
+			"40t_Long_Haul_Truck_Cycle_Drive_50_Dec_Increasing_Slope.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleDrive_50_Dec_Increasing_Slope"),
 		TestCase(SimpleDrivingCycles.CycleDrive_30_Dec_Increasing_Slope,
-			"40t_Long_Haul_Truck_Cycle_Drive_30_Dec_Increasing_Slope.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleDrive_30_Dec_Increasing_Slope"),
+			"40t_Long_Haul_Truck_Cycle_Drive_30_Dec_Increasing_Slope.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleDrive_30_Dec_Increasing_Slope"),
 		TestCase(SimpleDrivingCycles.CycleDecelerateWhileBrake_80_0_level,
-			"40t_Long_Haul_Truck_Cycle_DecelerateWhileBrake_80_0_level.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleDecelerateWhileBrake_80_0_level"),
+			"40t_Long_Haul_Truck_Cycle_DecelerateWhileBrake_80_0_level.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleDecelerateWhileBrake_80_0_level"),
 		TestCase(SimpleDrivingCycles.CycleAccelerateWhileBrake_80_0_level,
-			"40t_Long_Haul_Truck_Cycle_AccelerateWhileBrake_80_0_level.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleAccelerateWhileBrake_80_0_level"),
+			"40t_Long_Haul_Truck_Cycle_AccelerateWhileBrake_80_0_level.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleAccelerateWhileBrake_80_0_level"),
 		TestCase(SimpleDrivingCycles.CycleAccelerateAtBrake_80_0_level,
-			"40t_Long_Haul_Truck_Cycle_AccelerateAtBrake_80_0_level.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleAccelerateAtBrake_80_0_level"),
+			"40t_Long_Haul_Truck_Cycle_AccelerateAtBrake_80_0_level.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleAccelerateAtBrake_80_0_level"),
 		TestCase(SimpleDrivingCycles.CycleAccelerateBeforeBrake_80_0_level,
-			"40t_Long_Haul_Truck_Cycle_AccelerateBeforeBrake_80_0_level.vmod", GearboxType.AMT, TestName = "TruckSpecial CycleAccelerateBeforeBrake_80_0_level"),
+			"40t_Long_Haul_Truck_Cycle_AccelerateBeforeBrake_80_0_level.vmod", GearboxType.AMT,
+			TestName = "TruckSpecial CycleAccelerateBeforeBrake_80_0_level"),
 		TestCase(SimpleDrivingCycles.CycleDrive_stop_85_stop_85_level, "24t Truck_Cycle_Drive_stop_85_stop_85_level.vmod",
 			GearboxType.AMT, TestName = "TruckSpecial CycleDrive_stop_85_stop_85_level"),
 		TestCase(SimpleDrivingCycles.CycleDrive_SlopeChangeBeforeStop, "Truck_DriverStrategy_SlopeChangeBeforeStop.vmod",
@@ -293,9 +306,8 @@ namespace TUGraz.VectoCore.Tests.Integration.DriverStrategy
 			var cycle = string.Format(CultureInfo.InvariantCulture, "0, {0}, {1}, {2}\n1000, {3}, {4}, {5}", v1, slope,
 				v1.IsEqual(0) ? 2 : 0, v2, slope, v2.IsEqual(0) ? 2 : 0);
 
-			Coach_Special(cycle,
-				string.Format(CultureInfo.InvariantCulture, "24t Coach_Cycle_Accelerate_{0}_{1}_{2}.vmod",
-					v1, v2, GetSlopeString(slope)));
+			Coach_Special(cycle, string.Format(CultureInfo.InvariantCulture, "24t Coach_Cycle_Accelerate_{0}_{1}_{2}.vmod",
+				v1, v2, GetSlopeString(slope)));
 		}
 
 		[Category("ComparisonV2"),
@@ -430,6 +442,143 @@ namespace TUGraz.VectoCore.Tests.Integration.DriverStrategy
 
 			GraphWriter.Write(modFileName,
 				@"..\..\TestData\Integration\DriverStrategy\Vecto2.2\Coach_Overspeed\" + modFileName);
+		}
+
+		// ####################################################
+
+		[Category("AT Gearbox"),
+		TestCase(0, 20, -5),
+		TestCase(0, 20, 0),
+		TestCase(0, 85, -15),
+		TestCase(0, 85, -25),
+		TestCase(0, 85, -5),
+		TestCase(0, 85, 0),
+		TestCase(0, 85, 1),
+		TestCase(0, 85, 10),
+		TestCase(0, 85, 2),
+		TestCase(0, 85, 25),
+		TestCase(0, 85, 5),
+		TestCase(20, 22, 5),
+		TestCase(20, 60, -15),
+		TestCase(20, 60, -25),
+		TestCase(20, 60, -5),
+		TestCase(20, 60, 0),
+		TestCase(20, 60, 15),
+		TestCase(20, 60, 25),
+		TestCase(20, 60, 5),
+		]
+		public void AT_Gearbox_Accelerate(double v1, double v2, double slope)
+		{
+			var cycle = string.Format(CultureInfo.InvariantCulture, "0, {0}, {1}, {2}\n1000, {3}, {4}, {5}", v1, slope,
+				v1.IsEqual(0) ? 2 : 0, v2, slope, v2.IsEqual(0) ? 2 : 0);
+
+			AT_Gearbox_Special(cycle,
+				string.Format(CultureInfo.InvariantCulture, "AT-Gbx Accelerate_{0}_{1}_{2}.vmod",
+					v1, v2, GetSlopeString(slope)));
+		}
+
+		[Category("AT Gearbox"),
+		TestCase(22, 20, -5),
+		TestCase(45, 0, -5),
+		TestCase(45, 0, 0),
+		TestCase(45, 0, 5),
+		TestCase(60, 20, -15),
+		TestCase(60, 20, -25),
+		TestCase(60, 20, -5),
+		TestCase(60, 20, 0),
+		TestCase(60, 20, 15),
+		TestCase(60, 20, 5),
+		TestCase(80, 0, -15),
+		TestCase(80, 0, -25),
+		TestCase(80, 0, -5),
+		TestCase(80, 0, 0),
+		TestCase(80, 0, 20),
+		TestCase(80, 0, 15),
+		TestCase(80, 0, 3),
+		TestCase(80, 0, 5),
+		]
+		public void AT_Gearbox_Decelerate(double v1, double v2, double slope)
+		{
+			var cycle = string.Format(CultureInfo.InvariantCulture, "0, {0}, {1}, {2}\n1000, {3}, {4}, {5}", v1, slope,
+				v1.IsEqual(0) ? 2 : 0, v2, slope, v2.IsEqual(0) ? 2 : 0);
+
+			AT_Gearbox_Special(cycle,
+				string.Format(CultureInfo.InvariantCulture, "AT-Gbx Decelerate_{0}_{1}_{2}.vmod",
+					v1, v2, GetSlopeString(slope)));
+		}
+
+		[Category("AT Gearbox"),
+		TestCase(10, 10, -15),
+		TestCase(10, 10, -25),
+		TestCase(10, 10, -5),
+		TestCase(10, 10, 0),
+		TestCase(10, 10, 15),
+		TestCase(10, 10, 25),
+		TestCase(10, 10, 5),
+		TestCase(20, 20, -15),
+		TestCase(30, 30, -15),
+		TestCase(50, 50, -15),
+		TestCase(80, 80, -15),
+		TestCase(80, 80, -5),
+		TestCase(80, 80, 0),
+		TestCase(80, 80, 15),
+		TestCase(80, 80, 5),
+		]
+		public void AT_Gearbox_Drive(double v1, double v2, double slope)
+		{
+			var cycle = string.Format(CultureInfo.InvariantCulture, "0, {0}, {1}, {2}\n1000, {3}, {4}, {5}", v1, slope,
+				v1.IsEqual(0) ? 2 : 0, v2, slope, v2.IsEqual(0) ? 2 : 0);
+
+			AT_Gearbox_Special(cycle, string.Format(CultureInfo.InvariantCulture, "AT-Gbx Drive_{0}_{1}_{2}.vmod",
+				v1, v2, GetSlopeString(slope)));
+		}
+
+		[Category("AT Gearbox"),
+		TestCase(SimpleDrivingCycles.CycleDrive_80_Increasing_Slope,
+			"AT-Gbx Drive_80_Increasing_Slope.vmod", TestName = "AT-Gearbox CycleDrive_80_Increasing_Slope"),
+		TestCase(SimpleDrivingCycles.CycleDrive_50_Increasing_Slope,
+			"AT-Gbx Drive_50_Increasing_Slope.vmod", TestName = "AT-Gearbox CycleDrive_50_Increasing_Slope"),
+		TestCase(SimpleDrivingCycles.CycleDrive_30_Increasing_Slope,
+			"AT-Gbx Drive_30_Increasing_Slope.vmod", TestName = "AT-Gearbox CycleDrive_30_Increasing_Slope"),
+		TestCase(SimpleDrivingCycles.CycleDrive_80_Decreasing_Slope,
+			"AT-Gbx Drive_80_Decreasing_Slope.vmod", TestName = "AT-Gearbox CycleDrive_80_Decreasing_Slope"),
+		TestCase(SimpleDrivingCycles.CycleDrive_50_Decreasing_Slope,
+			"AT-Gbx Drive_50_Decreasing_Slope.vmod", TestName = "AT-Gearbox CycleDrive_50_Decreasing_Slope"),
+		TestCase(SimpleDrivingCycles.CycleDrive_30_Decreasing_Slope,
+			"AT-Gbx Drive_30_Decreasing_Slope.vmod", TestName = "AT-Gearbox CycleDrive_30_Decreasing_Slope"),
+		TestCase(SimpleDrivingCycles.CycleDrive_80_Dec_Increasing_Slope,
+			"AT-Gbx Drive_80_Dec_Increasing_Slope.vmod", TestName = "AT-Gearbox CycleDrive_80_Dec_Increasing_Slope"),
+		TestCase(SimpleDrivingCycles.CycleDrive_50_Dec_Increasing_Slope,
+			"AT-Gbx Drive_50_Dec_Increasing_Slope.vmod", TestName = "AT-Gearbox CycleDrive_50_Dec_Increasing_Slope"),
+		TestCase(SimpleDrivingCycles.CycleDrive_30_Dec_Increasing_Slope,
+			"AT-Gbx Drive_30_Dec_Increasing_Slope.vmod", TestName = "AT-GearboxCycleDrive_30_Dec_Increasing_Slope "),
+		TestCase(SimpleDrivingCycles.CycleDecelerateWhileBrake_80_0_level,
+			"AT-Gbx DecelerateWhileBrake_80_0_level.vmod",
+			TestName = "AT-Gearbox CycleDecelerateWhileBrake_80_0_level"),
+		TestCase(SimpleDrivingCycles.CycleAccelerateWhileBrake_80_0_level,
+			"AT-Gbx AccelerateWhileBrake_80_0_level.vmod",
+			TestName = "AT-Gearbox CycleAccelerateWhileBrake_80_0_level"),
+		TestCase(SimpleDrivingCycles.CycleAccelerateAtBrake_80_0_level,
+			"AT-Gbx AccelerateAtBrake_80_0_level.vmod", TestName = "AT-Gearbox CycleAccelerateAtBrake_80_0_level"),
+		TestCase(SimpleDrivingCycles.CycleAccelerateBeforeBrake_80_0_level,
+			"AT-Gbx AccelerateBeforeBrake_80_0_level.vmod",
+			TestName = "AT-Gearbox CycleAccelerateBeforeBrake_80_0_level"),
+		TestCase(SimpleDrivingCycles.CycleDrive_stop_85_stop_85_level, "AT-Gbx Drive_stop_85_stop_85_level.vmod",
+			TestName = "AT-Gearbox CycleDrive_stop_85_stop_85_level"),
+		TestCase(SimpleDrivingCycles.CycleDrive_SlopeChangeBeforeStop, "24t Coach_DriverStrategy_SlopeChangeBeforeStop.vmod",
+			TestName = "AT-Gearbox CycleDrive_SlopeChangeBeforeStop"),
+		TestCase(SimpleDrivingCycles.CycleDriver_FrequentSlopChange, "24t Coach_DriverStrategy_SlopeChangeBeforeStop.vmod",
+			TestName = "AT-Gearbox CycleDriver_FrequentSlopChange"),
+		]
+		public void AT_Gearbox_Special(string cycleData, string modFileName)
+		{
+			var cycle = SimpleDrivingCycles.CreateCycleData(cycleData);
+			var run = ATPowerTrain.CreateEngineeringRun(cycle, modFileName);
+
+			run.Run();
+			Assert.IsTrue(run.FinishedWithoutErrors);
+
+			//GraphWriter.Write(modFileName, @"..\..\TestData\Integration\DriverStrategy\Vecto2.2\Coach\" + modFileName);
 		}
 	}
 }
