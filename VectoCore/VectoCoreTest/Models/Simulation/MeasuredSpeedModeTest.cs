dc80f57e119f0b0ff6ac6478aeaec58ce5c128a8
Markus Quaritsch <markus.quaritsch@tugraz.at>
2016-08-18 16:18:41 +0200

    use mockModalDataContainer for powertrain builder (can't be null anymore)

--- VectoCore/VectoCoreTest/Models/Simulation/MeasuredSpeedModeTest.cs
+++ VectoCore/VectoCoreTest/Models/Simulation/MeasuredSpeedModeTest.cs
@@ -248,7 +248,7 @@ namespace TUGraz.VectoCore.Tests.Models.Simulation
 			};
 
 			// call builder (actual test)
-			var builder = new PowertrainBuilder(null);
+			var builder = new PowertrainBuilder(new MockModalDataContainer());
 			builder.Build(data);
 		}
 
