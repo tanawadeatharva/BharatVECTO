736267f440bfe83dfba31a60e3a94862776d2011
Michael Krisper <michael.krisper@tugraz.at>
2016-10-05 13:48:12 +0200

    updated PTO generic vehicle, corrected error with PTOCycleFileBrowser

--- VECTO/GUI/MainForm.vb
+++ VECTO/GUI/MainForm.vb
@@ -52,7 +52,7 @@ Imports TUGraz.VectoCore.Utils
 ''' </summary>
 ''' <remarks></remarks>
 
-Public Class MainForm
+	Public Class MainForm
 	Private _jobListView As FileListView
 	Private _cycleListView As FileListView
 
@@ -70,7 +70,7 @@ Public Class MainForm
 
 #Region "SLEEP Control - Prevent sleep while VECTO is running"
 
-	Private Declare Function SetThreadExecutionState Lib "kernel32" (esFlags As Long) As Long
+	Private Declare Function SetThreadExecutionState Lib "kernel32"(esFlags As Long) As Long
 
 	Private Shared Sub AllowSleepOff()
 #If Not PLATFORM = "x86" Then
@@ -121,6 +121,7 @@ Public Class MainForm
 		RetarderLossMapFileBrowser = New FileBrowser("vrlm")
 		TransmissionLossMapFileBrowser = New FileBrowser("vtlm")
 		PtoLossMapFileBrowser = New FileBrowser("vptol")
+		PTODrivingCycleFileBrowser = New FileBrowser("vptoc")
 		TorqueConverterFileBrowser = New FileBrowser("vtcc")
 		TorqueConverterShiftPolygonFileBrowser = New FileBrowser("vgbs")
 		CrossWindCorrectionFileBrowser = New FileBrowser("vcdx")
@@ -147,6 +148,7 @@ Public Class MainForm
 		RetarderLossMapFileBrowser.Extensions = New String() {"vrlm"}
 		TransmissionLossMapFileBrowser.Extensions = New String() {"vtlm"}
 		PtoLossMapFileBrowser.Extensions = New String() {"vptol"}
+		PTODrivingCycleFileBrowser.Extensions = New String() {"vptoc"}
 		TorqueConverterFileBrowser.Extensions = New String() {"vtcc"}
 		TorqueConverterShiftPolygonFileBrowser.Extensions = New String() {"vgbs"}
 		CrossWindCorrectionFileBrowser.Extensions = New String() {"vcdv", "vcdb"}
@@ -170,6 +172,7 @@ Public Class MainForm
 		RetarderLossMapFileBrowser.Close()
 		TransmissionLossMapFileBrowser.Close()
 		PtoLossMapFileBrowser.Close()
+		PTODrivingCycleFileBrowser.Close()
 		TorqueConverterFileBrowser.Close()
 		TorqueConverterShiftPolygonFileBrowser.Close()
 		CrossWindCorrectionFileBrowser.Close()
@@ -229,8 +232,8 @@ Public Class MainForm
 		LoadOptions()
 
 		'Resize columns ... after Loading the @file-lists
-		LvGEN.Columns(1).Width = -2
-		LvMsg.Columns(2).Width = -2
+		LvGEN.Columns(1).Width = - 2
+		LvMsg.Columns(2).Width = - 2
 
 		'Initialize BackgroundWorker
 
@@ -514,7 +517,7 @@ Public Class MainForm
 
 		lastindx = LvGEN.SelectedIndices(LvGEN.SelectedItems.Count - 1)
 
-		For i = UBound(selIx) To 0 Step -1
+		For i = UBound(selIx) To 0 Step - 1
 			LvGEN.Items.RemoveAt(selIx(i))
 		Next
 
@@ -574,7 +577,7 @@ Public Class MainForm
 		Dim p As Integer
 		Dim f As Integer
 		Dim fList As String()
-		Dim fListDim As Integer = -1
+		Dim fListDim As Integer = - 1
 		Dim listViewItem As ListViewItem
 
 		'If VECTO runs: Cancel operation (because Mode-change during calculation is not very clever)
@@ -629,7 +632,7 @@ Public Class MainForm
 			listViewItem.Selected = True
 			LvGEN.Items.Add(listViewItem)
 			listViewItem.EnsureVisible()
-lbFound:
+			lbFound:
 		Next
 
 		LvGEN.EndUpdate()
@@ -926,7 +929,7 @@ lbFound:
 			Status("Launching VECTO ...")
 			JobFileList.Clear()
 			JobFileList.AddRange(
-				From listViewItem As ListViewItem In LvGEN.CheckedItems.Cast(Of ListViewItem)()
+				From listViewItem As ListViewItem In LvGEN.CheckedItems.Cast (Of ListViewItem)()
 									Select fFileRepl = FileRepl(listViewItem.SubItems(0).Text))
 
 			SetOptions()
@@ -967,9 +970,9 @@ lbFound:
 			mode = ExecutionMode.Declaration
 		Else
 			mode = ExecutionMode.Engineering
-			Physics.FuelDensity = Cfg.FuelDens.SI(Of KilogramPerCubicMeter)() _
+			Physics.FuelDensity = Cfg.FuelDens.SI (Of KilogramPerCubicMeter)() _
 			'New SI(Cfg.FuelDens).Kilo.Gramm.Per.Cubic.Dezi.Meter.Cast(Of KilogramPerCubicMeter)()
-			Physics.AirDensity = Cfg.AirDensity.SI(Of KilogramPerCubicMeter)() _
+			Physics.AirDensity = Cfg.AirDensity.SI (Of KilogramPerCubicMeter)() _
 			'New SI(Cfg.AirDensity).Kilo.Gramm.Per.Cubic.Meter.Cast(Of KilogramPerCubicMeter)()
 			Physics.CO2PerFuelWeight = Cfg.Co2PerFc
 		End If
@@ -1032,10 +1035,10 @@ lbFound:
 			Dim sumProgress As Double = progress.Sum(Function(pair) pair.Value.Progress)
 			Dim duration As Double = (DateTime.Now() - start).TotalSeconds
 
-			sender.ReportProgress(Convert.ToInt32((sumProgress * 100.0) / progress.Count),
+			sender.ReportProgress(Convert.ToInt32((sumProgress*100.0)/progress.Count),
 								New VectoProgress With {.Target = "Status",
 									.Message = _
-									String.Format("Duration: {0:0}s, Current Progress: {1:P} ({2})", duration, sumProgress / progress.Count,
+									String.Format("Duration: {0:0}s, Current Progress: {1:P} ({2})", duration, sumProgress/progress.Count,
 												String.Join(", ", progress.Select(Function(pair) String.Format("{0,4:P}", pair.Value.Progress))))})
 
 			Dim justFinished As Dictionary(Of Integer, JobContainer.ProgressEntry) =
@@ -1059,7 +1062,7 @@ lbFound:
 									.Message = String.Format("{0,-60} {1,8:P} {2,10:F2}s - {3}",
 															String.Format("{0} {1} {2}", progressEntry.Value.RunName, progressEntry.Value.CycleName,
 																		progressEntry.Value.RunSuffix),
-															progressEntry.Value.Progress, progressEntry.Value.ExecTime / 1000.0,
+															progressEntry.Value.Progress, progressEntry.Value.ExecTime/1000.0,
 															IIf(progressEntry.Value.Success, "Success", "Aborted"))})
 			If (Not progressEntry.Value.Success) Then
 				sender.ReportProgress(100,
@@ -1353,9 +1356,9 @@ lbFound:
 
 			ToolStripProgBarJob.Value = .ProgJobInt
 
-			If .ProgOverallStartInt > -1 Then
+			If .ProgOverallStartInt > - 1 Then
 				ToolStripProgBarOverall.Value =
-					CInt(.ProgOverallStartInt + (.PgroOverallEndInt - .ProgOverallStartInt) * .ProgJobInt / 100)
+					CInt(.ProgOverallStartInt + (.PgroOverallEndInt - .ProgOverallStartInt)*.ProgJobInt/100)
 			End If
 
 		End With
@@ -1672,7 +1675,7 @@ lbFound:
 					_mainForm.LvMsg.Items.Insert(RowLim - 4, Space(ColLim - 30) & "         " & Space(10) & "*|       |*")
 			End Select
 			Exit Sub
-LbRace:
+			LbRace:
 
 			_pRbAlt = Not _pRbAlt
 
@@ -1700,17 +1703,17 @@ LbRace:
 					Abort()
 					Exit Sub
 				End If
-				_scr += 5 * _diffLvl
+				_scr += 5*_diffLvl
 			End If
 
 			_scr += _diffLvl
 			_diffC += 1
 
 			'Erhöhe Schwierigkeitsgrad
-			If _diffC = (_diffLvl + 3) * 4 Then
+			If _diffC = (_diffLvl + 3)*4 Then
 				_diffC = 0
 				_diffLvl += 1
-				If _diffLvl > 2 And _diffLvl < 7 Then _mainForm.TmProgSec.Interval = 300 - (_diffLvl) * 30
+				If _diffLvl > 2 And _diffLvl < 7 Then _mainForm.TmProgSec.Interval = 300 - (_diffLvl)*30
 				_scr += 100
 				Select Case _diffLvl
 					Case 3
@@ -1794,10 +1797,10 @@ LbRace:
 			_ctrls(RowLim + 1) = 0
 			_ctrlC += 1
 			If _ctrlC < _ctrlCl Then Exit Sub
-			Select Case CInt(Int((_ctrlRnd * Rnd()) + 1))
+			Select Case CInt(Int((_ctrlRnd*Rnd()) + 1))
 				Case 1, 2
 					_ctrlC = 0
-					x = CInt(Int((7 * Rnd()) + 1))
+					x = CInt(Int((7*Rnd()) + 1))
 					_ctrls(RowLim + 1) = x
 			End Select
 		End Sub
@@ -1840,7 +1843,7 @@ LbRace:
 				s = s.Insert(_ctrls(RowLim + 1) + 1, "X")
 			End If
 			Select Case _xPanel - _pnls(RowLim)
-				Case -1
+				Case - 1
 					s = Replace(s, "|", "\")
 				Case 1
 					s = Replace(s, "|", "/")
@@ -1852,15 +1855,15 @@ LbRace:
 			_pnDirC += 1
 			If _pnDirC < _pnDirCl Then GoTo Lb1
 			_pnDirC = 0
-			Select Case CInt(Int((_pnDirRnd * Rnd()) + 1))
+			Select Case CInt(Int((_pnDirRnd*Rnd()) + 1))
 				Case 1
 					_pnDir = 1
 				Case 2
-					_pnDir = -1
+					_pnDir = - 1
 				Case Else
 					_pnDir = 0
 			End Select
-Lb1:
+			Lb1:
 			_xPanel += _pnDir
 			If _xPanel > ColLim Then
 				_xPanel = ColLim
@@ -1911,7 +1914,7 @@ Lb1:
 			Dim builder As StringBuilder = New StringBuilder()
 			For Each selectedItem As ListViewItem In LvMsg.SelectedItems
 				builder.AppendLine(String.Join(", ",
-												selectedItem.SubItems.Cast(Of ListViewItem.ListViewSubItem).Select(
+												selectedItem.SubItems.Cast (Of ListViewItem.ListViewSubItem).Select(
 													Function(item) item.Text)))
 			Next
 			Clipboard.SetText(builder.ToString())
