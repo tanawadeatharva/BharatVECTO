a076f3996b299689d62d40e33f307c23717f7d93
Michael Krisper <michael.krisper@tugraz.at>
2016-11-14 13:53:33 +0100

    Better error messages in case when Declaration/Engineering Mode are not fitting or data is missing

--- VECTO/Input Files/VectoJob.vb
+++ VECTO/Input Files/VectoJob.vb
@@ -390,7 +390,10 @@ Public Class VectoJob
 						New ValidationResult("Vecto Job Configuration is invalid. ", result.Select(Function(r) r.ErrorMessage).ToList())
 				End If
 				Dim dataFactory As EngineeringModeVectoRunDataFactory = New EngineeringModeVectoRunDataFactory(vectoJob)
-				jobData = dataFactory.NextRun().First()
+				Dim jobDataIterator As IEnumerable(Of VectoRunData) = dataFactory.NextRun()
+				If Not jobDataIterator.Any() Then
+					Return New ValidationResult("No cycles selected in Vecto Job.", result.Select(Function(r) r.ErrorMessage).ToList())
+				End If
 			End If
 
 
