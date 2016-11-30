58bd7aaa384128995ec653078c8c213e98f94507
Michael Krisper <michael.krisper@tugraz.at>
2016-11-30 09:46:53 +0100

    ATGearbox: Corrected Bug with TorqueConverter InAngularSpeed

--- VectoCore/VectoCore/Models/SimulationComponent/Impl/ATGearbox.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Impl/ATGearbox.cs
@@ -288,7 +288,7 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 			retval.ClutchPowerRequest = 0.SI<Watt>();
 			CurrentState.SetState(0.SI<NewtonMeter>(), 0.SI<PerSecond>(), outTorque, outAngularVelocity);
 
-			TorqueConverter.Locked(CurrentState.InTorque, DataBus.EngineIdleSpeed);
+			TorqueConverter.Locked(CurrentState.InTorque, retval.EngineSpeed);
 
 			CurrentState.Gear = 1;
 			CurrentState.TorqueConverterLocked = !ModelData.Gears[Gear].HasTorqueConverter;
