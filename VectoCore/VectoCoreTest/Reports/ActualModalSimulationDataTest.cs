2f43dcbf45129998e698c0d83ed8d580911a1d5e
Markus Quaritsch <markus.quaritsch@tugraz.at>
2016-12-13 13:44:10 +0100

    adding filter to write output data according to simulation (no averaging of speed, etc)

---
+++ VectoCore/VectoCoreTest/Reports/ActualModalSimulationDataTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using TUGraz.VectoCommon.Models;
+using TUGraz.VectoCore.Configuration;
+using TUGraz.VectoCore.InputData.FileIO.JSON;
+using TUGraz.VectoCore.Models.Simulation.Impl;
+using TUGraz.VectoCore.OutputData;
+using TUGraz.VectoCore.OutputData.FileIO;
+using TUGraz.VectoCore.Tests.Integration;
+using TUGraz.VectoCore.Tests.Utils;
+
+namespace TUGraz.VectoCore.Tests.Reports
+{
+	[TestFixture]
+	public class ActualModalSimulationDataTest
+	{
+		[Test]
+		public void TestWriteActualModData()
+		{
+			const string jobFile = @"TestData\Integration\DeclarationMode\40t Truck\40t_Long_Haul_Truck.vecto";
+			var fileWriter = new FileOutputWriter(jobFile);
+			var sumData = new SummaryDataContainer(fileWriter);
+			var jobContainer = new JobContainer(sumData);
+
+			var inputData = JSONInputDataFactory.ReadJsonJob(jobFile);
+			var factory = new SimulatorFactory(ExecutionMode.Declaration, inputData, fileWriter);
+			factory.WriteModalResults = true;
+			factory.ActualModalData = true;
+
+			jobContainer.AddRuns(factory);
+			jobContainer.Execute();
+			//jobContainer.Runs[4].Run.Run();
+
+			jobContainer.WaitFinished();
+
+			Assert.IsTrue(jobContainer.AllCompleted);
+		}
+	}
+}
