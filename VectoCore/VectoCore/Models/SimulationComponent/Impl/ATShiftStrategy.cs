2c7f63ce8a3baf8a102e0dddb04e148b3cfbcd22
Markus Quaritsch <markus.quaritsch@tugraz.at>
2017-03-06 16:50:49 +0100

    at shift strategy: upshift 1C -> 2C only if gbx speed > 0

--- VectoCore/VectoCore/Models/SimulationComponent/Impl/ATShiftStrategy.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Impl/ATShiftStrategy.cs
@@ -225,7 +225,7 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 
 			// UPSHIFT - Special rule for 1C -> 2C
 			if (!_gearbox.TorqueConverterLocked && ModelData.Gears.ContainsKey(gear + 1) &&
-				ModelData.Gears[gear + 1].HasTorqueConverter) {
+				ModelData.Gears[gear + 1].HasTorqueConverter && outAngularVelocity.IsGreater(0)) {
 				// C -> C+1
 				var nextGear = ModelData.Gears[gear + 1];
 				var gearRatio = nextGear.TorqueConverterRatio / currentGear.TorqueConverterRatio;
