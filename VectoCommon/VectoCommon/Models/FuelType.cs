add12b5f436c7a92902522d74f295560f90a6f16
Markus Quaritsch <markus.quaritsch@tugraz.at>
2017-05-23 16:35:51 +0200

    adding missing file

---
+++ VectoCommon/VectoCommon/Models/FuelType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TUGraz.VectoCommon.Models
+{
+	public enum FuelType
+	{
+		// ReSharper disable InconsistentNaming
+		DieselCI,
+		EthanolCI,
+		PetrolPI,
+		EthanolPI,
+		LPG,
+		NG
+		// ReSharper restore InconsistentNaming
+	}
+
+	public static class FuelTypeHelper
+	{
+		public static string GetLabel(this FuelType ftype)
+		{
+			switch (ftype) {
+				case FuelType.DieselCI:
+					return "Diesel CI";
+				case FuelType.EthanolCI:
+					return "Ethanol CI";
+				case FuelType.PetrolPI:
+					return "Petrol PI";
+				case FuelType.EthanolPI:
+					return "Ethanol PI";
+				case FuelType.LPG:
+					return "LPG";
+				case FuelType.NG:
+					return "NG";
+				default:
+					throw new ArgumentOutOfRangeException("fuel type", ftype, null);
+			}
+		}
+
+		public static string ToXMLFormat(this FuelType ftype)
+		{
+			return ftype.GetLabel();
+		}
+	}
+}
