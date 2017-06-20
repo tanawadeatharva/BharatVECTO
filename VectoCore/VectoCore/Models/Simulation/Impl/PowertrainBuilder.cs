db6e25ecc74f5f211e1cf32354ca4a8f1fcb6105
Michael Krisper <michael.krisper@tugraz.at>
2017-06-20 17:01:39 +0200

    PTO corrected bug when PTO cycle begins with torque != 0

--- VectoCore/VectoCore/Models/Simulation/Impl/PowertrainBuilder.cs
+++ VectoCore/VectoCore/Models/Simulation/Impl/PowertrainBuilder.cs
@@ -111,13 +111,13 @@ namespace TUGraz.VectoCore.Models.Simulation.Impl
 
 			// PWheelCycle --> AxleGear --> Clutch --> Engine <-- Aux
 			var powertrain = new PWheelCycle(container, data.Cycle, data.AxleGearData.AxleGear.Ratio, data.VehicleData,
-				gearbox.ModelData.Gears.ToDictionary(g => g.Key, g => g.Value.Ratio))
+					gearbox.ModelData.Gears.ToDictionary(g => g.Key, g => g.Value.Ratio))
 				.AddComponent(new AxleGear(container, data.AxleGearData))
 				.AddComponent(data.AngledriveData != null ? new Angledrive(container, data.AngledriveData) : null)
 				.AddComponent(gearbox, data.Retarder, container)
 				.AddComponent(new Clutch(container, data.EngineData));
 			var engine = new CombustionEngine(container, data.EngineData, pt1Disabled: true);
-			var idleController = GetIdleController(data.PTO, engine);
+			var idleController = GetIdleController(data.PTO, engine, container);
 
 			powertrain.AddComponent(engine, idleController)
 				.AddAuxiliaries(container, data);
@@ -148,7 +148,7 @@ namespace TUGraz.VectoCore.Models.Simulation.Impl
 			}
 
 			var engine = new CombustionEngine(container, data.EngineData);
-			var idleController = GetIdleController(data.PTO, engine);
+			var idleController = GetIdleController(data.PTO, engine, container);
 
 			powertrain.AddComponent(engine, idleController)
 				.AddAuxiliaries(container, data);
@@ -208,7 +208,7 @@ namespace TUGraz.VectoCore.Models.Simulation.Impl
 			}
 
 			var engine = new CombustionEngine(container, data.EngineData);
-			var idleController = GetIdleController(data.PTO, engine);
+			var idleController = GetIdleController(data.PTO, engine, container);
 			cycle.IdleController = idleController as IdleControllerSwitcher;
 
 			powertrain.AddComponent(engine, idleController)
@@ -219,12 +219,12 @@ namespace TUGraz.VectoCore.Models.Simulation.Impl
 			return container;
 		}
 
-		private static IIdleController GetIdleController(PTOData pto, ICombustionEngine engine)
+		private static IIdleController GetIdleController(PTOData pto, ICombustionEngine engine, IVehicleContainer container)
 		{
 			var controller = engine.IdleController;
 
 			if (pto != null && pto.PTOCycle != null) {
-				var ptoController = new PTOCycleController(pto.PTOCycle);
+				var ptoController = new PTOCycleController(container, pto.PTOCycle);
 				controller = new IdleControllerSwitcher(engine.IdleController, ptoController);
 			}
 
--- VectoCore/VectoCore/Models/SimulationComponent/Impl/PTOCycleController.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Impl/PTOCycleController.cs
@@ -29,7 +29,6 @@
 *   Martin Rexeis, rexeis@ivt.tugraz.at, IVT, Graz University of Technology
 */
 
-using System;
 using System.Linq;
 using TUGraz.VectoCommon.Exceptions;
 using TUGraz.VectoCommon.Models;
@@ -37,6 +36,7 @@ using TUGraz.VectoCommon.Utils;
 using TUGraz.VectoCore.Configuration;
 using TUGraz.VectoCore.Models.Connector.Ports;
 using TUGraz.VectoCore.Models.Connector.Ports.Impl;
+using TUGraz.VectoCore.Models.Simulation;
 using TUGraz.VectoCore.Models.Simulation.Data;
 using TUGraz.VectoCore.Models.SimulationComponent.Data;
 using TUGraz.VectoCore.OutputData;
@@ -45,7 +45,8 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 {
 	public class PTOCycleController : PowertrainDrivingCycle, IIdleController
 	{
-		public ITnOutPort RequestPort {
+		public ITnOutPort RequestPort
+		{
 			set { NextComponent = value; }
 		}
 
@@ -53,8 +54,10 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 
 		protected Second IdleStart;
 
-		public PTOCycleController(DrivingCycleData cycle) : base(null, cycle)
+		public PTOCycleController(IVehicleContainer container, IDrivingCycleData cycle)
+			: base(null, cycle)
 		{
+			DataBus = container;
 			Duration = Data.Entries.Last().Time - Data.Entries.First().Time;
 		}
 
@@ -62,13 +65,15 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 			bool dryRun = false)
 		{
 			if (outAngularVelocity != null) {
-				throw new VectoException("{0} can only handle idle requests: AngularVelocity has to be null!", GetType().ToString());
+				throw new VectoException("{0} can only handle idle requests: AngularVelocity has to be null!",
+					GetType().ToString());
 			}
 			if (!outTorque.IsEqual(0)) {
 				throw new VectoException("{0} can only handle idle requests: Torque has to be 0!", GetType().ToString());
 			}
 			if (IdleStart == null) {
 				IdleStart = absTime;
+				PreviousState.InAngularVelocity = DataBus.EngineSpeed;
 			}
 			return base.Request(absTime - IdleStart, dt);
 		}
@@ -81,7 +86,6 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 		public void Reset()
 		{
 			CycleIterator.Reset();
-			PreviousState.InAngularVelocity = CycleIterator.LeftSample.AngularVelocity;
 			IdleStart = null;
 		}
 
