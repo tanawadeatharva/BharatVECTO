d069ce37ecd416447c0df3deb9a2d0214671a00c
Markus Quaritsch <markus.quaritsch@tugraz.at>
2017-07-05 10:18:26 +0200

    validation: check for number of driven axles depending on axle configuration (allow more than one driven axle)

--- VectoCore/VectoCore/Models/SimulationComponent/Data/VehicleData.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Data/VehicleData.cs
@@ -56,9 +56,10 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Data
 		public static ValidationResult ValidateAirDragData(AirdragData airDragData, ValidationContext validationContext)
 		{
 			if (airDragData.CrossWindCorrectionMode != CrossWindCorrectionMode.DeclarationModeCorrection &&
-				airDragData.CrossWindCorrectionCurve.AirDragArea == null)
+				airDragData.CrossWindCorrectionCurve.AirDragArea == null) {
 				return new ValidationResult(
 					"AirDrag Area (CdxA) must not be empty when the cross wind correction mode is not \"Speed dependent (Declaration Mode)\"");
+			}
 
 			return ValidationResult.Success;
 		}
@@ -280,8 +281,13 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Data
 			}
 
 			var numDrivenAxles = vehicleData._axleData.Count(x => x.AxleType == AxleType.VehicleDriven);
-			if (numDrivenAxles != 1) {
-				return new ValidationResult("Exactly one axle has to be defined as driven!");
+			if (numDrivenAxles != vehicleData.AxleConfiguration.NumDrivenAxles()) {
+				return
+					new ValidationResult(string.Format(
+						vehicleData.AxleConfiguration.NumAxles() == 1
+							? "Exactly {0} axle has to be defined as driven, given {1}!"
+							: "Exactly {0} axles have to be defined as driven, given {1}!", vehicleData.AxleConfiguration.NumDrivenAxles(),
+						numDrivenAxles));
 			}
 
 			return ValidationResult.Success;
