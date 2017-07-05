6fff57a13e000b7d95e205a4b37abdacba204111
Michael Krisper <michael.krisper@tugraz.at>
2017-07-05 15:52:04 +0200

    Added LongRunning Category to some tests

--- VectoCore/VectoCoreTest/Integration/TorqueLimitsTest.cs
+++ VectoCore/VectoCoreTest/Integration/TorqueLimitsTest.cs
@@ -171,10 +171,10 @@ namespace TUGraz.VectoCore.Tests.Integration
 			Assert.AreEqual(-115, engineData.FullLoadCurves[6].MaxDragTorque.Value());
 		}
 
-		[TestCase(GearboxLimitJobDecl_800),
-		TestCase(GearboxLimitJobDecl_865),
-		TestCase(VehicleLimitJobDecl_850),
-		TestCase(VehicleLimitJobDecl_910)]
+		[TestCase(GearboxLimitJobDecl_800, "LongRunning"),
+		TestCase(GearboxLimitJobDecl_865, "LongRunning"),
+		TestCase(VehicleLimitJobDecl_850, "LongRunning"),
+		TestCase(VehicleLimitJobDecl_910, "LongRunning")]
 		public void TestRunTorqueLimitedSimulations(string file)
 		{
 			var fileWriter = new FileOutputWriter(file);
@@ -215,7 +215,7 @@ namespace TUGraz.VectoCore.Tests.Integration
 			Assert.IsTrue(jobContainer.Runs.All(r => r.Success), string.Concat(jobContainer.Runs.Select(r => r.ExecException)));
 		}
 
-		[TestCase(EngineSpeedLimitJobDecl)]
+		[TestCase(EngineSpeedLimitJobDecl, "LongRunning")]
 		public void TestRunEngineSpeedLimitedSimulations(string file)
 		{
 			var fileWriter = new FileOutputWriter(file);
