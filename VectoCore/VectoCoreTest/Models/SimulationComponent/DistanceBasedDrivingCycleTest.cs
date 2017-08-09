3d349a6a6d88d069a588d8bd3b93a99fa3dc1654
Markus Quaritsch <markus.quaritsch@tugraz.at>
2017-08-09 10:03:53 +0200

    adapt testcases due to modified simulation of stop time

--- VectoCore/VectoCoreTest/Models/SimulationComponent/DistanceBasedDrivingCycleTest.cs
+++ VectoCore/VectoCoreTest/Models/SimulationComponent/DistanceBasedDrivingCycleTest.cs
@@ -164,30 +164,19 @@ namespace TUGraz.VectoCore.Tests.Models.SimulationComponent
 			var startDistance = container.CycleStartDistance.Value();
 			var absTime = 0.SI<Second>();
 
-			// waiting time of 40 seconds is split up to 3 steps: 0.5, 39, 0.5
-			var response = cycle.OutPort().Request(absTime, 1.SI<Meter>());
-			Assert.IsInstanceOfType(response, typeof(ResponseSuccess));
-			Assert.AreEqual(0, driver.LastRequest.TargetVelocity.Value(), Tolerance);
-			Assert.AreEqual(0.028416069495827, driver.LastRequest.Gradient.Value(), 1E-12);
-			Assert.AreEqual(0.5, driver.LastRequest.dt.Value(), Tolerance);
-			container.CommitSimulationStep(absTime, response.SimulationInterval);
-			absTime += response.SimulationInterval;
 
-			response = cycle.OutPort().Request(absTime, 1.SI<Meter>());
-			Assert.IsInstanceOfType(response, typeof(ResponseSuccess));
-			Assert.AreEqual(0, driver.LastRequest.TargetVelocity.Value(), Tolerance);
-			Assert.AreEqual(0.028416069495827, driver.LastRequest.Gradient.Value(), 1E-12);
-			Assert.AreEqual(39, driver.LastRequest.dt.Value(), Tolerance);
-			container.CommitSimulationStep(absTime, response.SimulationInterval);
-			absTime += response.SimulationInterval;
+			IResponse response;
 
-			response = cycle.OutPort().Request(absTime, 1.SI<Meter>());
-			Assert.IsInstanceOfType(response, typeof(ResponseSuccess));
-			Assert.AreEqual(0, driver.LastRequest.TargetVelocity.Value(), Tolerance);
-			Assert.AreEqual(0.028416069495827, driver.LastRequest.Gradient.Value(), 1E-12);
-			Assert.AreEqual(0.5, driver.LastRequest.dt.Value(), Tolerance);
-			container.CommitSimulationStep(absTime, response.SimulationInterval);
-			absTime += response.SimulationInterval;
+			// waiting 40s in 1s steps
+			for (var i = 0; i < 40; i++) {
+				response = cycle.OutPort().Request(absTime, 1.SI<Meter>());
+				Assert.IsInstanceOfType(response, typeof(ResponseSuccess));
+				Assert.AreEqual(0, driver.LastRequest.TargetVelocity.Value(), Tolerance);
+				Assert.AreEqual(0.028416069495827, driver.LastRequest.Gradient.Value(), 1E-12);
+				Assert.AreEqual(1, driver.LastRequest.dt.Value(), Tolerance);
+				container.CommitSimulationStep(absTime, response.SimulationInterval);
+				absTime += response.SimulationInterval;
+			}
 
 			response = cycle.OutPort().Request(absTime, 1.SI<Meter>());
 
