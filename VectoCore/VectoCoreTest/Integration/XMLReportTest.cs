27a26f547b61f14abf9a3d4b9ef23ec47ea5980a
Markus Quaritsch <markus.quaritsch@tugraz.at>
2017-08-29 10:18:43 +0200

    remove comments, correct assertion

--- VectoCore/VectoCoreTest/Integration/XMLReportTest.cs
+++ VectoCore/VectoCoreTest/Integration/XMLReportTest.cs
@@ -46,31 +46,7 @@ namespace TUGraz.VectoCore.Tests.Integration
 
 			var manufacturerReport = xmlReport.FullReport;
 
-			Assert.AreEqual(2, manufacturerReport.XPathSelectElement("//*[local-name()='VehicleGroup']").Value.ToInt());
-
-			//var sumRow = sumData.Table.Rows[1];
-			//Assert.AreEqual(dataProvider.VehicleInputData.Manufacturer, sumRow[SummaryDataContainer.VEHICLE_MANUFACTURER]);
-			//Assert.AreEqual(dataProvider.VehicleInputData.Model, sumRow[SummaryDataContainer.VEHICLE_MODEL]);
-			//Assert.AreEqual(dataProvider.VehicleInputData.VIN, sumRow[SummaryDataContainer.VIN_NUMBER]);
-			//Assert.AreEqual(dataProvider.EngineInputData.Manufacturer, sumRow[SummaryDataContainer.ENGINE_MANUFACTURER]);
-			//Assert.AreEqual(dataProvider.EngineInputData.Model, sumRow[SummaryDataContainer.ENGINE_MODEL]);
-			//Assert.AreEqual(dataProvider.EngineInputData.FuelType.GetLabel(), sumRow[SummaryDataContainer.ENGINE_FUEL_TYPE]);
-			//Assert.AreEqual((dataProvider.EngineInputData.RatedPowerDeclared.ConvertTo().Kilo.Watt.Value()),
-			//	((SI)sumRow[SummaryDataContainer.ENGINE_RATED_POWER]).Value());
-			//Assert.AreEqual(dataProvider.EngineInputData.RatedSpeedDeclared.AsRPM,
-			//	((SI)sumRow[SummaryDataContainer.ENGINE_RATED_SPEED]).Value());
-			//Assert.AreEqual(dataProvider.EngineInputData.Displacement.ConvertTo().Cubic.Centi.Meter.Value(),
-			//	((SI)sumRow[SummaryDataContainer.ENGINE_DISPLACEMENT]).Value());
-			//Assert.AreEqual(dataProvider.GearboxInputData.Manufacturer, sumRow[SummaryDataContainer.GEARBOX_MANUFACTURER]);
-			//Assert.AreEqual(dataProvider.GearboxInputData.Model, sumRow[SummaryDataContainer.GEARBOX_MODEL]);
-			//Assert.AreEqual(dataProvider.AxleGearInputData.Manufacturer, sumRow[SummaryDataContainer.AXLE_MANUFACTURER]);
-			//Assert.AreEqual(dataProvider.AxleGearInputData.Model, sumRow[SummaryDataContainer.AXLE_MODEL]);
-
-			//Assert.AreEqual(dataProvider.EngineInputData.ColdHotBalancingFactor, sumRow[SummaryDataContainer.ENGINE_BF_COLD_HOT]);
-			//Assert.AreEqual(dataProvider.EngineInputData.CorrectionFactorRegPer, sumRow[SummaryDataContainer.ENGINE_CF_REG_PER]);
-			//Assert.AreEqual(dataProvider.EngineInputData.WHTCRural, sumRow[SummaryDataContainer.ENGINE_WHTC_RURAL]);
-			//Assert.AreEqual(dataProvider.EngineInputData.WHTCUrban, sumRow[SummaryDataContainer.ENGINE_WHTC_URBAN]);
-			//Assert.AreEqual(dataProvider.EngineInputData.WHTCMotorway, sumRow[SummaryDataContainer.ENGINE_WHTC_MOTORWAY]);
+			Assert.AreEqual(5, manufacturerReport.XPathSelectElement("//*[local-name()='VehicleGroup']").Value.ToInt());
 		}
 	}
 }
