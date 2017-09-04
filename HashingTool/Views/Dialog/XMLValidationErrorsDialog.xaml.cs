f4021475f78cfed21a9168a1c70633ca687d3c42
Markus Quaritsch <markus.quaritsch@tugraz.at>
2017-09-04 09:41:30 +0200

    removing expander, showing xml errors in dialog instead

---
+++ HashingTool/Views/Dialog/XMLValidationErrorsDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HashingTool.ViewModel.UserControl;
+
+namespace HashingTool.Views
+{
+	/// <summary>
+	/// Interaction logic for XMLValidationErrorsDialog.xaml
+	/// </summary>
+	public partial class XMLValidationErrorsDialog : Window
+	{
+		public static readonly DependencyProperty XMLErrorsProperty = DependencyProperty.Register("XMLErrors",
+			typeof(ObservableCollection<string>),
+			typeof(XMLValidationErrorsDialog));
+
+		public XMLValidationErrorsDialog()
+		{
+			InitializeComponent();
+			(Content as FrameworkElement).DataContext = this;
+		}
+
+		public ObservableCollection<string> XMLErrors
+		{
+			get { return (ObservableCollection<string>)GetValue(XMLErrorsProperty); }
+			set { SetValue(XMLErrorsProperty, value); }
+		}
+	}
+}
--- HashingTool/Views/HashComponentData.xaml.cs
+++ HashingTool/Views/HashComponentData.xaml.cs
@@ -18,5 +18,18 @@ namespace HashingTool.Views
 		{
 			InitializeComponent();
 		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var dialog = new XMLValidationErrorsDialog();
+			dialog.XMLErrors = (DataContext as HashComponentDataViewModel).XMLValidationErrors;
+
+			dialog.ShowDialog();
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			Clipboard.SetText(tbDigestValue.Text);
+		}
 	}
 }
