3720be352b98cc45419e365ebef629865c98e58a
Markus Quaritsch <markus.quaritsch@tugraz.at>
2017-09-18 17:25:12 +0200

    handle the case where the vehicle has no idling speed set

--- VectoCore/VectoCore/OutputData/XML/XMLDeclarationWriter.cs
+++ VectoCore/VectoCore/OutputData/XML/XMLDeclarationWriter.cs
@@ -128,7 +128,10 @@ namespace TUGraz.VectoCore.OutputData.XML
 				new XElement(tns + XMLNames.Vehicle_AxleConfiguration, vehicle.AxleConfiguration.GetName()),
 				new XElement(tns + XMLNames.Vehicle_CurbMassChassis, vehicle.CurbMassChassis.ToXMLFormat(0)),
 				new XElement(tns + XMLNames.Vehicle_GrossVehicleMass, vehicle.GrossVehicleMassRating.ToXMLFormat(0)),
-				new XElement(tns + XMLNames.Vehicle_IdlingSpeed, vehicle.EngineIdleSpeed.AsRPM.ToXMLFormat(0)),
+				new XElement(tns + XMLNames.Vehicle_IdlingSpeed,
+					vehicle.EngineIdleSpeed != null
+						? vehicle.EngineIdleSpeed.AsRPM.ToXMLFormat(0)
+						: data.EngineInputData.IdleSpeed.AsRPM.ToXMLFormat(0)),
 				new XElement(tns + XMLNames.Vehicle_RetarderType, retarder.Type.ToXMLFormat()),
 				retarder.Type.IsDedicatedComponent()
 					? new XElement(tns + XMLNames.Vehicle_RetarderRatio, retarder.Ratio.ToXMLFormat(3))
