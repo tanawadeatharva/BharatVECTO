e74c72d6815883224adc782504ca7f69b8a8adf0
Markus Quaritsch <markus.quaritsch@tugraz.at>
2018-05-15 17:11:01 +0200

    adding schema version to xml validator

--- VectoCore/VectoCore/Utils/XMLValidator.cs
+++ VectoCore/VectoCore/Utils/XMLValidator.cs
@@ -51,7 +51,7 @@ namespace TUGraz.VectoCore.Utils
 			{XmlDocumentType.DeclarationJobData, Tuple.Create("VectoInput{0}.xsd", new [] {"1.0"}) },
 			{XmlDocumentType.DeclarationComponentData, Tuple.Create("VectoComponent{0}.xsd", new [] {"1.0"}) },
 			{XmlDocumentType.EngineeringData, Tuple.Create("VectoEngineeringInput{0}.xsd", new [] {"0.7"}) },
-			{XmlDocumentType.ManufacturerReport, Tuple.Create("VectoOutputManufacturer{0}.xsd", new [] {"0.4"}) },
+			{XmlDocumentType.ManufacturerReport, Tuple.Create("VectoOutputManufacturer{0}.xsd", new [] {"0.4", "0.5"}) },
 			{ XmlDocumentType.CustomerReport , Tuple.Create("VectoOutputCustomer{0}.xsd", new [] {"0.4"})},
 		};
 
