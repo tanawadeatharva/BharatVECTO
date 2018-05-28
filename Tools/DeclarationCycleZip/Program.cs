a1d4212e03a63d71140de411883665f6a6febd67
Markus Quaritsch <markus.quaritsch@tugraz.at>
2018-05-28 17:56:03 +0200

    fix in cycle zip

--- Tools/DeclarationCycleZip/Program.cs
+++ Tools/DeclarationCycleZip/Program.cs
@@ -64,8 +64,8 @@ namespace DeclarationCycleZip
 				}
 				var row = table.NewRow();
 				row["<s>"] = x.Distance.Value().ToString(CultureInfo.InvariantCulture);
-			    row["<v>"] = x.VehicleTargetSpeed.ConvertToKiloMeterPerHour().Value.ToString(CultureInfo.InvariantCulture);
-                row["<grad>"] = x.RoadGradientPercent.Value().ToString(CultureInfo.InvariantCulture);
+				row["<v>"] = x.VehicleTargetSpeed.ConvertToKiloMeterPerHour().Value.ToString(CultureInfo.InvariantCulture);
+				row["<grad>"] = x.RoadGradientPercent.Value().ToString(CultureInfo.InvariantCulture);
 				row["<stop>"] = x.StoppingTime.Value().ToString(CultureInfo.InvariantCulture);
 				table.Rows.Add(row);
 				lastDistance = x.Distance;
