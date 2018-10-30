ebea16500f233740499daa3c74c6235f7522c282
Markus Quaritsch <markus.quaritsch@tugraz.at>
2018-10-30 13:36:22 +0100

    new VECTO release

--- VectoCore/VectoCore/Properties/Version.cs
+++ VectoCore/VectoCore/Properties/Version.cs
@@ -30,5 +30,5 @@
 */
 
 using System.Reflection;
-[assembly: AssemblyVersion("3.3.0.1250")]
-[assembly: AssemblyFileVersion("3.3.0.1250")]
+[assembly: AssemblyVersion("3.3.0.1398")]
+[assembly: AssemblyFileVersion("3.3.0.1398")]
