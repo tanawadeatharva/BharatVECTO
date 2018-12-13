aceba39b33d65b9119a44b61672781c2218281c4
Markus Quaritsch <markus.quaritsch@tugraz.at>
2018-12-13 08:14:06 +0100

    adapt testcase for meanshift clustering

--- VectoCore/VectoCoreTest/Algorithms/MeanShiftClusteringTest.cs
+++ VectoCore/VectoCoreTest/Algorithms/MeanShiftClusteringTest.cs
@@ -20,27 +20,27 @@ namespace TUGraz.VectoCore.Tests.Algorithms
 			foreach (var center in centers) {
 				var sum = 0.0;
 				for (var i = 0; i < 30; i++) {
-					var val = center + rnd.NextDouble() * 20 - 10;
+					var val = center + rnd.NextDouble() * 5 - 2.5;
 					sum += val;
 					entries.Add(val);
 				}
-				Console.WriteLine(sum / 30);
+				//Console.WriteLine(sum / 30);
 			}
 
-			Console.WriteLine(string.Join(", ", entries));
+			//Console.WriteLine(string.Join(", ", entries));
 
 			var clusterer = new MeanShiftClustering() {
 				ClusterCount = 8
 			};
 			var clusters = clusterer.FindClusters(entries.ToArray(), 10);
 
-			Console.WriteLine(clusterer.IterationCount);
-			Console.WriteLine(string.Join(", ",centers));
+			//Console.WriteLine(clusterer.IterationCount);
+			Console.WriteLine(string.Join(", ", centers));
 			Console.WriteLine(string.Join(", ", clusters));
 
 			Assert.AreEqual(centers.Length, clusters.Length);
 			foreach (var center in centers) {
-				Assert.IsTrue(clusters.Any(x => Math.Abs(center - x) < 5));
+				Assert.IsTrue(clusters.Any(x => Math.Abs(center - x) < 2.5));
 			}
 		}
 
