18c3f3fcd2968deab3e212d988d000df40e7b7bc
Markus Quaritsch <markus.quaritsch@tugraz.at>
2019-02-20 11:20:00 +0100

    updated testcases with expected fuel consumption (slight changes due to shorter effective traction interruption)

--- VectoCore/VectoCoreTest/Integration/Declaration/NaturalGasVehicles.cs
+++ VectoCore/VectoCoreTest/Integration/Declaration/NaturalGasVehicles.cs
@@ -63,10 +63,10 @@ namespace TUGraz.VectoCore.Tests.Integration.Declaration
 
 
 		[
-		 TestCase(Class5NG, 2, TankSystem.Liquefied, 253.7, 702.8, TestName = "Class5 LNG 2"),
-		 TestCase(Class5NG, 2, TankSystem.Compressed, 259.5, 698.1, TestName = "Class5 CNG 2"),
-		TestCase(Class5NG, 6, TankSystem.Liquefied, 252.9, 700.6, TestName = "Class5 LNG 6"),
-		TestCase(Class5NG, 6, TankSystem.Compressed, 258.7, 696.0, TestName = "Class5 CNG 6"),
+		 TestCase(Class5NG, 2, TankSystem.Liquefied, 253.8, 703.0, TestName = "Class5 LNG 2"),
+		 TestCase(Class5NG, 2, TankSystem.Compressed, 259.6, 698.3, TestName = "Class5 CNG 2"),
+		TestCase(Class5NG, 6, TankSystem.Liquefied, 253.0, 700.7, TestName = "Class5 LNG 6"),
+		TestCase(Class5NG, 6, TankSystem.Compressed, 258.8, 696.1, TestName = "Class5 CNG 6"),
 			]
 		public void NaturalGasTankSystemTest(string filename, int runIdx, TankSystem tankSystem, double expectedFc, double expectedCo2)
 		{
