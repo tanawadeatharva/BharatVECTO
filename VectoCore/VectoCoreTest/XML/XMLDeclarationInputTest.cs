c0b9ceb963dceadd1edfa307a5fb3aed53bd0581
Markus Quaritsch <markus.quaritsch@tugraz.at>
2019-02-26 17:21:57 +0100

    remove testcase that is no longer required (no default values for optional new parameters)

--- VectoCore/VectoCoreTest/XML/XMLDeclarationInputTest.cs
+++ VectoCore/VectoCoreTest/XML/XMLDeclarationInputTest.cs
@@ -1011,28 +1011,7 @@ namespace TUGraz.VectoCore.Tests.XML
 			Assert.AreEqual(pcc, adas.PredictiveCruiseControl);
 		}
 
-		[TestCase(SampleVehicleDecl)]
-		public void TestDefaultValuesNewParameters(string file)
-		{
-			var reader = XmlReader.Create(file);
-
-			var inputDataProvider = new XMLDeclarationInputDataProvider(reader, true);
-			var vehicle = inputDataProvider.JobInputData.Vehicle;
-			var adas = vehicle.ADAS;
-
-			Assert.IsFalse(vehicle.ExemptedVehicle);
-
-			Assert.AreEqual(false, vehicle.ZeroEmissionVehicle);
-			Assert.AreEqual(false, vehicle.VocationalVehicle);
-			Assert.AreEqual(true, vehicle.SleeperCab);
-
-			Assert.AreEqual(TankSystem.Compressed, vehicle.TankSystem);
-
-			Assert.AreEqual(false, adas.EngineStopStart);
-			Assert.AreEqual(false, adas.EcoRollWitoutEngineStop);
-			Assert.AreEqual(false, adas.EcoRollWithEngineStop);
-			Assert.AreEqual(PredictiveCruiseControlType.None, adas.PredictiveCruiseControl);
-		}
+		
 
 		[TestCase(SampleVehicleFullDeclUpdated)]
 		public void TestRigidTruckIsReadAsRigidLorry(string file)
