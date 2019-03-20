5652339b83d1a7ed4ab42cc6524d1fa37da2b2d1
Markus Quaritsch <markus.quaritsch@tugraz.at>
2019-03-20 16:49:09 +0100

    refactoring: move digestData class to separate file

--- VectoCommon/VectoCommon/InputData/VTPDeclarationInputData.cs
+++ VectoCommon/VectoCommon/InputData/VTPDeclarationInputData.cs
@@ -31,9 +31,6 @@
 
 using System.Collections.Generic;
 using System.Linq;
-using System.Xml;
-using System.Xml.Linq;
-using System.Xml.XPath;
 using TUGraz.VectoCommon.Hashing;
 using TUGraz.VectoCommon.Utils;
 using TUGraz.VectoHashing;
@@ -74,72 +71,5 @@ namespace TUGraz.VectoCommon.InputData
 		IDictionary<VectoComponents,IList<string>> ComponentDigests { get; }
 
 		DigestData JobDigest { get; }
-	}
-
-	public class DigestData
-	{
-		private const string ReferenceQueryXPath = ".//*[local-name()='Reference']/@URI";
-		private const string AlgorithmQueryXPath = ".//*[local-name()='Transform']/@Algorithm";
-		private const string DigestMethodQueryXPath = ".//*[local-name()='DigestMethod']/@Algorithm";
-		private const string DigestValueQuerXPath = ".//*[local-name()='DigestValue']";
-
-		public DigestData(string reference, string[] c14n, string digestMethod, string digestValue)
-		{
-			Reference = reference;
-			CanonicalizationMethods = c14n;
-			DigestMethod = digestMethod;
-			DigestValue = digestValue;
-		}
-
-		public DigestData(XPathNavigator navigator)
-		{
-			Reference = navigator.SelectSingleNode(ReferenceQueryXPath)?.InnerXml;
-			var nodes = navigator.Select(AlgorithmQueryXPath);
-			var c14n = new List<string>();
-			while (nodes.MoveNext()) {
-				c14n.Add(nodes.Current.InnerXml);
-			}
-			CanonicalizationMethods = c14n.ToArray();
-			DigestMethod = navigator.SelectSingleNode(DigestMethodQueryXPath)?.InnerXml;
-			DigestValue = navigator.SelectSingleNode(DigestValueQuerXPath)?.InnerXml;
-		}
-
-		public DigestData(XmlNode xmlNode)
-		{
-			Reference = xmlNode.SelectSingleNode(ReferenceQueryXPath)?.InnerXml;
-			var nodes = xmlNode.SelectNodes(AlgorithmQueryXPath);
-			var c14n = new List<string>();
-			if (nodes != null) {
-				for (var i = 0; i < nodes.Count; i++) {
-					c14n.Add(nodes[i].InnerXml);
-				}
-			}
-			CanonicalizationMethods = c14n.ToArray();
-			DigestMethod = xmlNode.SelectSingleNode(DigestMethodQueryXPath)?.InnerXml;
-			DigestValue = xmlNode.SelectSingleNode(DigestValueQuerXPath)?.InnerXml;
-		}
-
-		public DigestData(XElement xmlNode)
-		{
-			Reference = xmlNode.XPathSelectElement(".//*[local-name()='Reference']")?.Attribute(XName.Get("URI"))?.Value;
-			var nodes = xmlNode.XPathSelectElements(".//*[local-name()='Transform']");
-			var c14n = new List<string>();
-
-			foreach (var node in nodes) {
-				c14n.Add(node.Attribute(XName.Get("Algorithm"))?.Value);
-			}
-
-			CanonicalizationMethods = c14n.ToArray();
-			DigestMethod = xmlNode.XPathSelectElement(".//*[local-name()='DigestMethod']")?.Attribute(XName.Get("Algorithm"))?.Value;
-			DigestValue = xmlNode.XPathSelectElement(DigestValueQuerXPath)?.Value;
-		}
-
-		public string DigestValue { get; }
-
-		public string Reference { get; }
-
-		public string[] CanonicalizationMethods { get; }
-		public string DigestMethod { get; }
-
 	}
 }
