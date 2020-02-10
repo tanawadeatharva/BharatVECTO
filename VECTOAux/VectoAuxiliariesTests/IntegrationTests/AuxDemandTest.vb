7c75230b3458081d0c4ab105181bf609132ec9ad
Markus Quaritsch <markus.quaritsch@tugraz.at>
2020-02-10 09:14:38 +0100

    make testcases green

--- VECTOAux/VectoAuxiliariesTests/IntegrationTests/AuxDemandTest.vb
+++ VECTOAux/VectoAuxiliariesTests/IntegrationTests/AuxDemandTest.vb
@@ -52,6 +52,7 @@ Namespace IntegrationTests
             aux.Signals.PreExistingAuxPower = (6.1 * 1000).SI(Of Watt)()
             aux.Signals.Idle = False
             aux.Signals.InNeutral = False
+            aux.Signals.SimulationInterval = 1.SI(of Second)
             'aux.Signals.RunningCalc = True
             'aux.Signals.InternalEnginePower = (internalPower * 1000).SI(Of Watt)()        'kW
             Dim internalEnginePower = (internalPower * 1000).SI(Of Watt)()        'kW
@@ -106,6 +107,7 @@ Namespace IntegrationTests
             'aux.Signals.InternalEnginePower = (internalPower * 1000).SI(Of Watt)()       'kW
             Dim internalEnginePower = (internalPower * 1000).SI(Of Watt)()       'kW
             aux.Signals.ExcessiveDragPower = engineMotoringPower + internalEnginePower
+            aux.Signals.SimulationInterval = 1.SI(of Second)
 
             'Dim msg As String = String.Empty
             For i As Integer = 0 To 9
