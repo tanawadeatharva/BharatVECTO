f660aa0d498fb31cdc5734d505c2eb8193865728
Markus Quaritsch <markus.quaritsch@tugraz.at>
2020-02-25 13:51:34 +0100

    adapt torque converter testcases due to changes in the tc drag characteristics automatically appended (see commit b5ad883f , 19.12.2019)

--- VectoCore/VectoCore/Models/SimulationComponent/Data/Gearbox/TorqueConverterData.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Data/Gearbox/TorqueConverterData.cs
@@ -32,6 +32,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Linq;
 using TUGraz.VectoCommon.Exceptions;
 using TUGraz.VectoCommon.Utils;
@@ -365,6 +366,7 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Data.Gearbox
 		}
 	}
 
+	[DebuggerDisplay("nu: {SpeedRatio}, mu: {TorqueRatio}, T_ref: {Torque}")]
 	public class TorqueConverterEntry
 	{
 		public double SpeedRatio;
