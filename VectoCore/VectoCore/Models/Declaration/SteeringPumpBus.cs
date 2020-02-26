887b30ff69ec63e32c8c9dacc436c4d11539cec6
Markus Quaritsch <markus.quaritsch@tugraz.at>
2020-02-26 16:49:32 +0100

    busaux steering pump: always apply axle factor and tubing factor

--- VectoCore/VectoCore/Models/Declaration/SteeringPumpBus.cs
+++ VectoCore/VectoCore/Models/Declaration/SteeringPumpBus.cs
@@ -37,8 +37,8 @@ namespace TUGraz.VectoCore.Models.Declaration {
 				var powerDemandTubing = (Constants.BusParameters.Auxiliaries.SteeringPump.TubingLoss * 2 *
 										(vehicleLength - Constants.BusParameters.Auxiliaries.SteeringPump.LengthBonus) *
 										Constants.BusParameters.Auxiliaries.SteeringPump.VolumeFlow).Cast<Watt>();
-				var tubingFactor = i == 0 ? techLookup.TubingFactor : 0.0;
-				var axleFactor = i == 0 ? 1.0 : techLookup.AxleFactor;
+				var tubingFactor = techLookup.TubingFactor;
+				var axleFactor = techLookup.AxleFactor;
 
 				powerDemand += baseDemand * axleFactor + powerDemandTubing * tubingFactor;
 			}
