98b204854ecf6fead9e5132d0e75ecccce12d897
VKMTHD\franzjosefkober <franz.josef.kober@ivt.tugraz.at>
2020-03-10 09:35:02 +0100

    finished 3 axle buses test

--- VectoCore/VectoCoreTest/Models/Declaration/DeclarationSegmentComplete3AxleBusesTest.cs
+++ VectoCore/VectoCoreTest/Models/Declaration/DeclarationSegmentComplete3AxleBusesTest.cs
@@ -36,6 +36,7 @@ namespace TUGraz.VectoCore.Tests.Models.Declaration
 			TestCase(3, VehicleCode.CB, "34f"),
 			TestCase(3, VehicleCode.CG, "35a"),
 			TestCase(3, VehicleCode.CG, "35b"),
+			TestCase(3, VehicleCode.CH, "35c"),
 			TestCase(3, VehicleCode.CC, "36a"),
 			TestCase(3, VehicleCode.CC, "36b"),
 			TestCase(3, VehicleCode.CC, "36c"),
@@ -270,7 +271,6 @@ namespace TUGraz.VectoCore.Tests.Models.Declaration
 			}
 		}
 
-
 		[TestCase(3, VehicleCode.CA, "34b")]
 		public void TestComplete3AxlesCompleteBus34B(int numberOfAxles, VehicleCode vehicleCode, string vehicleParameterGroup)
 		{
@@ -344,7 +344,6 @@ namespace TUGraz.VectoCore.Tests.Models.Declaration
 				AssertVehicleEquipment(externalDisplays: 1, internalDisplays: 2, fridge: 1, kitchenStandard: 1, vehicleEquipment: mission.BusParameter.VehicleEquipment);
 			}
 		}
-
 
 		[TestCase(3, VehicleCode.CA, "34d")]
 		public void TestComplete3AxlesCompleteBus34D(int numberOfAxles, VehicleCode vehicleCode, string vehicleParameterGroup)
@@ -457,8 +456,339 @@ namespace TUGraz.VectoCore.Tests.Models.Declaration
 			}
 		}
 
+		[TestCase(3, VehicleCode.CG, "35a")]
+		public void TestComplete3AxlesCompleteBus35A(int numberOfAxles, VehicleCode vehicleCode, string vehicleParameterGroup)
+		{
+			var segment = DeclarationData.CompletedBusSegments.Lookup(numberOfAxles, vehicleCode, vehicleParameterGroup);
 
+			Assert.AreEqual(_missionsTypes.Length, segment.Missions.Length);
 
+			for (int i = 0; i < segment.Missions.Length; i++)
+			{
+				var mission = segment.Missions[i];
+
+				Assert.AreEqual(_missionsTypes[i], mission.MissionType);
+				Assert.AreEqual(5.1, mission.DefaultCDxA.Value());
+				Assert.AreEqual(false, mission.AirDragMeasurement);
+
+				AssertBusParameters(
+					isDoubleDeck: false,
+					numberOfAxles: numberOfAxles,
+					isArticulated: true,
+					floorType: FloorType.LowFloor,
+					vehicleCode: vehicleCode,
+					regClasses: "I/I+II/II/A",
+					lowEntry: false,
+					numPassengersLower: 0,
+					passengersLowerOrEqual: null,
+					bodyHeight: null,
+					bodyHeightLowerOrEqual: null,
+					vehicleParam: vehicleParameterGroup,
+					busParameters: mission.BusParameter
+				);
+
+				AssertAveragePassengers(heavyUrban: 3, urban: 3, suburban: 3, interurban: 0, coach: 0, busParameters: mission.BusParameter);
+				AssertAxleDistribution(axle1: 27.3, axle2: 45.4, axle3: 27.3, axle4: 0, axleLoadDistribution: mission.BusParameter.AxleLoadDistribution);
+				AssertVehicleEquipment(externalDisplays: 3, internalDisplays: 3, fridge: 0, kitchenStandard: 0, vehicleEquipment: mission.BusParameter.VehicleEquipment);
+			}
+		}
+
+		[TestCase(3, VehicleCode.CG, "35b")]
+		public void TestComplete3AxlesCompleteBus35B(int numberOfAxles, VehicleCode vehicleCode, string vehicleParameterGroup)
+		{
+			var segment = DeclarationData.CompletedBusSegments.Lookup(numberOfAxles, vehicleCode, vehicleParameterGroup);
+
+			Assert.AreEqual(_missionsTypes.Length, segment.Missions.Length);
+
+			for (int i = 0; i < segment.Missions.Length; i++)
+			{
+				var mission = segment.Missions[i];
+
+				Assert.AreEqual(_missionsTypes[i], mission.MissionType);
+				Assert.AreEqual(5.1, mission.DefaultCDxA.Value());
+				Assert.AreEqual(false, mission.AirDragMeasurement);
+
+				AssertBusParameters(
+					isDoubleDeck: false,
+					numberOfAxles: numberOfAxles,
+					isArticulated: true,
+					floorType: FloorType.LowFloor,
+					vehicleCode: vehicleCode,
+					regClasses: "I/I+II/II/A",
+					lowEntry: true,
+					numPassengersLower: 0,
+					passengersLowerOrEqual: null,
+					bodyHeight: null,
+					bodyHeightLowerOrEqual: null,
+					vehicleParam: vehicleParameterGroup,
+					busParameters: mission.BusParameter
+				);
+
+				AssertAveragePassengers(heavyUrban: 3, urban: 3, suburban: 3, interurban: 0, coach: 0, busParameters: mission.BusParameter);
+				AssertAxleDistribution(axle1: 27.3, axle2: 45.4, axle3: 27.3, axle4: 0, axleLoadDistribution: mission.BusParameter.AxleLoadDistribution);
+				AssertVehicleEquipment(externalDisplays: 3, internalDisplays: 3, fridge: 0, kitchenStandard: 0, vehicleEquipment: mission.BusParameter.VehicleEquipment);
+			}
+		}
+
+		[TestCase(3, VehicleCode.CH, "35c")]
+		public void TestComplete3AxlesCompleteBus35C(int numberOfAxles, VehicleCode vehicleCode, string vehicleParameterGroup)
+		{
+			var segment = DeclarationData.CompletedBusSegments.Lookup(numberOfAxles, vehicleCode, vehicleParameterGroup);
+
+			Assert.AreEqual(_missionsTypes.Length, segment.Missions.Length);
+
+			for (int i = 0; i < segment.Missions.Length; i++)
+			{
+				var mission = segment.Missions[i];
+
+				Assert.AreEqual(_missionsTypes[i], mission.MissionType);
+				Assert.AreEqual(6.4, mission.DefaultCDxA.Value());
+				Assert.AreEqual(false, mission.AirDragMeasurement);
+
+				AssertBusParameters(
+					isDoubleDeck: true,
+					numberOfAxles: numberOfAxles,
+					isArticulated: true,
+					floorType: FloorType.LowFloor,
+					vehicleCode: vehicleCode,
+					regClasses: "I/I+II/II/A",
+					lowEntry: null,
+					numPassengersLower: 0,
+					passengersLowerOrEqual: null,
+					bodyHeight: null,
+					bodyHeightLowerOrEqual: null,
+					vehicleParam: vehicleParameterGroup,
+					busParameters: mission.BusParameter
+				);
+
+				AssertAveragePassengers(heavyUrban: 3.7, urban: 3.7, suburban: 3.7, interurban: 0, coach: 0, busParameters: mission.BusParameter);
+				AssertAxleDistribution(axle1: 27.3, axle2: 45.4, axle3: 27.3, axle4: 0, axleLoadDistribution: mission.BusParameter.AxleLoadDistribution);
+				AssertVehicleEquipment(externalDisplays: 0, internalDisplays: 0, fridge: 0, kitchenStandard: 0, vehicleEquipment: mission.BusParameter.VehicleEquipment);
+			}
+		}
+
+		[TestCase(3, VehicleCode.CC, "36a")]
+		public void TestComplete3AxlesCompleteBus36A(int numberOfAxles, VehicleCode vehicleCode, string vehicleParameterGroup)
+		{
+			var segment = DeclarationData.CompletedBusSegments.Lookup(numberOfAxles, vehicleCode, vehicleParameterGroup);
+
+			Assert.AreEqual(_missionsTypes.Length, segment.Missions.Length);
+
+			for (int i = 0; i < segment.Missions.Length; i++)
+			{
+				var mission = segment.Missions[i];
+
+				Assert.AreEqual(_missionsTypes[i], mission.MissionType);
+				Assert.AreEqual(4.8, mission.DefaultCDxA.Value());
+				Assert.AreEqual(true, mission.AirDragMeasurement);
+
+				AssertBusParameters(
+					isDoubleDeck: false,
+					numberOfAxles: numberOfAxles,
+					isArticulated: true,
+					floorType: FloorType.HighFloor,
+					vehicleCode: vehicleCode,
+					regClasses: "II",
+					lowEntry: null,
+					numPassengersLower: 0,
+					passengersLowerOrEqual: null,
+					bodyHeight: null,
+					bodyHeightLowerOrEqual: null,
+					vehicleParam: vehicleParameterGroup,
+					busParameters: mission.BusParameter
+				);
+
+				AssertAveragePassengers(heavyUrban: 0, urban: 0, suburban: 0, interurban: 2.2, coach: 1.4, busParameters: mission.BusParameter);
+				AssertAxleDistribution(axle1: 27.3, axle2: 45.4, axle3: 27.3, axle4: 0, axleLoadDistribution: mission.BusParameter.AxleLoadDistribution);
+				AssertVehicleEquipment(externalDisplays: 0, internalDisplays: 0, fridge: 0, kitchenStandard: 0, vehicleEquipment: mission.BusParameter.VehicleEquipment);
+			}
+		}
+
+		[TestCase(3, VehicleCode.CC, "36b")]
+		public void TestComplete3AxlesCompleteBus36B(int numberOfAxles, VehicleCode vehicleCode, string vehicleParameterGroup)
+		{
+			var segment = DeclarationData.CompletedBusSegments.Lookup(numberOfAxles, vehicleCode, vehicleParameterGroup);
+
+			Assert.AreEqual(_missionsTypes.Length, segment.Missions.Length);
+
+			for (int i = 0; i < segment.Missions.Length; i++)
+			{
+				var mission = segment.Missions[i];
+
+				Assert.AreEqual(_missionsTypes[i], mission.MissionType);
+				Assert.AreEqual(4.8, mission.DefaultCDxA.Value());
+				Assert.AreEqual(true, mission.AirDragMeasurement);
+
+				AssertBusParameters(
+					isDoubleDeck: false,
+					numberOfAxles: numberOfAxles,
+					isArticulated: true,
+					floorType: FloorType.HighFloor,
+					vehicleCode: vehicleCode,
+					regClasses: "II+III",
+					lowEntry: null,
+					numPassengersLower: 0,
+					passengersLowerOrEqual: null,
+					bodyHeight: 3100,
+					bodyHeightLowerOrEqual: true,
+					vehicleParam: vehicleParameterGroup,
+					busParameters: mission.BusParameter
+				);
+
+				AssertAveragePassengers(heavyUrban: 0, urban: 0, suburban: 0, interurban: 2.2, coach: 1.4, busParameters: mission.BusParameter);
+				AssertAxleDistribution(axle1: 27.3, axle2: 45.4, axle3: 27.3, axle4: 0, axleLoadDistribution: mission.BusParameter.AxleLoadDistribution);
+				AssertVehicleEquipment(externalDisplays: 0, internalDisplays: 0, fridge: 0, kitchenStandard: 0, vehicleEquipment: mission.BusParameter.VehicleEquipment);
+			}
+		}
+
+		[TestCase(3, VehicleCode.CC, "36c")]
+		public void TestComplete3AxlesCompleteBus36C(int numberOfAxles, VehicleCode vehicleCode, string vehicleParameterGroup)
+		{
+			var segment = DeclarationData.CompletedBusSegments.Lookup(numberOfAxles, vehicleCode, vehicleParameterGroup);
+
+			Assert.AreEqual(_missionsTypes.Length, segment.Missions.Length);
+
+			for (int i = 0; i < segment.Missions.Length; i++)
+			{
+				var mission = segment.Missions[i];
+
+				Assert.AreEqual(_missionsTypes[i], mission.MissionType);
+				Assert.AreEqual(4.8, mission.DefaultCDxA.Value());
+				Assert.AreEqual(true, mission.AirDragMeasurement);
+
+				AssertBusParameters(
+					isDoubleDeck: false,
+					numberOfAxles: numberOfAxles,
+					isArticulated: true,
+					floorType: FloorType.HighFloor,
+					vehicleCode: vehicleCode,
+					regClasses: "II+III",
+					lowEntry: null,
+					numPassengersLower: 0,
+					passengersLowerOrEqual: null,
+					bodyHeight: 3100,
+					bodyHeightLowerOrEqual: false,
+					vehicleParam: vehicleParameterGroup,
+					busParameters: mission.BusParameter
+				);
+
+				AssertAveragePassengers(heavyUrban: 0, urban: 0, suburban: 0, interurban: 2.2, coach: 1.4, busParameters: mission.BusParameter);
+				AssertAxleDistribution(axle1: 27.3, axle2: 45.4, axle3: 27.3, axle4: 0, axleLoadDistribution: mission.BusParameter.AxleLoadDistribution);
+				AssertVehicleEquipment(externalDisplays: 0, internalDisplays: 0, fridge: 0, kitchenStandard: 0, vehicleEquipment: mission.BusParameter.VehicleEquipment);
+			}
+		}
+
+		[TestCase(3, VehicleCode.CC, "36d")]
+		public void TestComplete3AxlesCompleteBus36D(int numberOfAxles, VehicleCode vehicleCode, string vehicleParameterGroup)
+		{
+			var segment = DeclarationData.CompletedBusSegments.Lookup(numberOfAxles, vehicleCode, vehicleParameterGroup);
+
+			Assert.AreEqual(_missionsTypes.Length, segment.Missions.Length);
+
+			for (int i = 0; i < segment.Missions.Length; i++)
+			{
+				var mission = segment.Missions[i];
+
+				Assert.AreEqual(_missionsTypes[i], mission.MissionType);
+				Assert.AreEqual(4.8, mission.DefaultCDxA.Value());
+				Assert.AreEqual(true, mission.AirDragMeasurement);
+
+				AssertBusParameters(
+					isDoubleDeck: false,
+					numberOfAxles: numberOfAxles,
+					isArticulated: true,
+					floorType: FloorType.HighFloor,
+					vehicleCode: vehicleCode,
+					regClasses: "III/B",
+					lowEntry: null,
+					numPassengersLower: 0,
+					passengersLowerOrEqual: null,
+					bodyHeight: null,
+					bodyHeightLowerOrEqual: null,
+					vehicleParam: vehicleParameterGroup,
+					busParameters: mission.BusParameter
+				);
+
+				AssertAveragePassengers(heavyUrban: 0, urban: 0, suburban: 0, interurban: 2.2, coach: 1.4, busParameters: mission.BusParameter);
+				AssertAxleDistribution(axle1: 27.3, axle2: 45.4, axle3: 27.3, axle4: 0, axleLoadDistribution: mission.BusParameter.AxleLoadDistribution);
+				AssertVehicleEquipment(externalDisplays: 0, internalDisplays: 0, fridge: 0, kitchenStandard: 0, vehicleEquipment: mission.BusParameter.VehicleEquipment);
+			}
+		}
+
+		[TestCase(3, VehicleCode.CD, "36e")]
+		public void TestComplete3AxlesCompleteBus36E(int numberOfAxles, VehicleCode vehicleCode, string vehicleParameterGroup)
+		{
+			var segment = DeclarationData.CompletedBusSegments.Lookup(numberOfAxles, vehicleCode, vehicleParameterGroup);
+
+			Assert.AreEqual(_missionsTypes.Length, segment.Missions.Length);
+
+			for (int i = 0; i < segment.Missions.Length; i++)
+			{
+				var mission = segment.Missions[i];
+
+				Assert.AreEqual(_missionsTypes[i], mission.MissionType);
+				Assert.AreEqual(5.4, mission.DefaultCDxA.Value());
+				Assert.AreEqual(true, mission.AirDragMeasurement);
+
+				AssertBusParameters(
+					isDoubleDeck: true,
+					numberOfAxles: numberOfAxles,
+					isArticulated: true,
+					floorType: FloorType.HighFloor,
+					vehicleCode: vehicleCode,
+					regClasses: "II/II+III/III/B",
+					lowEntry: null,
+					numPassengersLower: 6,
+					passengersLowerOrEqual: true,
+					bodyHeight: null,
+					bodyHeightLowerOrEqual: null,
+					vehicleParam: vehicleParameterGroup,
+					busParameters: mission.BusParameter
+				);
+
+				AssertAveragePassengers(heavyUrban: 0, urban: 0, suburban: 0, interurban: 2.2, coach: 1.4, busParameters: mission.BusParameter);
+				AssertAxleDistribution(axle1: 27.3, axle2: 45.4, axle3: 27.3, axle4: 0, axleLoadDistribution: mission.BusParameter.AxleLoadDistribution);
+				AssertVehicleEquipment(externalDisplays: 0, internalDisplays: 0, fridge: 0, kitchenStandard: 0, vehicleEquipment: mission.BusParameter.VehicleEquipment);
+			}
+		}
+
+		[TestCase(3, VehicleCode.CD, "36f")]
+		public void TestComplete3AxlesCompleteBus36F(int numberOfAxles, VehicleCode vehicleCode, string vehicleParameterGroup)
+		{
+			var segment = DeclarationData.CompletedBusSegments.Lookup(numberOfAxles, vehicleCode, vehicleParameterGroup);
+
+			Assert.AreEqual(_missionsTypes.Length, segment.Missions.Length);
+
+			for (int i = 0; i < segment.Missions.Length; i++)
+			{
+				var mission = segment.Missions[i];
+
+				Assert.AreEqual(_missionsTypes[i], mission.MissionType);
+				Assert.AreEqual(5.4, mission.DefaultCDxA.Value());
+				Assert.AreEqual(true, mission.AirDragMeasurement);
+
+				AssertBusParameters(
+					isDoubleDeck: true,
+					numberOfAxles: numberOfAxles,
+					isArticulated: true,
+					floorType: FloorType.HighFloor,
+					vehicleCode: vehicleCode,
+					regClasses: "II/II+III/III/B",
+					lowEntry: null,
+					numPassengersLower: 6,
+					passengersLowerOrEqual: false,
+					bodyHeight: null,
+					bodyHeightLowerOrEqual: null,
+					vehicleParam: vehicleParameterGroup,
+					busParameters: mission.BusParameter
+				);
+
+				AssertAveragePassengers(heavyUrban: 0, urban: 0, suburban: 0, interurban: 3, coach: 2, busParameters: mission.BusParameter);
+				AssertAxleDistribution(axle1: 27.3, axle2: 45.4, axle3: 27.3, axle4: 0, axleLoadDistribution: mission.BusParameter.AxleLoadDistribution);
+				AssertVehicleEquipment(externalDisplays: 0, internalDisplays: 0, fridge: 0, kitchenStandard: 0, vehicleEquipment: mission.BusParameter.VehicleEquipment);
+			}
+		}
+		
 
 		#region Assert Methods
 
