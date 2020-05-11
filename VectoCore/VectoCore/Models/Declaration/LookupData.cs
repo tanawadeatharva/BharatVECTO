15490e7a9536b676e994cb66f8b0c0ada8cf9119
Markus Quaritsch <markus.quaritsch@tugraz.at>
2020-05-11 16:55:04 +0200

    read declaration data from delcaration folder instead of embedded data

--- VectoCore/VectoCore/Models/Declaration/LookupData.cs
+++ VectoCore/VectoCore/Models/Declaration/LookupData.cs
@@ -32,6 +32,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using TUGraz.VectoCommon.Exceptions;
 using TUGraz.VectoCommon.Models;
 using TUGraz.VectoCommon.Utils;
@@ -61,13 +62,15 @@ namespace TUGraz.VectoCore.Models.Declaration
 
 		protected static DataTable ReadCsvResource(string resourceId)
 		{
-#if USE_EXTENAL_DECLARATION_DATA
+//#if USE_EXTENAL_DECLARATION_DATA
+			var tmp = resourceId.Replace(DeclarationData.DeclarationDataResourcePrefix + ".", "");
+			var parts = tmp.Split('.');
 			return
-				VectoCSVFile.Read(Path.Combine("Declaration",
-					resourceId.Replace(DeclarationData.DeclarationDataResourcePrefix + ".", "")));
-#else
-			return VectoCSVFile.ReadStream(RessourceHelper.ReadStream(resourceId), source: resourceId);
-#endif
+				VectoCSVFile.Read(Path.Combine("Declaration", string.Join(".", parts[parts.Length - 2], parts[parts.Length - 1])
+					));
+//#else
+//			return VectoCSVFile.ReadStream(RessourceHelper.ReadStream(resourceId), source: resourceId);
+//#endif
 		}
 
 		protected static void NormalizeTable(DataTable table)
