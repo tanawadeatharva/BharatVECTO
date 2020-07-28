ed516c5429ddeb4b806fa717b886c32c68a01058
Markus Quaritsch <markus.quaritsch@tugraz.at>
2020-07-28 07:51:22 +0200

    adding missing file

---
+++ VectoCore/VectoCore/InputData/FileIO/JSON/JSONHybridStrategyParameters.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using TUGraz.VectoCommon.InputData;
+
+namespace TUGraz.VectoCore.InputData.FileIO.JSON
+{
+	public class JSONHybridStrategyParameters : JSONFile, IHybridStrategyParameters
+	{
+		public JSONHybridStrategyParameters(JObject json, string filename, bool tolerateMissing) : base(json, filename, tolerateMissing)
+		{ }
+
+		public double EquivalenceFactor
+		{
+			get
+			{
+				return Body.GetEx<double>("EquivalenceFactor");
+			}
+		}
+
+		public double MinSoC
+		{
+			get
+			{
+				return Body.GetEx<double>("MinSoC") / 100.0;
+			}
+		}
+
+		public double MaxSoC
+		{
+			get
+			{
+				return Body.GetEx<double>("MaxSoC") / 100.0;
+			}
+		}
+
+		public double TargetSoC
+		{
+			get
+			{
+				return Body.GetEx<double>("TargetSoC") / 100.0;
+			}
+		}
+	}
+}
