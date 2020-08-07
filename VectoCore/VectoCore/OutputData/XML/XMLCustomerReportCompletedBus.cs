a1db2d75c17f56b3a9d0369ae181154244040c56
Markus Quaritsch <markus.quaritsch@tugraz.at>
2020-08-07 14:43:09 +0200

    CIF: only provide total number of registered passengers (not per deck)

--- VectoCore/VectoCore/OutputData/XML/XMLCustomerReportCompletedBus.cs
+++ VectoCore/VectoCore/OutputData/XML/XMLCustomerReportCompletedBus.cs
@@ -55,9 +55,7 @@ namespace TUGraz.VectoCore.OutputData.XML {
 				new XElement(tns + XMLNames.Vehicle_VocationalVehicle, modelData.VehicleData.VocationalVehicle),
 				new XElement(tns + XMLNames.Vehicle_SleeperCab, modelData.VehicleData.SleeperCab),
 				new XElement(
-					tns + "RegisteredPassengers",
-					new XElement(tns + XMLNames.Bus_LowerDeck, modelData.VehicleData.InputData.NumberOfPassengersLowerDeck),
-					new XElement(tns + XMLNames.Bus_UpperDeck, modelData.VehicleData.InputData.NumberOfPassengersUpperDeck)
+					tns + "RegisteredPassengers", modelData.VehicleData.InputData.NumberOfPassengersLowerDeck + modelData.VehicleData.InputData.NumberOfPassengersUpperDeck
 				),
 				new XElement(tns + XMLNames.Bus_LowEntry, modelData.VehicleData.InputData.LowEntry),
 				new XElement(tns + XMLNames.Bus_HeighIntegratedBody, modelData.VehicleData.InputData.Height.ToXMLFormat(3)),
