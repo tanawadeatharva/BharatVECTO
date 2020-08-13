3fcdba856ac696a7bdf7e8c2ed3aa14369ded2a0
Markus Quaritsch <markus.quaritsch@tugraz.at>
2020-08-13 18:47:49 +0200

    slightly change search algorithm so that it is possible to re-start search in debugger

--- VectoCore/VectoCore/Utils/SearchAlgorithm.cs
+++ VectoCore/VectoCore/Utils/SearchAlgorithm.cs
@@ -120,10 +120,14 @@ namespace TUGraz.VectoCore.Utils
 		/// Phase 1: Linear Bracketing: Search iterative for the area of interest (with fixed step size).
 		/// Phase 2: Binary Sectioning: Binary search in the area of interest.
 		/// </summary>
-		private static T LineSearch<T>(T x, SI y, T interval, Func<object, SI> getYValue, Func<T, object> evaluateFunction,
+		private static T LineSearch<T>(T xStart, SI yStart, T intervalStart, Func<object, SI> getYValue, Func<T, object> evaluateFunction,
 			Func<object, double> criterion, Func<object, int, bool> abortCriterion, ref int iterationCount) where T : SIBase<T>
 		{
 			var log = LogManager.GetLogger(typeof(SearchAlgorithm).FullName);
+
+			var x = xStart;
+			var y = yStart;
+			var interval = intervalStart;
 
 			var intervalFactor = 1.0;
 			var origY = y;
