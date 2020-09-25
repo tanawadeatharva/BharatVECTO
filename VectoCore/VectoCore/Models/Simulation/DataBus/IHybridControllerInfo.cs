bcdee611dc8a227c2a8d4118a3b17d1cd14a88c0
Markus Quaritsch <markus.quaritsch@tugraz.at>
2020-09-25 13:35:15 +0200

    adding missing file, correct testdata

---
+++ VectoCore/VectoCore/Models/Simulation/DataBus/IHybridControllerInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace TUGraz.VectoCore.Models.SimulationComponent
+{
+	public interface IHybridControllerInfo
+	{
+		GearInfo SelectedGear { get; }
+
+		//IList<PowertrainPosition> ElectricMotors { get; }
+
+		//NewtonMeter ElectricMotorTorque(PowertrainPosition pos);
+	}
+}
