0f30d052de5f3de42348d92174aceae12222836d
Markus Quaritsch <markus.quaritsch@tugraz.at>
2020-12-11 08:44:44 +0100

    building new VECTO version

--- HashingTool/Properties/Version.cs
+++ HashingTool/Properties/Version.cs
@@ -30,5 +30,5 @@
 */
 
 using System.Reflection;
-[assembly: AssemblyVersion("0.2.0.2108")]
-[assembly: AssemblyFileVersion("0.2.0.2108")]
+[assembly: AssemblyVersion("0.2.0.2171")]
+[assembly: AssemblyFileVersion("0.2.0.2171")]
--- VectoCommon/VectoHashing/Properties/Version.cs
+++ VectoCommon/VectoHashing/Properties/Version.cs
@@ -30,5 +30,5 @@
 */
 
 using System.Reflection;
-[assembly: AssemblyVersion("1.2.0.2108")]
-[assembly: AssemblyFileVersion("1.2.0.2108")]
+[assembly: AssemblyVersion("1.2.0.2171")]
+[assembly: AssemblyFileVersion("1.2.0.2171")]
