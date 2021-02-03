55d682a83375e719b9958349b22b03e2aea244a6
Markus Quaritsch <markus.quaritsch@tugraz.at>
2021-02-03 12:34:20 +0100

    adding testcase for P1 hybrid (bus)

--- VectoCore/VectoCoreTest/Integration/Hybrid/ParallelHybridTest.cs
+++ VectoCore/VectoCoreTest/Integration/Hybrid/ParallelHybridTest.cs
@@ -239,6 +239,8 @@ namespace TUGraz.VectoCore.Tests.Integration.Hybrid
 		const string TestJobP1_APTS = @"TestData\Hybrids\GenericVehicle_P1-APT\CityBus_AT_Ser.vecto";
 		const string TestJobP1_APTP = @"TestData\Hybrids\GenericVehicle_P1-APT\CityBus_AT_PS.vecto";
 
+		private const string TestJobCityBusP1_APTP = @"TestData\Hybrids\Citybus_P1-APT-P-220kW-7.7l\CityBus_AT-P.vecto";
+
 		[
 			TestCase(TestJobP1_APTS, 0, TestName = "P1 Hybrid APT-S, DriveCycle LongHaul"),
 			TestCase(TestJobP1_APTS, 1, TestName = "P1 Hybrid APT-S, DriveCycle Coach"),
@@ -261,7 +263,18 @@ namespace TUGraz.VectoCore.Tests.Integration.Hybrid
 			TestCase(TestJobP1_APTP, 7, TestName = "P1 Hybrid APT-P, DriveCycle Suburban"),
 			TestCase(TestJobP1_APTP, 8, TestName = "P1 Hybrid APT-P, DriveCycle Urban"),
 			TestCase(TestJobP1_APTP, 9, TestName = "P1 Hybrid APT-P, DriveCycle UrbanDelivery"),
-		]
+
+            TestCase(TestJobCityBusP1_APTP, 0, TestName = "P1 CityBus Hybrid APT-P, DriveCycle LongHaul"),
+            //TestCase(TestJobCityBusP1_APTP, 1, TestName = "P1 CityBus Hybrid APT-P, DriveCycle Coach"),
+            TestCase(TestJobCityBusP1_APTP, 2, TestName = "P1 CityBus Hybrid APT-P, DriveCycle Construction"),
+            TestCase(TestJobCityBusP1_APTP, 3, TestName = "P1 CityBus Hybrid APT-P, DriveCycle HeavyUrban"),
+            //TestCase(TestJobCityBusP1_APTP, 4, TestName = "P1 CityBus Hybrid APT-P, DriveCycle Interurban"),
+            TestCase(TestJobCityBusP1_APTP, 5, TestName = "P1 CityBus Hybrid APT-P, DriveCycle MunicipalUtility"),
+            TestCase(TestJobCityBusP1_APTP, 6, TestName = "P1 CityBus Hybrid APT-P, DriveCycle RegionalDelivery"),
+            TestCase(TestJobCityBusP1_APTP, 7, TestName = "P1 CityBus Hybrid APT-P, DriveCycle Suburban"),
+            TestCase(TestJobCityBusP1_APTP, 8, TestName = "P1 CityBus Hybrid APT-P, DriveCycle Urban"),
+			TestCase(TestJobCityBusP1_APTP, 9, TestName = "P1 CityBus Hybrid APT-P, DriveCycle UrbanDelivery"),
+        ]
 		public void P1APTHybridDriveCycle(string jobFile, int cycleIdx)
 		{
 			var inputProvider = JSONInputDataFactory.ReadJsonJob(jobFile);
@@ -1225,7 +1238,8 @@ namespace TUGraz.VectoCore.Tests.Integration.Hybrid
 		private static HybridStrategyParameters CreateHybridStrategyData(Watt maxDriveTrainPower)
 		{
 			return new HybridStrategyParameters() {
-				EquivalenceFactor = 2.5,
+				EquivalenceFactorDischarge = 2.5,
+				EquivalenceFactorCharge = 2.5,
 				MinSoC = 0.22,
 				MaxSoC = 0.8,
 				TargetSoC = 0.5,
