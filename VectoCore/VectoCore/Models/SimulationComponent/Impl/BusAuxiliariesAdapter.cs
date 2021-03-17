be3af2bbf879178a7e48f8e55a4609cdc1b00cd8
Markus Quaritsch <markus.quaritsch@tugraz.at>
2021-03-17 08:43:21 +0100

    set ESS utility factor in simulation models to 1, only apply in post-processing

--- VectoCore/VectoCore/Models/SimulationComponent/Impl/BusAuxiliariesAdapter.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Impl/BusAuxiliariesAdapter.cs
@@ -62,7 +62,7 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 			IVehicleContainer container, IAuxiliaryConfig auxiliaryConfig, IAuxPort additionalAux = null) : base(container)
 		{
 			container.AddComponent(this);
-			EngineStopStartUtilityFactor = container.RunData?.DriverData?.EngineStopStart?.UtilityFactorStandstill ?? double.NaN;
+			EngineStopStartUtilityFactor = 1; // container.RunData?.DriverData?.EngineStopStart?.UtilityFactorStandstill ?? double.NaN;
 
 			CurrentState = new BusAuxState();
 			PreviousState = new BusAuxState { AngularSpeed = container.EngineInfo.EngineIdleSpeed };
