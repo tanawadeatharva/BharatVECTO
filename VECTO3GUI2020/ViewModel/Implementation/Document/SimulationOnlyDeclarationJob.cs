b14adb06f13fc1278cfae067960ab445d72f82c1
harald.martini@student.tugraz.at <harald.martini@student.tugraz.at>
2021-05-25 10:46:01 +0200

    Added Document Viewmodel for not editable jobs

---
+++ VECTO3GUI2020/ViewModel/Implementation/Document/SimulationOnlyDeclarationJob.cs
@@ -0,0 +1,62 @@
+﻿using System.Configuration;
+using TUGraz.VectoCommon.InputData;
+using TUGraz.VectoCore.Utils;
+using VECTO3GUI2020.ViewModel.Implementation.Common;
+using VECTO3GUI2020.ViewModel.Interfaces;
+using VECTO3GUI2020.ViewModel.Interfaces.Document;
+
+namespace VECTO3GUI2020.ViewModel.Implementation.Document
+{
+	public class SimulationOnlyDeclarationJob : ViewModelBase, IDocumentViewModel, IJobViewModel
+	{
+		#region Implementation of IDocumentViewModel
+
+		public string DocumentName
+		{
+			get => _documentName;
+			set => _documentName = value;
+		}
+
+		public XmlDocumentType DocumentType
+		{
+			get => _documentType;
+			set => _documentType = value;
+		}
+
+		public DataSource DataSource
+		{
+			get => _dataSource;
+			set => _dataSource = value;
+		}
+
+		public IEditViewModel EditViewModel { get; set; }
+
+		private bool _selected;
+		private XmlDocumentType _documentType;
+		private string _documentName;
+		private DataSource _dataSource;
+
+		public bool Selected
+		{
+			get => _selected;
+			set => SetProperty(ref _selected, value);
+		}
+
+		public bool CanBeEdited
+		{
+			get => false;
+			set => throw new System.NotImplementedException();
+		}
+
+		#endregion
+
+		public SimulationOnlyDeclarationJob(DataSource dataSource, string name, XmlDocumentType documentType)
+		{
+			_documentType = documentType;
+			_dataSource = dataSource;
+			_documentName = name;
+		}
+
+	}
+
+}
