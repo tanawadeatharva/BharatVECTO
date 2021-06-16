8f9473fe47030e1627b697101f4d77d3d2e38479
harald.martini@student.tugraz.at <harald.martini@student.tugraz.at>
2021-06-16 10:47:53 +0200

    Added Settings Tab and About View

---
+++ VECTO3GUI2020/ViewModel/Implementation/AboutViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.Input;
+using VECTO3GUI2020.ViewModel.Implementation.Common;
+using VECTO3GUI2020.ViewModel.Interfaces;
+
+namespace VECTO3GUI2020.ViewModel.Implementation
+{
+	public class AboutViewModel : ViewModelBase, IMainViewModel
+	{
+		#region Members
+
+		private ICommand _euplLinkClickedCommand;
+		private ICommand _mailClickedCommand;
+		private ICommand _jrcPicClickedCommand;
+
+		#endregion
+
+		#region Properties
+
+		public string EUPLLink { get; set; }
+		public string JRCMail { get; set; }
+		public string JRCPic { get; set; }
+
+		#endregion
+
+		public AboutViewModel()
+		{
+			EUPLLink = "https://joinup.ec.europa.eu/community/eupl/og_page/eupl";
+			JRCMail = "mailto:jrc-vecto@ec.europa.eu";
+			JRCPic = "http://ec.europa.eu/dgs/jrc/index.cfm";
+		}
+		
+		#region Commands
+
+		public ICommand EUPLLinkClickedCommand
+		{
+			get
+			{
+				return _euplLinkClickedCommand ?? (_euplLinkClickedCommand = new RelayCommand(DoLinkClickedCommand));
+			}
+		}
+
+		private void DoLinkClickedCommand()
+		{
+			Process.Start(EUPLLink);
+		}
+
+		public ICommand MailClickedCommand
+		{
+			get { return _mailClickedCommand ?? (_mailClickedCommand = new RelayCommand(DoMailClickedCommand)); }
+		}
+
+		private void DoMailClickedCommand()
+		{
+			Process.Start(JRCMail);
+		}
+
+		public ICommand JrcPicClickedCommand
+		{
+			get { return _jrcPicClickedCommand ?? (_jrcPicClickedCommand = new RelayCommand(DoJrcPicClickedCommand)); }
+		}
+
+		private void DoJrcPicClickedCommand()
+		{
+			Process.Start(JRCPic);
+		}
+		#endregion
+
+
+	}
+}
