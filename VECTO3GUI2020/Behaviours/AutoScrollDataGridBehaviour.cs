c2a3fde29b4e8dd0db9a73bef10c9b16e891e91d
harald.martini@student.tugraz.at <harald.martini@student.tugraz.at>
2021-06-21 10:30:57 +0200

    Added Autoscrollbehavior to OutputView

---
+++ VECTO3GUI2020/Behaviours/AutoScrollDataGridBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace VECTO3GUI2020.Behaviours
+{
+    public class AutoScrollDataGridBehaviour : Behavior<DataGrid>
+    {
+		#region Overrides of Behavior
+
+		private INotifyCollectionChanged sourceCollection;
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+
+			//subsrice to Itemssource
+			var dpd = DependencyPropertyDescriptor.FromProperty(ItemsControl.ItemsSourceProperty, typeof(DataGrid));
+			if (dpd != null)
+			{
+				dpd.AddValueChanged(this.AssociatedObject, OnItemsSourceChanged);
+			}
+
+
+		}
+
+		private void OnItemsSourceChanged(object sender, EventArgs e)
+		{
+			UnSubscribeFromSourceCollectionChanged();
+
+			sourceCollection = AssociatedObject.ItemsSource as INotifyCollectionChanged;
+			if (sourceCollection != null) {
+				sourceCollection.CollectionChanged += SourceCollectionChanged;
+			}
+		}
+
+		private void SourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		{
+			var newIndex = e.NewStartingIndex;
+			this.AssociatedObject.ScrollIntoView(this.AssociatedObject.Items[newIndex]);
+		}
+
+
+		protected override void OnDetaching()
+		{
+			base.OnDetaching();
+			UnSubscribeFromSourceCollectionChanged();
+		}
+
+		private void UnSubscribeFromSourceCollectionChanged()
+		{
+			if (sourceCollection != null) {
+				sourceCollection.CollectionChanged -= SourceCollectionChanged;
+			}
+		}
+
+		#endregion
+	}
+}
