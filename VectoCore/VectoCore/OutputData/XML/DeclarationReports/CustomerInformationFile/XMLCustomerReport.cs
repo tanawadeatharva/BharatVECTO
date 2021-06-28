a4e28177cd320d39d3c06085d2f186b0b9ed0147
harald.martini@student.tugraz.at <harald.martini@student.tugraz.at>
2021-06-28 09:19:15 +0200

    Replaced ToMinSignificantDigits with ToXmlFormat in XMLCustomerReport

--- VectoCore/VectoCore/OutputData/XML/DeclarationReports/CustomerInformationFile/XMLCustomerReport.cs
+++ VectoCore/VectoCore/OutputData/XML/DeclarationReports/CustomerInformationFile/XMLCustomerReport.cs
@@ -220,7 +220,7 @@ namespace TUGraz.VectoCore.OutputData.XML.DeclarationReports.CustomerInformation
 					XMLHelper.ValueAsUnit(result.TotalVehicleMass, XMLNames.Unit_kg)),
 				new XElement(tns + XMLNames.Report_ResultEntry_Payload, XMLHelper.ValueAsUnit(result.Payload, XMLNames.Unit_kg)),
 				result.PassengerCount.HasValue && result.PassengerCount.Value > 0
-					? new XElement(tns + "PassengerCount", result.PassengerCount.Value.ToXMLFormat(3, 1))
+					? new XElement(tns + "PassengerCount", result.PassengerCount.Value.ToXMLFormat(3))
 					: null,
 				new XElement(tns + XMLNames.Report_Result_FuelMode,
 					result.FuelData.Count > 1 ? XMLNames.Report_Result_FuelMode_Val_Dual : XMLNames.Report_Result_FuelMode_Val_Single),
