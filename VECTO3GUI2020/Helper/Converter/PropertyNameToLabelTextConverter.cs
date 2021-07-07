fda6d9801f16f7adbd608786f1c14ea036e9472b
harald.martini@student.tugraz.at <harald.martini@student.tugraz.at>
2021-07-07 13:59:14 +0200

    Added PropertyNameToLabelTextConverter

---
+++ VECTO3GUI2020/Helper/Converter/PropertyNameToLabelTextConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using VECTO3GUI2020.Properties;
+
+namespace VECTO3GUI2020.Helper.Converter
+{
+	public class PropertyNameToLabelTextConverter : IValueConverter
+	{
+		#region Implementation of IValueConverter
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return NameResolver.ResolveName(value as string, BusStrings.ResourceManager, Strings.ResourceManager) ?? Binding.DoNothing;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
