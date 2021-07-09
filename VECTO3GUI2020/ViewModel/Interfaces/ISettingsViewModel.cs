aabe6df91ce423147320a5511de66abd0be3d572
harald.martini@student.tugraz.at <harald.martini@student.tugraz.at>
2021-07-09 07:47:46 +0200

    Added Properties to ISettingsViewModel

--- VECTO3GUI2020/ViewModel/Interfaces/ISettingsViewModel.cs
+++ VECTO3GUI2020/ViewModel/Interfaces/ISettingsViewModel.cs
@@ -2,8 +2,12 @@
 {
     public interface ISettingsViewModel : IMainViewModel
     {
-
-
-
-    }
+		string DefaultFilePath { get; set; }
+		string DefaultOutputPath { get; set; }
+		bool SerializeVectoRunData { get; set; }
+		bool ActualModalData { get; set; }
+		bool Validate { get; set; }
+		bool ModalResults1Hz { get; set; }
+		bool WriteModalResults { get; set; }
+	}
 }
