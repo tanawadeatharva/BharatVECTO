ae55e7da3fc8e4030cdbd89163b9d837a0428037
harald.martini@student.tugraz.at <harald.martini@student.tugraz.at>
2021-07-13 20:00:13 +0200

    removed file: IMultistageDocumentViewModelFactory.cs

--- VECTO3GUI2020/ViewModel/Interfaces/Document/IMultistageDocumentViewModelFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using TUGraz.VectoCommon.InputData;
-
-namespace VECTO3GUI2020.ViewModel.Interfaces.Document
-{
-	public interface IMultistageDocumentViewModelFactory
-	{
-		IDocumentViewModel CreateDocumentViewModel(IDeclarationInputDataProvider inputData);
-	}
-}
