a43cc984f590d5c1bb1252a1886415f1b1d546a3
harald.martini@student.tugraz.at <harald.martini@student.tugraz.at>
2021-08-03 15:47:03 +0200

    Added PathHelper to create relative paths in JobFiles,
    Added PathHelper testclass
    
    Added MultistageMultipleRunsTest Class

---
+++ VectoCore/VectoCoreTest/Integration/Multistage/MultistageMultipleRunsTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using TUGraz.VectoCommon.InputData;
+using TUGraz.VectoCommon.Models;
+using TUGraz.VectoCore.InputData.FileIO.JSON;
+using TUGraz.VectoCore.InputData.FileIO.XML.Declaration.DataProvider;
+using TUGraz.VectoCore.Models.Simulation.Impl;
+using TUGraz.VectoCore.OutputData;
+using TUGraz.VectoCore.OutputData.FileIO;
+
+namespace TUGraz.VectoCore.Tests.Integration.Multistage
+{
+	[TestFixture]
+	public class MultistageMultipleRunsTest
+	{
+		//public const 
+		[SetUp]
+		public void SetUp()
+		{
+
+		}
+
+		[TestCase]
+		public void ExemptedPrimaryAndCompletedTest()
+		{
+
+		}
+
+		[TestCase]
+		public void ExemptedPrimaryAndInterimTest()
+		{
+
+		}
+
+		[TestCase]
+		public void PrimaryAndCompletedTest()
+		{
+
+
+
+		}
+
+		[TestCase]
+		public void PrimaryAndInterimTest()
+		{
+
+
+
+		}
+
+
+
+
+		
+	}
+}
