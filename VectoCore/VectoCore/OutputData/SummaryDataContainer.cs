b4f883652ae62bf809d9316c388a89f6d5043811
Markus Quaritsch <markus.quaritsch@tugraz.at>
2021-08-18 17:06:44 +0200

    bugfix sum-data

--- VectoCore/VectoCore/OutputData/SummaryDataContainer.cs
+++ VectoCore/VectoCore/OutputData/SummaryDataContainer.cs
@@ -667,7 +667,7 @@ namespace TUGraz.VectoCore.OutputData
 				row[Fields.E_PS_CompressorOff] = modData.EnergyPneumaticCompressorPowerOff().ConvertToKiloWattHour();
 				row[Fields.E_PS_CompressorOn] = modData.EnergyPneumaticCompressorOn().ConvertToKiloWattHour();
 
-				row[Fields.E_BusAux_ES_generated] = modData.EnergyBusAuxESGeneratedMech().ConvertToKiloWattHour();
+				row[Fields.E_BusAux_ES_generated] = modData.EnergyBusAuxESGenerated().ConvertToKiloWattHour();
 				row[Fields.E_BusAux_ES_consumed] = modData.EnergyBusAuxESConsumed().ConvertToKiloWattHour();
 				row[Fields.Delta_E_BusAux_Battery] = (runData.BusAuxiliaries.ElectricalUserInputsConfig.AlternatorType == AlternatorType.Smart
 						? modData.DeltaSOCBusAuxBattery() * runData.BusAuxiliaries.ElectricalUserInputsConfig.ElectricStorageCapacity
