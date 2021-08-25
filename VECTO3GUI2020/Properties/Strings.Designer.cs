4ab5c62ea5a3afa6c9dfd2ee69dc337f8bdd6373
harald.martini@student.tugraz.at <harald.martini@student.tugraz.at>
2021-08-25 12:14:58 +0200

    Added VehicleTypeApprovalNumber to Strings

--- VECTO3GUI2020/Properties/Strings.Designer.cs
+++ VECTO3GUI2020/Properties/Strings.Designer.cs
@@ -638,6 +638,15 @@ namespace VECTO3GUI2020.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Vehicle Type Approval Number.
+        /// </summary>
+        public static string VehicleTypeApprovalNumber {
+            get {
+                return ResourceManager.GetString("VehicleTypeApprovalNumber", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Vehicle Identification Number.
         /// </summary>
         public static string VehicleViewModel_v1_0_VIN {
