5679abecde5dd5f570c2ed31b12fc912df2cef67
Markus Quaritsch <markus.quaritsch@tugraz.at>
2021-09-15 13:09:09 +0200

    adding missing includes

--- VectoCore/VectoCore/Models/Declaration/CompletedBusSegments.cs
+++ VectoCore/VectoCore/Models/Declaration/CompletedBusSegments.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using TUGraz.VectoCommon.BusAuxiliaries;
 using TUGraz.VectoCommon.Exceptions;
