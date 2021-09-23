3f5028ff7a264620fe538dd55b87a5aab8ac6ead
Michael Krisper <michael.krisper@tugraz.at>
2021-09-23 16:44:07 +0200

    corrected bug with braking for one single timestep at the end of PCC-UseCase1 when target speed was reached.

--- VectoCore/VectoCore/Models/SimulationComponent/Impl/DefaultDriverStrategy.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Impl/DefaultDriverStrategy.cs
@@ -1039,7 +1039,7 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 			bool prohibitOverspeed, MeterPerSecond velocityWithOverspeed, DebugData debug)
 		{
 			if (DriverStrategy._PCCState == PCCStates.UseCase1 || DriverStrategy._PCCState == PCCStates.UseCase2) {
-				return Driver.DrivingActionCoast(absTime, ds, targetVelocity, gradient);
+				return Driver.DrivingActionCoast(absTime, ds, velocityWithOverspeed, gradient);
 			}
 
 			IResponse first;
