9e4ba0eb540d53bfb1eef83a2378bb914ca4c8cb
Harald Martini <martini@ivt.tugraz.at>
2021-10-20 13:58:42 +0200

    Added XMLDeclarationReportFactory

---
+++ VectoCore/VectoCore/OutputData/XML/XMLDeclarationReportFactoryNinjectModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Ninject.Extensions.Factory;
+using TUGraz.VectoCore.OutputData.XML.DeclarationReports.VehicleInformationFile;
+using TUGraz.VectoCore.Utils.Ninject;
+
+namespace TUGraz.VectoCore.OutputData.XML
+{
+	public class XMLReportFactoryNinjectModule : AbstractNinjectModule
+	{
+		#region Overrides of NinjectModule
+
+		public override void Load()
+		{
+			Bind<IXMLDeclarationReportFactory>().To<XMLDeclarationReportFactory>();
+
+		}
+
+		#endregion
+	}
+}
