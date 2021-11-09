571c39a91dac1cc26f659416b7bb8fcc000d87af
Michael Krisper <michael.krisper@tugraz.at>
2021-11-09 18:04:03 +0100

    Hybrid P1: added check in search for BrakeAction if Combustion Engine was off

--- VectoCore/VectoCore/Models/SimulationComponent/Impl/StopStartCombustionEngine.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Impl/StopStartCombustionEngine.cs
@@ -30,8 +30,6 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 
 		public override bool CombustionEngineOn { get; set; }
 
-		public override bool EngineOn => CombustionEngineOn;
-
 		#region Overrides of CombustionEngine
 
 		public override IResponse Request(Second absTime, Second dt, NewtonMeter outTorque, PerSecond outAngularVelocity, bool dryRun)
--- VectoCore/VectoCore/Models/SimulationComponent/Strategies/HybridStrategy.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Strategies/HybridStrategy.cs
@@ -1191,9 +1191,21 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Strategies
 					return;
 				}
 
-				// full recuperation is not possible - ICE would need to propel - search max possible EM torque
-				DataBus.EngineCtl.CombustionEngineOn = true;
 
+				// if Hybrid P1 and Engine is off, the search for braking power is not possible:
+				// Therefore, switch on engine (if reasonable), otherwise simply do NOT search for mechanicalAssistPower.
+				if (emPos == PowertrainPosition.HybridP1 && !DataBus.EngineCtl.CombustionEngineOn) {
+					
+					var gearboxOut = firstResponse.Gearbox.OutputSpeed * firstResponse.Gearbox.OutputTorque;
+					var elMotor = DataBus.ElectricMotorInfo(PowertrainPosition.HybridP1);
+					var powerToleranceEl = 0.1 * elMotor.MaxPowerDrive(DataBus.BatteryInfo.InternalVoltage, firstResponse.Engine.EngineSpeed);
+					if (gearboxOut - firstResponse.Engine.DragPower < powerToleranceEl)
+						DataBus.EngineCtl.CombustionEngineOn = true;
+					else
+						return;
+				}
+
+				// full recuperation is not possible - ICE would need to propel - search max possible EM torque
 				var emRecuperationTq = SearchAlgorithm.Search(
 					maxRecuperationResponse.ElectricMotor.ElectricMotorPowerMech /
 					maxRecuperationResponse.ElectricMotor.AngularVelocity,
@@ -1210,7 +1222,7 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Strategies
 					},
 					evaluateFunction: emTq => {
 						var cfg = new HybridStrategyResponse {
-							CombustionEngineOn = DataBus.EngineInfo.EngineOn,
+							CombustionEngineOn = DataBus.EngineCtl.CombustionEngineOn,
 							GearboxInNeutral = false,
 							NextGear = nextGear,
 							MechanicalAssistPower = new Dictionary<PowertrainPosition, Tuple<PerSecond, NewtonMeter>> {
