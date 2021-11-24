5ee5912b0be00649e91bdde46e94f316cab2aef3
Harald Martini <harald.martini@student.tugraz.at>
2021-11-24 09:14:31 +0100

    use different docker image with older pandoc version

--- Documentation/MultiStepTool/build_in_docker.sh
+++ Documentation/MultiStepTool/build_in_docker.sh
@@ -1,2 +1,2 @@
-docker build -t pandoc/latex/bash .
-docker run --rm --volume /mnt/c/Users/Harry/source/repos/hm_vecto-dev/Documentation/MultiStepTool:/data --entrypoint /data/convert.sh pandoc/latex/bash
+docker build -t ubuntu_pandoc .
+docker run --rm --volume /mnt/c/Users/Harry/source/repos/hm_vecto-dev/Documentation/MultiStepTool:/documentation --entrypoint /documentation/convert.sh ubuntu_pandoc
--- Documentation/MultiStepTool/convert.sh
+++ Documentation/MultiStepTool/convert.sh
@@ -3,14 +3,21 @@
 
 #readarray [-d delim] [-n count] [-O origin] [-s count]
  #   [-t] [-u fd] [-C callback] [-c quantum] [array]
+#ls
+
+cd documentation
+
+#ls
 
 declare -a FILES
 mapfile -t FILES < files.txt
 cat files.txt
+echo "Running pandoc with these input files"
 echo "${FILES[@]}"
+
+OUTPUTFILE="MultiStepToolHelp.html"
 
 #pandoc --verbose  -f markdown+raw_html %LIST% -s -S --toc --toc-depth=2 --self-contained --email-obfuscation=none --section-divs --webtex -c includes/style.css -c includes/print.css -B includes/header.html -A includes/footer.html -o HashingToolHelp.html  -H includes/jquery.js -H includes/jquery-ui.js -H includes/include.js
-pandoc --verbose  -f markdown+raw_html+smart ${FILES[@]} -s --toc --toc-depth=2 --self-contained --email-obfuscation=none --section-divs --webtex -c includes/style.css -c includes/print.css -B includes/header.html -A includes/footer.html -o HashingToolHelp.html  -H includes/jquery.js -H includes/jquery-ui.js -H includes/include.js
+pandoc --verbose -f markdown+raw_html ${FILES[@]} -s -S --toc --toc-depth=2 --self-contained --email-obfuscation=none --section-divs --webtex -c includes/style.css -c includes/print.css -B includes/header.html -A includes/footer.html -o $OUTPUTFILE  -H includes/jquery.js -H includes/jquery-ui.js -H includes/include.js
 
 
-echo "${FILES[@]}"
