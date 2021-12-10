12bcf47fd54e689bed91c1228891a2400934095c
Michael Krisper <michael.krisper@tugraz.at>
2021-12-10 15:07:05 +0100

    GearboxType formatted Names of GearboxType Enum

--- VectoCommon/VectoCommon/Models/GearboxType.cs
+++ VectoCommon/VectoCommon/Models/GearboxType.cs
@@ -53,9 +53,9 @@ namespace TUGraz.VectoCommon.Models
 			switch (type) {
 				case GearboxType.MT:           return "Manual Transmission (MT)";
 				case GearboxType.AMT:          return "Automated Transmission (AMT)";
-				case GearboxType.ATSerial:     return "Automatic Transmission - Serial (AT-S)";
-				case GearboxType.ATPowerSplit: return "Automatic Transmission - PowerSplit (AT-P)";
-				case GearboxType.APTN:         return "Automatic Transmission - No TorqueConverter (APT-N)";
+				case GearboxType.ATSerial:     return "Automatic Transmission - Serial (AT-S)"; //todo mk20211210 shouldn't we call it APT-S?
+				case GearboxType.ATPowerSplit: return "Automatic Transmission - PowerSplit (AT-P)";//todo mk20211210 shouldn't we call it APT-P?
+				case GearboxType.APTN:         return "Automatic Transmission - No Torque Converter (APT-N)";
 				case GearboxType.DrivingCycle: return "Gear from Driving Cycle";
 				default: throw new ArgumentOutOfRangeException("GearboxType", type, null);
 			}
@@ -84,12 +84,12 @@ namespace TUGraz.VectoCommon.Models
 		public static string ToXMLFormat(this GearboxType type)
 		{
 			switch (type) {
-				case GearboxType.MT: return "SMT";
-				case GearboxType.AMT: return "AMT";
-				case GearboxType.DrivingCycle: return type.ToString();
-				case GearboxType.ATSerial: return "APT-S";
+				case GearboxType.MT:           return "SMT";
+				case GearboxType.AMT:          return "AMT";
+				case GearboxType.DrivingCycle: return "DrivingCycle";
+				case GearboxType.ATSerial:     return "APT-S";
 				case GearboxType.ATPowerSplit: return "APT-P";
-				case GearboxType.APTN: return "APT-N";
+				case GearboxType.APTN:         return "APT-N";
 				default: throw new ArgumentOutOfRangeException("GearboxType", type, null);
 			}
 		}
