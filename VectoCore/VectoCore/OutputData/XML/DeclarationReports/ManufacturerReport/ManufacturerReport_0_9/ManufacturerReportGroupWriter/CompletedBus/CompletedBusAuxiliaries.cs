b5bf692e6a3e8549156fea3c229736ce410b9035
Harald Martini <harald.martini@student.tugraz.at>
2022-01-20 12:51:26 +0100

    updated folder structure

---
+++ VectoCore/VectoCore/OutputData/XML/DeclarationReports/ManufacturerReport/ManufacturerReport_0_9/ManufacturerReportGroupWriter/CompletedBus/CompletedBusAuxiliaries.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUGraz.VectoCore.OutputData.XML.DeclarationReports.ManufacturerReport.ManufacturerReport_0_9.ManufacturerReportGroupWriter.CompletedBus
+{
+    //internal class CompletedBusAuxiliaries : 
+    //{
+    //}
+}
