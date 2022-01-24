85c47a99e06ff2b5fe8ea0f2ea017ab0d808d5f6
Michael Krisper <michael.krisper@tugraz.at>
2022-01-24 15:18:21 +0100

    removed check for ResponseEngineSpeedToHigh from DistanceRun again.

--- VectoCore/VectoCore/Models/Simulation/Impl/DistanceRun.cs
+++ VectoCore/VectoCore/Models/Simulation/Impl/DistanceRun.cs
@@ -79,6 +79,9 @@ namespace TUGraz.VectoCore.Models.Simulation.Impl
 						FinishedWithoutErrors = true;
 						Log.Info("========= REESS empty");
 						break;
+					//case ResponseEngineSpeedTooHigh _:
+					//	Log.Info("DistanceRun got ResponseEngineSpeedTooHigh. Trying again.");
+					//	break;
 					default:
 						throw new VectoException("DistanceRun got an unexpected response: {0}", response);
 				}
