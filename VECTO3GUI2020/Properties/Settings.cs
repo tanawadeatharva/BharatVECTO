40cd6073eb802d1c210743b38ea23e131544f8da
Michael Krisper <michael.krisper@tugraz.at>
2022-02-08 15:02:26 +0100

    [VECTO3GUI2020] Removed unnecessary files

--- VECTO3GUI2020/Properties/Settings.cs
+++ VECTO3GUI2020/Properties/Settings.cs
@@ -9,7 +9,7 @@ namespace VECTO3GUI2020.Properties {
     //  The PropertyChanged event is raised after a setting's value is changed.
     //  The SettingsLoaded event is raised after the setting values are loaded.
     //  The SettingsSaving event is raised before the setting values are saved.
-    public sealed partial class Settings {
+    internal sealed partial class Settings {
         
         public Settings() {
             // // To add event handlers for saving and changing settings, uncomment the lines below:
