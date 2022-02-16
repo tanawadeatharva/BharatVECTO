c5681336298e1b81f7569d8e1e89e55639486ee8
VKMTHD\franzjosefkober <franz.josef.kober@ivt.tugraz.at>
2022-02-16 14:41:06 +0100

    updated hash sort test

--- VectoCommon/VectoHashingTest/VectoHashSortTest.cs
+++ VectoCommon/VectoHashingTest/VectoHashSortTest.cs
@@ -1,103 +1,79 @@
 ﻿using System.IO;
-using Ninject;
 using NUnit.Framework;
 using TUGraz.VectoCommon.Hashing;
-using TUGraz.VectoCore;
-using TUGraz.VectoCore.InputData.FileIO.XML;
 using TUGraz.VectoHashing;
 
 namespace VectoHashingTest
 {
 	public class VectoHashSortTest
 	{
-		private IXMLInputDataReader xmlInputReader;
-		private IKernel _kernel;
 		private const string UnsortedComponentPath = @"Testdata\XML\Sort\Component\Unsorted\";
-		private const  string SortedComponentPath = @"Testdata\XML\Sort\Component\Sorted\";
+		private const string SortedComponentPath = @"Testdata\XML\Sort\Component\Sorted\";
 
 		[OneTimeSetUp]
 		public void RunBeforeAnyTests()
 		{
 			Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
-
-			_kernel = new StandardKernel(new VectoNinjectModule());
-			xmlInputReader = _kernel.Get<IXMLInputDataReader>();
 		}
-
-		[TestCase(SortedComponentPath + "TrailerAerodynamicDevice.xml")]
-		public void TestHash(string filePath)
-		{
-			var loadedFile = VectoHash.Load(filePath);
-			var hash = loadedFile.ComputeHash(VectoComponents.CertifiedAeroReduction);
-		}
-
-		[TestCase("ttvMnXYdQrEAu47QEO2AKyfzOdPSlcdsR/MrmH/mX+k=", SortedComponentPath + "Engine.xml"),
-		TestCase("ttvMnXYdQrEAu47QEO2AKyfzOdPSlcdsR/MrmH/mX+k=",  UnsortedComponentPath + "Engine.xml")]
+		
+		[TestCase("sYOwPn3VlMw71bjQFfACuekdAjZ8QRT3S8c71w7810A=", SortedComponentPath + "Engine.xml"),
+		TestCase("sYOwPn3VlMw71bjQFfACuekdAjZ8QRT3S8c71w7810A=",  UnsortedComponentPath + "Engine.xml")]
 		public void TestEngineHashSort(string expectedHash, string filePath)
 		{
 			var loadedFile = VectoHash.Load(filePath);
 			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.Engine));
 		}
 
-
-		[TestCase("LrsR3WfAkFia53dMiwGEIeAiA+9bdWvaS6x7dIL9yiQ=", SortedComponentPath + "Gearbox_FWD.xml"),
-		 TestCase("LrsR3WfAkFia53dMiwGEIeAiA+9bdWvaS6x7dIL9yiQ=", UnsortedComponentPath + "Gearbox_FWD.xml"),
-		 TestCase("hRzWfx3/El/LwEtP86Utm3dgBAF6CagrpEREsca7+/0=", SortedComponentPath + "Gearbox_APT-N.xml"),
-		 TestCase("hRzWfx3/El/LwEtP86Utm3dgBAF6CagrpEREsca7+/0=", UnsortedComponentPath + "Gearbox_APT-N.xml"),
-		 TestCase("XZCluPiG05mOAj5rTjTllCWbhCTEYVxCbE940ck3XsA=", SortedComponentPath + "Gearbox_IHPC.xml"),
-		 TestCase("XZCluPiG05mOAj5rTjTllCWbhCTEYVxCbE940ck3XsA=", UnsortedComponentPath + "Gearbox_IHPC.xml")]
-		public void TestGearboxComponentHashSort(string expectedHash, string filePath)
+		[TestCase("lYoUAB6Xob1azFaCPJBLK1HIT5Jr0K24H2jJec3r5BM=", SortedComponentPath + "Gearbox_APT-N.xml"),
+		TestCase("lYoUAB6Xob1azFaCPJBLK1HIT5Jr0K24H2jJec3r5BM=", UnsortedComponentPath + "Gearbox_APT-N.xml")]
+		public void TestGearboxAPT_NComponentHashSort(string expectedHash, string filePath)
 		{
 			var loadedFile = VectoHash.Load(filePath);
 			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.Gearbox));
 		}
 		
-		[TestCase("og41nROicUT1OoesUTZ9/uoroCUbqlTo7nKmCRYQap4=", SortedComponentPath + "Axlegear.xml"),
-		TestCase("og41nROicUT1OoesUTZ9/uoroCUbqlTo7nKmCRYQap4=", UnsortedComponentPath + "Axlegear.xml")]
+		[TestCase("BlXQyrK6qVsW5MHt74jB1Y1+a9toEwNC5KXYUmKawfQ=", SortedComponentPath + "Gearbox_FWD.xml"),
+		TestCase("BlXQyrK6qVsW5MHt74jB1Y1+a9toEwNC5KXYUmKawfQ=", UnsortedComponentPath + "Gearbox_FWD.xml")]
+		public void TestGearboxFWDComponentHashSort(string expectedHash, string filePath)
+		{
+			var loadedFile = VectoHash.Load(filePath);
+			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.Gearbox));
+		}
+
+		[TestCase("QVPf1HhUClM3JoxJHTVqcp2gPXMiqaZ5vFTVyjnG1Co=", SortedComponentPath + "Gearbox_IHPC.xml"),
+		TestCase("QVPf1HhUClM3JoxJHTVqcp2gPXMiqaZ5vFTVyjnG1Co=", UnsortedComponentPath + "Gearbox_IHPC.xml")]
+		public void TestGearboxIHPCComponentHashSort(string expectedHash, string filePath)
+		{
+			var loadedFile = VectoHash.Load(filePath);
+			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.Gearbox));
+		}
+		
+		[TestCase("MCdQW6mfKSC6WVU+1A5UVLkxKc7eBbg4GWQp6KpzjEw=", SortedComponentPath + "Axlegear.xml"),
+		TestCase("MCdQW6mfKSC6WVU+1A5UVLkxKc7eBbg4GWQp6KpzjEw=", UnsortedComponentPath + "Axlegear.xml")]
 		public void TestAxlegearComponentHashSort(string expectedHash, string filePath)
 		{
 			var loadedFile = VectoHash.Load(filePath);
 			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.Axlegear));
 		}
-
-
-		[TestCase("uMH8HJlAtm/SNaj8QOhuX/cBNXTHAZ1la3oEXI42bls=", SortedComponentPath + "Retarder.xml"),
-		TestCase("uMH8HJlAtm/SNaj8QOhuX/cBNXTHAZ1la3oEXI42bls=", UnsortedComponentPath + "Retarder.xml")]
-		public void TestRetarderComponentHashSort(string expectedHash, string filePath)
-		{
-			var loadedFile = VectoHash.Load(filePath);
-			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.Retarder));
-		}
-
-
-		[TestCase("o5/ZtRQ8nAui3rhM6adKiHVU9jRync6f1cDlBcnyOg4=", SortedComponentPath + "TorqueConverter.xml"),
-		TestCase("o5/ZtRQ8nAui3rhM6adKiHVU9jRync6f1cDlBcnyOg4=", UnsortedComponentPath + "TorqueConverter.xml")]
-		public void TestTorqueConverterComponentHashSort(string expectedHash, string filePath)
-		{
-			var loadedFile = VectoHash.Load(filePath);
-			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.TorqueConverter));
-		}
-
-
-		[TestCase("FGaItzPcx1qGytVlroFp+PU9rzsaQWRWnxmIspkqCRM=", SortedComponentPath + "Angledrive.xml"),
-		TestCase("FGaItzPcx1qGytVlroFp+PU9rzsaQWRWnxmIspkqCRM=", UnsortedComponentPath + "Angledrive.xml")]
+		
+		[TestCase("lFLmCC4J39gaYU+VNw4q6ScyWOBzKoeVqmQ8/mQizBQ=", SortedComponentPath + "Angledrive.xml"),
+		TestCase("lFLmCC4J39gaYU+VNw4q6ScyWOBzKoeVqmQ8/mQizBQ=", UnsortedComponentPath + "Angledrive.xml")]
 		public void TestAngledriveComponentHashSort(string expectedHash, string filePath)
 		{
 			var loadedFile = VectoHash.Load(filePath);
 			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.Angledrive));
 		}
-
-
-		[TestCase("NiyH2Xp0rQswwXIOf52Jm0wvK4Yc2/PL/T+zQCWQGFo=", SortedComponentPath + "ADC.xml"),
-		 TestCase("NiyH2Xp0rQswwXIOf52Jm0wvK4Yc2/PL/T+zQCWQGFo=", UnsortedComponentPath + "ADC.xml")]
+		
+		[TestCase("HWRjRcjJ/S1JBoUv+xjhWQDAsRn5c7D4LbwB04yyxrQ=", SortedComponentPath + "ADC.xml"),
+		 TestCase("HWRjRcjJ/S1JBoUv+xjhWQDAsRn5c7D4LbwB04yyxrQ=", UnsortedComponentPath + "ADC.xml")]
 		public void TestADCHashSort(string expectedHash, string filePath)
 		{
 			var loadedFile = VectoHash.Load(filePath);
 			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.ADC));
 		}
 
-		[TestCase("tam1LGpdznHGFGo+rp0WVr0/6+F2yU2Kv4G4tYvAe+Y=", SortedComponentPath + "BatterySystem_1.xml"),
-		TestCase("tam1LGpdznHGFGo+rp0WVr0/6+F2yU2Kv4G4tYvAe+Y=", UnsortedComponentPath + "BatterySystem_1.xml")]
+		[TestCase("MNKcycaIzgndCWPEFDp84PlujKPkb9QaOxA8yKGNiKI=", SortedComponentPath + "BatterySystem_1.xml"),
+		TestCase("MNKcycaIzgndCWPEFDp84PlujKPkb9QaOxA8yKGNiKI=", UnsortedComponentPath + "BatterySystem_1.xml")]
 		public void TestBatterySystemHashSort(string expectedHash, string filePath)
 		{
 			var loadedFile = VectoHash.Load(filePath);
@@ -112,29 +88,62 @@ namespace VectoHashingTest
 			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.CapacitorSystem));
 		}
 
-		[TestCase("X5dgavua/V/jzBQeJ6SxZXsXm3i1jruL48LedzZ5IxU=", SortedComponentPath + "ElectricMachineSystem-IHPC_1.xml"),
-		 TestCase("X5dgavua/V/jzBQeJ6SxZXsXm3i1jruL48LedzZ5IxU=", UnsortedComponentPath + "ElectricMachineSystem-IHPC_1.xml"),
-		 TestCase("wLFLpJxFZ6mDXeqdlZCGVOLCoXTCf7XTL0q9ZKkmt7o=", SortedComponentPath + "ElectricMachineSystem_1.xml"),
-		 TestCase("wLFLpJxFZ6mDXeqdlZCGVOLCoXTCf7XTL0q9ZKkmt7o=", UnsortedComponentPath + "ElectricMachineSystem_1.xml"),
-		 TestCase("CunnDxsiE9kciX+v9oeEGADZpEc88NtfMtmrHyJkCQ0=", SortedComponentPath + "ElectricMachineSystem_StdValues.xml"),
-		 TestCase("CunnDxsiE9kciX+v9oeEGADZpEc88NtfMtmrHyJkCQ0=", UnsortedComponentPath + "ElectricMachineSystem_StdValues.xml")]
+		[TestCase("6VgGv3QToPDXpr7nSg1fupTLZ8qACAOEshZ8l5pUNI4=", SortedComponentPath + "ElectricMachineSystem-IHPC_1.xml"),
+		TestCase("6VgGv3QToPDXpr7nSg1fupTLZ8qACAOEshZ8l5pUNI4=", UnsortedComponentPath + "ElectricMachineSystem-IHPC_1.xml")]
+		public void TestElectricMachineIHPCHashSort(string expectedHash, string filePath)
+		{
+			var loadedFile = VectoHash.Load(filePath);
+			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.ElectricMachineSystem));
+		}
+		
+		[TestCase("wLFLpJxFZ6mDXeqdlZCGVOLCoXTCf7XTL0q9ZKkmt7o=", SortedComponentPath + "ElectricMachineSystem_1.xml"),
+		TestCase("wLFLpJxFZ6mDXeqdlZCGVOLCoXTCf7XTL0q9ZKkmt7o=", UnsortedComponentPath + "ElectricMachineSystem_1.xml")]
 		public void TestElectricMachineHashSort(string expectedHash, string filePath)
 		{
 			var loadedFile = VectoHash.Load(filePath);
 			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.ElectricMachineSystem));
 		}
 
+		[TestCase("qP2WjwhL0iXRHZN7OK1Bz3jEpdP4cuYvOWSeJUu6Rl8=", SortedComponentPath + "ElectricMachineSystem_StdValues.xml"),
+		TestCase("qP2WjwhL0iXRHZN7OK1Bz3jEpdP4cuYvOWSeJUu6Rl8=", UnsortedComponentPath + "ElectricMachineSystem_StdValues.xml")]
+		public void TestElectricMachineStdValuesHashSort(string expectedHash, string filePath)
+		{
+			var loadedFile = VectoHash.Load(filePath);
+			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.ElectricMachineSystem));
+		}
 
 		[TestCase("3L/fYxKTdIwzADHQMnUBPxcNwZNEHM+sKEC2M32UnEA=", SortedComponentPath + "IEPC_1.xml"),
-		 TestCase("3L/fYxKTdIwzADHQMnUBPxcNwZNEHM+sKEC2M32UnEA=", UnsortedComponentPath + "IEPC_1.xml"),
-		 TestCase("BTHs/Hh2SgycIwU5OSuTgU/2SptMvmRFvPXr2X1Y7XQ=", SortedComponentPath + "IEPC_StdValues.xml"),
-		 TestCase("BTHs/Hh2SgycIwU5OSuTgU/2SptMvmRFvPXr2X1Y7XQ=", UnsortedComponentPath + "IEPC_StdValues.xml")]
+		TestCase("3L/fYxKTdIwzADHQMnUBPxcNwZNEHM+sKEC2M32UnEA=", UnsortedComponentPath + "IEPC_1.xml")]
 		public void TestIEPCHashSort(string expectedHash, string filePath)
 		{
 			var loadedFile = VectoHash.Load(filePath);
 			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.IEPC));
 		}
+		
+		[TestCase("BTHs/Hh2SgycIwU5OSuTgU/2SptMvmRFvPXr2X1Y7XQ=", SortedComponentPath + "IEPC_StdValues.xml"),
+		TestCase("BTHs/Hh2SgycIwU5OSuTgU/2SptMvmRFvPXr2X1Y7XQ=", UnsortedComponentPath + "IEPC_StdValues.xml")]
+		public void TestIEPCStdHashSort(string expectedHash, string filePath)
+		{
+			var loadedFile = VectoHash.Load(filePath);
+			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.IEPC));
+		}
 
+		[TestCase("fo5vyZY6IeQgfFpGmNV49YdfJlcEsGycJoNa3qqVChM=", SortedComponentPath + "Retarder.xml"),
+		TestCase("fo5vyZY6IeQgfFpGmNV49YdfJlcEsGycJoNa3qqVChM=", UnsortedComponentPath + "Retarder.xml")]
+		public void TestRetarderComponentHashSort(string expectedHash, string filePath)
+		{
+			var loadedFile = VectoHash.Load(filePath);
+			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.Retarder));
+		}
+		
+		[TestCase("mxPXYmGbF6eUzjxqAp9KXjy96DzMCGm5Xq9WLvNHwVA=", SortedComponentPath + "TorqueConverter.xml"),
+		TestCase("mxPXYmGbF6eUzjxqAp9KXjy96DzMCGm5Xq9WLvNHwVA=", UnsortedComponentPath + "TorqueConverter.xml")]
+		public void TestTorqueConverterComponentHashSort(string expectedHash, string filePath)
+		{
+			var loadedFile = VectoHash.Load(filePath);
+			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.TorqueConverter));
+		}
+		
 		[TestCase("3YVNlu+1souB/4IwePLPoBwhiJormfqMNRxQaZ75wvM=", SortedComponentPath + "TrailerAerodynamicDevice.xml"),
 		TestCase("3YVNlu+1souB/4IwePLPoBwhiJormfqMNRxQaZ75wvM=", UnsortedComponentPath + "TrailerAerodynamicDevice.xml")]
 		public void TestTrailerAerodynamicDeviceSort(string expectedHash, string filePath)
@@ -142,6 +151,5 @@ namespace VectoHashingTest
 			var loadedFile = VectoHash.Load(filePath);
 			Assert.AreEqual(expectedHash, loadedFile.ComputeHash(VectoComponents.CertifiedAeroReduction));
 		}
-
 	}
 }
