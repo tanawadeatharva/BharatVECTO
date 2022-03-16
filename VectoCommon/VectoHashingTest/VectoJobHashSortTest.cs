6052f120092b3384cee8249b0e428daa632c269d
VKMTHD\franzjosefkober <franz.josef.kober@ivt.tugraz.at>
2022-03-16 17:27:59 +0100

    Job test files adapted to schema changes

--- VectoCommon/VectoHashingTest/VectoJobHashSortTest.cs
+++ VectoCommon/VectoHashingTest/VectoJobHashSortTest.cs
@@ -17,8 +17,8 @@ namespace VectoHashingTest
 			Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
 		}
 
-		[TestCase("NBawT5DzeZHW9gIupRB8uNtII+JYLP/JxgiMKMaskpg=", SortedComponentPath + "HEV-S_heavyLorry_IEPC-S.xml")]
-		[TestCase("NBawT5DzeZHW9gIupRB8uNtII+JYLP/JxgiMKMaskpg=", UnsortedComponentPath + "HEV-S_heavyLorry_IEPC-S.xml")]
+		[TestCase("HpFMjk3vmSp7FuZD6FEyBmDyrix7ifdcrKI26a5qEiw=", SortedComponentPath + "HEV-S_heavyLorry_IEPC-S.xml")]
+		[TestCase("HpFMjk3vmSp7FuZD6FEyBmDyrix7ifdcrKI26a5qEiw=", UnsortedComponentPath + "HEV-S_heavyLorry_IEPC-S.xml")]
 		public void TestJobHashSort(string expectedJobHash, string filePath)
 		{
 			var loadedFile = VectoHash.Load(filePath);
