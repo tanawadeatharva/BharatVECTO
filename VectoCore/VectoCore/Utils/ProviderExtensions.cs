294f16b96f9d748123edbb479b3aa00b76a86817
Michael Krisper <michael.krisper@tugraz.at>
2022-03-29 13:41:57 +0200

    PowertrainBuilder: removed unnecessary usings

--- VectoCore/VectoCore/Utils/ProviderExtensions.cs
+++ VectoCore/VectoCore/Utils/ProviderExtensions.cs
@@ -29,15 +29,11 @@
 *   Martin Rexeis, rexeis@ivt.tugraz.at, IVT, Graz University of Technology
 */
 
-using System;
-using System.ServiceModel.Configuration;
-using TUGraz.VectoCommon.Models;
 using TUGraz.VectoCore.Models.Connector.Ports;
 using TUGraz.VectoCore.Models.Simulation;
 using TUGraz.VectoCore.Models.Simulation.Data;
 using TUGraz.VectoCore.Models.Simulation.Impl;
 using TUGraz.VectoCore.Models.SimulationComponent;
-using TUGraz.VectoCore.Models.SimulationComponent.Data;
 using TUGraz.VectoCore.Models.SimulationComponent.Impl;
 
 namespace TUGraz.VectoCore.Utils
