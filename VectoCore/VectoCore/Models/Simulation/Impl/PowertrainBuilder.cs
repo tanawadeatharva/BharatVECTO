01bf574b4892040763733a9f3600078c7a7956f5
Michael Krisper <michael.krisper@tugraz.at>
2022-03-29 11:14:47 +0200

    PowertrainBuilder: Retarder in BuildFullPowertrainConventional

--- VectoCore/VectoCore/Models/Simulation/Impl/PowertrainBuilder.cs
+++ VectoCore/VectoCore/Models/Simulation/Impl/PowertrainBuilder.cs
@@ -350,13 +350,14 @@ namespace TUGraz.VectoCore.Models.Simulation.Impl
 		///   └┬Wheels
 		///    └┬Brakes
 		///     └┬AxleGear
-		///      └┬(Angledrive)
-		///       └┬(Output Retarder)
-		///        └┬Gearbox, ATGearbox, or APTNGearbox
-		///         └┬(Input Retarder)
-		///          └┬(Clutch when Manual Transmission)
-		///           └StopStartCombustionEngine
-		///                                    └(Aux)
+		///      └┬(AxlegearInputRetarder)
+		///       └┬(Angledrive)
+		///        └┬(TransmissionOutputRetarder)
+		///         └┬Gearbox, ATGearbox, or APTNGearbox
+		///          └┬(TransmissionInputRetarder)
+		///           └┬(Clutch)
+		///            └StopStartCombustionEngine
+		///                                     └(Aux)
 		/// </code>
 		/// </summary>
 		private IVehicleContainer BuildFullPowertrainConventional(VectoRunData data)
@@ -375,12 +376,14 @@ namespace TUGraz.VectoCore.Models.Simulation.Impl
 				.AddComponent(new Wheels(container, data.VehicleData.DynamicTyreRadius, data.VehicleData.WheelsInertia))
 				.AddComponent(new Brakes(container))
 				.AddComponent(new AxleGear(container, data.AxleGearData))
+				.AddComponent(data.Retarder.Type == RetarderType.AxlegearInputRetarder ? new Retarder(container, data.Retarder.LossMap, data.Retarder.Ratio) : null)
 				.AddComponent(data.AngledriveData != null ? new Angledrive(container, data.AngledriveData) : null)
-				.AddComponent(GetGearbox(container), data.Retarder, container)
+				.AddComponent(data.Retarder.Type == RetarderType.TransmissionOutputRetarder ? new Retarder(container, data.Retarder.LossMap, data.Retarder.Ratio) : null)
+				.AddComponent(GetGearbox(container), container)
+				.AddComponent(data.Retarder.Type == RetarderType.TransmissionInputRetarder ? new Retarder(container, data.Retarder.LossMap, data.Retarder.Ratio) : null)
 				.AddComponent(data.GearboxData.Type.ManualTransmission() ? new Clutch(container, data.EngineData) : null)
 				.AddComponent(engine, idleController)
 				.AddAuxiliaries(container, data);
-
 			return container;
 		}
 
