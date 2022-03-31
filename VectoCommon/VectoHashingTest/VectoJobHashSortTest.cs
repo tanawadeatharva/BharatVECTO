7b287949217821668e8bb6a1bd9b634018a78922
Markus Quaritsch <markus.quaritsch@tugraz.at>
2022-03-31 13:07:52 +0200

    bugfix in hashing tool: sort components before reading (tyres, batteries) handling of REESS components

--- VectoCommon/VectoHashingTest/VectoJobHashSortTest.cs
+++ VectoCommon/VectoHashingTest/VectoJobHashSortTest.cs
@@ -24,8 +24,8 @@ namespace VectoHashingTest
 
             Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Engine));
             Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricMachineSystem));//ElectricMachineGEN
-            Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.REESS, 0));//ElectricEnergyStorage
-            Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.REESS, 1));//ElectricEnergyStorage
+            Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricEnergyStorage, 0));//ElectricEnergyStorage
+            Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricEnergyStorage, 1));//ElectricEnergyStorage
             Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.IEPC));
             Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Retarder));
             Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Axlegear));
@@ -64,8 +64,8 @@ namespace VectoHashingTest
 			var loadedFile = VectoHash.Load(filePath);
 
 			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricMachineSystem));
-			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.REESS,0));
-			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.REESS,1));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricEnergyStorage,0));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricEnergyStorage,1));
 			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Gearbox));
 			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.TorqueConverter));
 			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Angledrive));
@@ -76,6 +76,54 @@ namespace VectoHashingTest
 
 			Assert.AreEqual(expectedJobHash, loadedFile.ComputeHash());
 
+        }
+
+		[TestCase("OLkdJQk/V5cvGFqPahsKDIVV3RUxFGgbl4z1EABouac=", UnsortedJobPath + "PEV_mediumLorry_AMT_E2_3Bat.xml")]
+
+		public void TestJobPEVMediumLorryBattery(string expectedJobHash, string filePath)
+		{
+
+			var loadedFile = VectoHash.Load(filePath);
+
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricMachineSystem));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricEnergyStorage, 0), loadedFile.ComputeHash(VectoComponents.ElectricEnergyStorage, 0));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricEnergyStorage, 1), loadedFile.ComputeHash(VectoComponents.ElectricEnergyStorage, 1));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricEnergyStorage, 2), loadedFile.ComputeHash(VectoComponents.ElectricEnergyStorage, 2));
+            Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Gearbox));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.TorqueConverter));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Angledrive));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Retarder));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Tyre, 0));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Tyre, 1));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Airdrag));
+
+			Assert.AreEqual(expectedJobHash, loadedFile.ComputeHash());
+
+            Assert.AreEqual("uoQkX7zZBul57xMVc4ztT/YkPoI2Y9/l1OUpT2C6eZg=", loadedFile.ReadHash(VectoComponents.ElectricEnergyStorage, 0));
+            Assert.AreEqual("uoQkX7zZBul57xMVc4ztT/YkPoI2Y9/l1OUpT2C6eZg=", loadedFile.ReadHash(VectoComponents.ElectricEnergyStorage, 1));
+            Assert.AreEqual("bqolF4NKnQMMf9Kvc4Xj0nzMdbeIyAR4/Ov2USB5CPs=", loadedFile.ReadHash(VectoComponents.ElectricEnergyStorage, 2));
+		}
+
+		[TestCase("uEtvb6ULv7SX4tV1e5zmcl02Yl3/AFibGnt5T2oCCUg=", UnsortedJobPath + "PEV_mediumLorry_AMT_E2_SuperCap.xml")]
+
+		public void TestJobPEVMediumLorrySuperCap(string expectedJobHash, string filePath)
+		{
+
+			var loadedFile = VectoHash.Load(filePath);
+
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricMachineSystem));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.ElectricEnergyStorage, 0), loadedFile.ComputeHash(VectoComponents.ElectricEnergyStorage, 0));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Gearbox));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.TorqueConverter));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Angledrive));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Retarder));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Tyre, 0));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Tyre, 1));
+			Assert.IsTrue(loadedFile.ValidateHash(VectoComponents.Airdrag));
+
+			Assert.AreEqual(expectedJobHash, loadedFile.ComputeHash());
+
+            Assert.AreEqual("4fFjMBbayKbJEq/7sJTUi3C4i006qzDQCqVOEL8y6RU=", loadedFile.ReadHash(VectoComponents.ElectricEnergyStorage, 0));
         }
     }
 }
