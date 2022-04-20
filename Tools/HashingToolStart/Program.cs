8b50fde71975b9c38267225f0f077d9dff709683
Michael Krisper <michael.krisper@tugraz.at>
2022-04-20 15:51:59 +0200

    upgrade to .NET 6.0

--- Tools/HashingToolStart/Program.cs
+++ Tools/HashingToolStart/Program.cs
@@ -19,8 +19,8 @@ namespace TUGraz.VECTO
 		private static string GetHighestNETVersion()
 		{
 			//todo mk2022-02-17 hashing tool currently only works under net45. this has to be fixed.
-			//if (SupportsNet50()) {
-			//	return "net50";
+			//if (SupportsNet60()) {
+			//	return "net60";
 			//}
 
 			//if (SupportsNet48()) {
@@ -30,7 +30,7 @@ namespace TUGraz.VECTO
 			return "net45";
 		}
 
-		private static bool SupportsNet50()
+		private static bool SupportsNet60()
 		{
 			try {
 				var p = Process.Start(new ProcessStartInfo("dotnet", "--list-runtimes") {
@@ -42,7 +42,7 @@ namespace TUGraz.VECTO
 
 				p.WaitForExit();
 				var output = p.StandardOutput.ReadToEnd();
-				return output.Contains("Microsoft.WindowsDesktop.App 5.0");
+				return output.Contains("Microsoft.WindowsDesktop.App 6");
 			} catch (Exception e) {
 				Console.WriteLine(e);
 			}
