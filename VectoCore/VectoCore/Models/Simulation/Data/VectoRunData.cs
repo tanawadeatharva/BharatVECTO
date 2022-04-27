34e5afea0ea73dbc48462f98515cfb4885811bd6
Michael Krisper <michael.krisper@tugraz.at>
2022-04-27 13:01:23 +0200

    VectoRunData: reformulated error message for P0

--- VectoCore/VectoCore/Models/Simulation/Data/VectoRunData.cs
+++ VectoCore/VectoCore/Models/Simulation/Data/VectoRunData.cs
@@ -236,7 +236,7 @@ namespace TUGraz.VectoCore.Models.Simulation.Data
 			}
 
 			if (runData.ElectricMachinesData?.Any(e => e.Item1 == PowertrainPosition.HybridP0) ?? false){
-				return new ValidationResult("PowertrainPosition P0 is currently not supported.");
+				return new ValidationResult("P0 Hybrids are modeled as SmartAlternator in the BusAuxiliary model.");
 			}
 
 			return ValidationResult.Success;
