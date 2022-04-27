4a1a0b2c65e17ea0791718fdc9b1132e3386527f
Harald Martini <harald.martini@student.tugraz.at>
2022-04-27 11:19:13 +0200

    updated dockerfile for multistep user manual

--- Documentation/MultiStepTool/build_in_docker.sh
+++ Documentation/MultiStepTool/build_in_docker.sh
@@ -1,2 +1,2 @@
-docker build -t ubuntu_pandoc .
+docker build -t ubuntupandoc .
 docker run --rm --volume /mnt/c/Users/Harry/source/repos/hm_vecto-dev/Documentation/MultiStepTool:/documentation --entrypoint /documentation/convert.sh ubuntu_pandoc
