3588591f6da15ac31fd1bab58df368efc4d3b85c
VKMTHD\franzjosefkober <franz.josef.kober@ivt.tugraz.at>
2022-05-23 18:24:30 +0200

    comment out json writer class

--- VECTO/GUI/IEPCForm.vb
+++ VECTO/GUI/IEPCForm.vb
@@ -390,10 +390,10 @@ Public Class IEPCForm
         iepc.SetDragCurveEntries(lvDragCurve)
 
 
-        If Not iepc.SaveFile(file) Then
-            MsgBox("Cannot save to " & file, MsgBoxStyle.Critical)
-            Return False
-        End If
+        'If Not iepc.SaveFile(file) Then
+        '    MsgBox("Cannot save to " & file, MsgBoxStyle.Critical)
+        '    Return False
+        'End If
         
         _changed = False
         LbStatus.Text = ""
