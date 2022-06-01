fa95113c35cd535f31b1bb909fc18adaaad75e86
Harald Martini <harald.martini@student.tugraz.at>
2022-06-01 17:12:32 +0200

    updated XMLDeclarationComponentsMultistagePrimaryVehicleBusDataProviderV01 XSD type

--- VectoCore/VectoCore/InputData/FileIO/XML/Declaration/DataProvider/XMLDeclarationComponentsDataProvider.cs
+++ VectoCore/VectoCore/InputData/FileIO/XML/Declaration/DataProvider/XMLDeclarationComponentsDataProvider.cs
@@ -215,7 +215,7 @@ namespace TUGraz.VectoCore.InputData.FileIO.XML.Declaration.DataProvider
 	{
 		public new static readonly XNamespace NAMESPACE_URI = XMLDefinitions.DECLARATION_MULTISTAGE_BUS_VEHICLE_NAMESPACE_VO1;
 
-		public new const string XSD_TYPE = "VehicleComponentsVIFType";
+		public new const string XSD_TYPE = "Vehicle_Conventional_ComponentsVIFType";
 
 		public new static readonly string QUALIFIED_XSD_TYPE = XMLHelper.CombineNamespace(NAMESPACE_URI.NamespaceName, XSD_TYPE);
 
