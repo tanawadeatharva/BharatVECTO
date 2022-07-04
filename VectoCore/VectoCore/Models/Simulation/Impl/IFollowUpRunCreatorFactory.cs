4aadfe79d7c4d9fa0d29229313b808477be1f5f3
Harald Martini <harald.martini@student.tugraz.at>
2022-07-04 13:34:59 +0200

    added followup run creator factory

---
+++ VectoCore/VectoCore/Models/Simulation/Impl/IFollowUpRunCreatorFactory.cs
@@ -0,0 +1,30 @@
+﻿using TUGraz.VectoCore.Models.Simulation.Data;
+
+namespace TUGraz.VectoCore.Models.Simulation.Impl
+{
+	public interface IFollowUpRunCreatorFactory
+	{
+		IFollowUpRunCreator CreateFollowUpRunCreator(VectoRunData runData);
+	}
+	public class FollowUpRunCreatorFactory : IFollowUpRunCreatorFactory
+	{
+		#region Implementation of IFollowUpRunCreatorFactory
+
+		public IFollowUpRunCreator CreateFollowUpRunCreator(VectoRunData runData)
+		{
+			//if (runData.VehicleData.InputData.OvcHev) {
+			//	return new FollowUpOvcRunCreator();
+			//}else{
+			//	return new NoFollowUpRunCreator();
+			//}
+
+
+		//TEST Purposes return always the
+			return new FollowUpOvcRunCreator();
+
+
+		}
+
+		#endregion
+	}
+}
