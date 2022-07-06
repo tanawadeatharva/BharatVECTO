8cd940c9bb623df117efe91badcb80ee00dd7184
Stefanos Doumpoulakis <dubulak@gmail.com>
2022-07-06 04:17:34 +0300

    a)removed vehicle speed from vdris, b)recreated E2 vdris

--- VectoCore/VectoCoreTest/Integration/BatteryElectric/BEVTimeRunTest.cs
+++ VectoCore/VectoCoreTest/Integration/BatteryElectric/BEVTimeRunTest.cs
@@ -36,25 +36,25 @@ namespace TUGraz.VectoCore.Tests.Integration.BatteryElectric
         TestCase(E2_JOB, 7, 1, 4.3124, 117.0889, TestName = "E2 BEV TimeRun MeasuredSpeedGear RegionalDelivery"),
         TestCase(E2_JOB, 8, 2, 28.8197, 125.4709, TestName = "E2 BEV TimeRun MeasuredSpeedGear UrbanDelivery"),
 
-        TestCase(E2_JOB, 9, 0, 1.1568, 119.989, TestName = "E2 BEV TimeRun MeasuredSpeedPWheel LongHaul"),
-        TestCase(E2_JOB, 10, 1, 4.6931, 117.2523, TestName = "E2 BEV TimeRun MeasuredSpeedPWheel RegionalDelivery"),
-        TestCase(E2_JOB, 11, 2, 31.8158, 126.7559, TestName = "E2 BEV TimeRun MeasuredSpeedPWheel UrbanDelivery"),
+        TestCase(E2_JOB, 9, 0, 1.1568, 119.989, TestName = "E2 BEV TimeRun PWheel LongHaul"),
+        TestCase(E2_JOB, 10, 1, 4.4741, 116.9733, TestName = "E2 BEV TimeRun PWheel RegionalDelivery"),
+        TestCase(E2_JOB, 11, 2, 30.5471, 124.9811, TestName = "E2 BEV TimeRun PWheel UrbanDelivery"),
 
         TestCase(E3_JOB, 0, 0, 0.7916, 101.6194, TestName = "E3 BEV TimeRun MeasuredSpeed LongHaul"),
         TestCase(E3_JOB, 1, 1, 3.3777, 104.792, TestName = "E3 BEV TimeRun MeasuredSpeed RegionalDelivery"),
         TestCase(E3_JOB, 2, 2, 28.1637, 128.3685, TestName = "E3 BEV TimeRun MeasuredSpeed UrbanDelivery"),
 
-        TestCase(E3_JOB, 6, 0, 0.8144, 101.6097, TestName = "E3 BEV TimeRun MeasuredSpeedPWheel LongHaul"),
-        TestCase(E3_JOB, 7, 1, 3.5103, 104.7876, TestName = "E3 BEV TimeRun MeasuredSpeedPWheel RegionalDelivery"),
-        TestCase(E3_JOB, 8, 2, 29.6221, 128.1818, TestName = "E3 BEV TimeRun MeasuredSpeedPWheel UrbanDelivery"),
+        TestCase(E3_JOB, 6, 0, 0.8144, 101.6097, TestName = "E3 BEV TimeRun PWheel LongHaul"),
+        TestCase(E3_JOB, 7, 1, 3.5103, 104.7876, TestName = "E3 BEV TimeRun PWheel RegionalDelivery"),
+        TestCase(E3_JOB, 8, 2, 29.6221, 128.1818, TestName = "E3 BEV TimeRun PWheel UrbanDelivery"),
 
         TestCase(E4_JOB, 0, 0, 0.9165, 97.3067, TestName = "E4 BEV TimeRun MeasuredSpeed LongHaul"),
         TestCase(E4_JOB, 1, 1, 3.7896, 100.3712, TestName = "E4 BEV TimeRun MeasuredSpeed RegionalDelivery"),
         TestCase(E4_JOB, 2, 2, 30.1342, 123.7528, TestName = "E4 BEV TimeRun MeasuredSpeed UrbanDelivery"),
 
-        TestCase(E4_JOB, 6, 0, 0.94, 97.3005, TestName = "E4 BEV TimeRun MeasuredSpeedPWheel LongHaul"),
-        TestCase(E4_JOB, 7, 1, 3.9236, 100.3755, TestName = "E4 BEV TimeRun MeasuredSpeedPWheel RegionalDelivery"),
-        TestCase(E4_JOB, 8, 2, 31.6158, 123.6378, TestName = "E4 BEV TimeRun MeasuredSpeedPWheel UrbanDelivery")
+        TestCase(E4_JOB, 6, 0, 0.94, 97.3005, TestName = "E4 BEV TimeRun PWheel LongHaul"),
+        TestCase(E4_JOB, 7, 1, 3.9236, 100.3755, TestName = "E4 BEV TimeRun PWheel RegionalDelivery"),
+        TestCase(E4_JOB, 8, 2, 31.6158, 123.6378, TestName = "E4 BEV TimeRun PWheel UrbanDelivery")
         ]
         public void TestBEVTimeRunCycle(string jobFile, int cycleIdx, int distanceCycleIdx, double charge, double discharge)
         {
