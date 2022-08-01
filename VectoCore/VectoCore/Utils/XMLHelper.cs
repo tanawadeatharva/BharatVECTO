47709dd3547d4aeac2a837249dfc1b7f42306426
Markus Quaritsch <markus.quaritsch@tugraz.at>
2022-08-01 21:08:14 +0200

    updating XML Schema: more vehicle information in CIF, input signature in MRF and CIF

--- VectoCore/VectoCore/Utils/XMLHelper.cs
+++ VectoCore/VectoCore/Utils/XMLHelper.cs
@@ -97,6 +97,18 @@ namespace TUGraz.VectoCore.Utils
 			throw new NotImplementedException($"unknown unit '{unit}'");
 		}
 
+		public static object[] ValueAsUnit(this WattSecond energy, string unit, uint? decimals = 0)
+		{
+			switch (unit) {
+				case "kWh":
+					return GetValueAsUnit(energy?.ConvertToKiloWattHour(), unit, decimals);
+				case "Wh":
+					return GetValueAsUnit(energy?.ConvertToWattHour(), unit, decimals);
+			}
+
+			throw new NotImplementedException($"unknown unit '{unit}'");
+		}
+
 		public static object[] ValueAsUnit(this AmpereSecond capacity, string unit, uint? decimals = 0)
 		{
 			switch (unit)
@@ -159,7 +171,7 @@ namespace TUGraz.VectoCore.Utils
 			throw new NotImplementedException($"unknown unit '{unit}'");
 		}
 
-		public static object[] ValueAsUnit(double value, string unit, uint? decimals)
+		public static object[] ValueAsUnit(this double value, string unit, uint? decimals)
 		{
 			switch (unit) {
 				case "%": return GetValueAsUnit(value * 100, unit, decimals);
@@ -167,7 +179,7 @@ namespace TUGraz.VectoCore.Utils
 			}
 		}
 
-		private static object[] GetValueAsUnit(double? value, string unit, uint? decimals)
+		private static object[] GetValueAsUnit(this double? value, string unit, uint? decimals)
 		{
 			if (value == null) {
 				return new object[0];
