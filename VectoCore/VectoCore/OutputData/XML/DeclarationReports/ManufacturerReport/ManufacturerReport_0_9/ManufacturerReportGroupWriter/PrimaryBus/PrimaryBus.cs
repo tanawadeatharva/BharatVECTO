a47643ce02de4670bf0dcb0504c9fed1ed5370e1
Harald Martini <harald.martini@student.tugraz.at>
2022-08-16 13:01:37 +0200

    removed zero emission HDV and hybrid electric HDV from PEVPrimaryBusVehicleOutputGroup

--- VectoCore/VectoCore/OutputData/XML/DeclarationReports/ManufacturerReport/ManufacturerReport_0_9/ManufacturerReportGroupWriter/PrimaryBus/PrimaryBus.cs
+++ VectoCore/VectoCore/OutputData/XML/DeclarationReports/ManufacturerReport/ManufacturerReport_0_9/ManufacturerReportGroupWriter/PrimaryBus/PrimaryBus.cs
@@ -63,8 +63,6 @@ namespace TUGraz.VectoCore.OutputData.XML.DeclarationReports.ManufacturerReport.
 		{
 			var result = new List<XElement>();
 			result.AddRange(_mrfFactory.GetPrimaryBusGeneralVehicleOutputGroup().GetElements(inputData));
-			result.Add(new XElement(_mrf + "ZeroEmissionHDV", inputData.JobInputData.Vehicle.ZeroEmissionVehicle));
-			result.Add(new XElement(_mrf + XMLNames.Vehicle_HybridElectricHDV, inputData.JobInputData.Vehicle.HybridElectricHDV));
 			result.AddRange(_mrfFactory.GetPEV_VehicleSequenceGroup().GetElements(inputData));
 
 			return result;
