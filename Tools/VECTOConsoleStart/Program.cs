511f1eb9390ea2ccd5424accd5f2d3c369ce5ccf
Harald Martini <harald.martini@student.tugraz.at>
2022-08-30 12:14:14 +0200

    removed possibility to select .net version via command line, pass commandline arguments to launched console application

--- Tools/VECTOConsoleStart/Program.cs
+++ Tools/VECTOConsoleStart/Program.cs
@@ -4,6 +4,9 @@ namespace TUGraz.VECTO
 {
 	class Program
 	{
-		static void Main(string[] args) => StarterHelper.StartVECTO(args);
+		static void Main(string[] args)
+		{
+			StarterHelper.StartVECTO(args, true);
+		}
 	}
 }
