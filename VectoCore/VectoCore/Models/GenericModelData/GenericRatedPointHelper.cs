43012f56095e83cb71f874abe631fb676c20fd08
VKMTHD\franzjosefkober <franz.josef.kober@ivt.tugraz.at>
2022-09-12 14:24:53 +0200

    Replacing data tables with structs for handling values

--- VectoCore/VectoCore/Models/GenericModelData/GenericRatedPointHelper.cs
+++ VectoCore/VectoCore/Models/GenericModelData/GenericRatedPointHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Data;
+using System.Collections.Generic;
+using Castle.Core.Internal;
 using TUGraz.VectoCommon.InputData;
 using TUGraz.VectoCommon.Utils;
 using TUGraz.VectoCore.Utils;
@@ -29,87 +30,108 @@ namespace TUGraz.VectoCore.Models.GenericModelData
 
 	public static class GenericRatedPointHelper
 	{
+		struct FullLoadCurveEntry
+		{
+			public PerSecond MotorSpeed { get; }
+			public NewtonMeter TorqueDrive { get; }
+			public NewtonMeter TorqueDrag { get; }
+			public Watt PowerDrive { get; }
+
+			public FullLoadCurveEntry(PerSecond motorSpeed, NewtonMeter torqueDrive, NewtonMeter torqueDrag)
+			{
+				MotorSpeed = motorSpeed;
+				TorqueDrive = torqueDrive;
+				TorqueDrag = torqueDrag;
+				PowerDrive = motorSpeed * torqueDrive * Math.PI / 30000;
+			}
+		}
+
+
+		struct SlopeValueEntry
+		{
+			public double Slope { get; }
+			public double Delta { get; }
+
+			public SlopeValueEntry(double slope, double delta)
+			{
+				Slope = slope;
+				Delta = delta;
+			}
+		}
+
+
 		#region Constants
 
-		public const string MotorSpeed = "n";
-		public const string TorqueDrive = "T_drive";
-		public const string TorqueDrag = "T_drag";
-		public const string PowerDrive = "P_drive";
-
-		public const string SlopeValue = "slope";
-		public const string DeltaValue = "delta";
+		public const string MotorSpeedColumn = "outShaftSpeed";
+		public const string TorqueDriveColumn = "maxTorque";
+		public const string TorqueDragColumn = "minTorque";
 
 		#endregion
+
+		private static List<FullLoadCurveEntry> fullLoadCurveEntries;
+		private static List<SlopeValueEntry> slopeValueEntries;
 		
+
 		public static RatedPoint GetRatedPointOfFullLoadCurve(TableData fullLoadCurve)
 		{
-			var curveValues = new DataTable();
-			var slopeValues = new DataTable();
-			SetCurveValues(fullLoadCurve, ref curveValues, ref slopeValues);
-
-			var ratedIndex = FindRowOfRatedPoint(slopeValues);
-			var nRated = curveValues.Rows[ratedIndex].ParseDouble(MotorSpeed).SI<PerSecond>();
-			var tRated = GetHighestTorque(curveValues);
+			SetCurveValues(fullLoadCurve);
+			var ratedIndex = FindRowOfRatedPoint();
+			var nRated = fullLoadCurveEntries[ratedIndex].MotorSpeed;
+			var tRated = GetHighestTorque();
 
 			return new RatedPoint (nRated, tRated);
 		}
 
-		private static void SetCurveValues(TableData fullLoadCurve, ref DataTable curveValues, ref DataTable slopeValues)
+
+		private static void SetCurveValues(TableData fullLoadCurve)
 		{
-			curveValues.Columns.Add(MotorSpeed);
-			curveValues.Columns.Add(TorqueDrive);
-			curveValues.Columns.Add(TorqueDrag);
-			curveValues.Columns.Add(PowerDrive);
+			fullLoadCurveEntries = new List<FullLoadCurveEntry>();
+			slopeValueEntries = new List<SlopeValueEntry>();
 
-			slopeValues.Columns.Add(SlopeValue);
-			slopeValues.Columns.Add(DeltaValue);
+			for (int r = 0; r < fullLoadCurve.Rows.Count; r++) {
 
-			for (int r = 0; r < fullLoadCurve.Rows.Count; r++)
-			{
-				curveValues.Rows.Add(curveValues.NewRow());
+				var motorSpeed = fullLoadCurve.Rows[r].ParseDouble(MotorSpeedColumn).SI<PerSecond>();
+				var torqueDrive = fullLoadCurve.Rows[r].ParseDouble(TorqueDriveColumn).SI<NewtonMeter>();
+				var torqueDrag = fullLoadCurve.Rows[r].ParseDouble(TorqueDragColumn).SI<NewtonMeter>();
 
-				var motorSpeed = fullLoadCurve.Rows[r].ParseDouble("outShaftSpeed");
-				var torqueDrive = fullLoadCurve.Rows[r].ParseDouble("maxTorque");
-
-				curveValues.Rows[r][MotorSpeed] = motorSpeed;
-				curveValues.Rows[r][TorqueDrive] = torqueDrive;
-				curveValues.Rows[r][TorqueDrag] = fullLoadCurve.Rows[r].ParseDouble("minTorque");
-				curveValues.Rows[r][PowerDrive] = motorSpeed * torqueDrive * Math.PI / 30000;
+				fullLoadCurveEntries.Add(new FullLoadCurveEntry(motorSpeed, torqueDrive, torqueDrag));
 
 				if (r == 0)
 					continue;
-
-				slopeValues.Rows.Add(slopeValues.NewRow());
-
-				var slopeValue = (curveValues.Rows[r].ParseDouble(PowerDrive) - curveValues.Rows[r - 1].ParseDouble(PowerDrive)) /
-								(curveValues.Rows[r].ParseDouble(MotorSpeed) - curveValues.Rows[r - 1].ParseDouble(MotorSpeed));
 				
-				slopeValues.Rows[r - 1][SlopeValue] = slopeValue;
-				slopeValues.Rows[r - 1][DeltaValue] = slopeValues.Rows[r - 1].ParseDouble(SlopeValue) / slopeValues.Rows[0].ParseDouble(SlopeValue) - 1;
+				var slopeValue = (fullLoadCurveEntries[r].PowerDrive.Value() - fullLoadCurveEntries[r - 1].PowerDrive.Value()) /
+								 (fullLoadCurveEntries[r].MotorSpeed.Value() - fullLoadCurveEntries[r - 1].MotorSpeed.Value());
+
+				var deltaValue = slopeValue / (slopeValueEntries.IsNullOrEmpty() ? slopeValue  : slopeValueEntries[0].Slope ) -1;
+
+				slopeValueEntries.Add(new SlopeValueEntry(slopeValue, deltaValue));
 			}
 		}
+		
 
-		private static NewtonMeter GetHighestTorque(DataTable curveValues)
+		private static NewtonMeter GetHighestTorque()
 		{
 			var value = double.MinValue;
 
-			foreach (DataRow row in curveValues.Rows) {
-				var currentValue = row.ParseDouble(TorqueDrive);
+			foreach (var entry in fullLoadCurveEntries)
+			{
+				var currentValue = entry.TorqueDrive.Value();
 				if (value < currentValue)
 					value = currentValue;
 			}
 
 			return value.SI<NewtonMeter>();
 		}
-		
-		private static int FindRowOfRatedPoint(DataTable slopeValues)
-		{
-			for (int r = 0; r < slopeValues.Rows.Count; r++) {
 
-				var deltaValue = slopeValues.Rows[r].ParseDouble(DeltaValue);
+
+		private static int FindRowOfRatedPoint()
+		{
+			for (int i = 0; i < slopeValueEntries.Count; i++)
+			{
+				var deltaValue = slopeValueEntries[i].Delta;
 
 				if (Math.Abs(deltaValue) > 0.2)
-					return r;
+					return i;
 			}
 
 			return -1;
