fed55cf18aa4d4e28a68dcc623c47a856ed0a506
Markus Quaritsch <markus.quaritsch@tugraz.at>
2022-11-07 09:06:08 +0100

    deactivate XML export in GUI

--- VECTO/GUI/MainForm.Designer.vb
+++ VECTO/GUI/MainForm.Designer.vb
@@ -238,6 +238,7 @@ Partial Class MainForm
         Me.btnExportXML.TabIndex = 22
         Me.btnExportXML.Text = "Export as XML"
         Me.btnExportXML.UseVisualStyleBackColor = True
+        Me.btnExportXML.Visible = false
         '
         'Label6
         '
