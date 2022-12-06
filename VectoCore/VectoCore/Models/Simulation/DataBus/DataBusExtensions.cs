4fc0733a9320bbf7a19eb3c7bdf60d4a5e03d338
VKMTHD\haraldmartini <harald.martini@student.tugraz.at>
2022-12-06 14:47:18 +0100

    added DataBusExtensionclass

---
+++ VectoCore/VectoCore/Models/Simulation/DataBus/DataBusExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace TUGraz.VectoCore.Models.Simulation.DataBus
+{
+	public static class DataBusExtensions
+	{
+		public static IList<IElectricMotorInfo> GetElectricMotors(this IDataBus db)
+		{
+			var result = new List<IElectricMotorInfo>();
+
+			foreach (var pos in db.PowertrainInfo.ElectricMotorPositions) {
+				result.Add(db.ElectricMotorInfo(pos));
+			}
+
+			return result;
+		}
+	}
+}
