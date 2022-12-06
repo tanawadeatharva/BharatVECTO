077eb317b0fc556ce1d8bf78743babfd37905080
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2022-12-06 19:50:04 +0100

    provide dummy componten for retarder and angledrive also for input in schema version 2.4

--- VectoCore/VectoCore/InputData/FileIO/XML/Declaration/DataProvider/XMLDeclarationComponentsDataProvider.cs
+++ VectoCore/VectoCore/InputData/FileIO/XML/Declaration/DataProvider/XMLDeclarationComponentsDataProvider.cs
@@ -157,14 +157,14 @@ namespace TUGraz.VectoCore.InputData.FileIO.XML.Declaration.DataProvider
 
 		#region Overrides of XMLDeclarationComponentsDataProviderV10
 
-		public override IAngledriveInputData AngledriveInputData
-			=> ElementExists(XMLNames.Component_Angledrive) ? base.AngledriveInputData : null;
+		//public override IAngledriveInputData AngledriveInputData
+		//	=> ElementExists(XMLNames.Component_Angledrive) ? base.AngledriveInputData : null;
 
-		public override IRetarderInputData RetarderInputData
-			=> ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
+		//public override IRetarderInputData RetarderInputData
+		//	=> ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
 
-		public override IAirdragDeclarationInputData AirdragInputData
-			=> ElementExists(XMLNames.Component_AirDrag) ? base.AirdragInputData : null;
+		//public override IAirdragDeclarationInputData AirdragInputData
+		//	=> ElementExists(XMLNames.Component_AirDrag) ? base.AirdragInputData : null;
 
 		#endregion
 	}
@@ -699,14 +699,14 @@ namespace TUGraz.VectoCore.InputData.FileIO.XML.Declaration.DataProvider
 		
 		#region Overrides of XMLDeclarationComponentsDataProviderV10
 
-		public override IRetarderInputData RetarderInputData =>
-			ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
+		//public override IRetarderInputData RetarderInputData =>
+		//	ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
 
-		public override IAxleGearInputData AxleGearInputData =>
-			ElementExists(XMLNames.Component_Axlegear) ? base.AxleGearInputData : null;
+		//public override IAxleGearInputData AxleGearInputData =>
+		//	ElementExists(XMLNames.Component_Axlegear) ? base.AxleGearInputData : null;
 
-		public override IAirdragDeclarationInputData AirdragInputData => 
-			ElementExists(XMLNames.Component_AirDrag) ? base.AirdragInputData : null;
+		//public override IAirdragDeclarationInputData AirdragInputData => 
+		//	ElementExists(XMLNames.Component_AirDrag) ? base.AirdragInputData : null;
 		
 		#endregion
 	}
@@ -731,11 +731,11 @@ namespace TUGraz.VectoCore.InputData.FileIO.XML.Declaration.DataProvider
 		
 		public override IBusAuxiliariesDeclarationData BusAuxiliaries => _busAuxiliariesDeclarationInputData ??
 																		(_busAuxiliariesDeclarationInputData = ComponentReader.BusAuxiliariesInputData);
-		public override IRetarderInputData RetarderInputData =>
-			ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
+        //public override IRetarderInputData RetarderInputData =>
+        //	ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
 
-		public override IAxleGearInputData AxleGearInputData =>
-			ElementExists(XMLNames.Component_Axlegear) ? base.AxleGearInputData : null;
+        public override IAxleGearInputData AxleGearInputData =>
+            ElementExists(XMLNames.Component_Axlegear) ? base.AxleGearInputData : null;
 		#endregion
 
 	}
@@ -757,11 +757,11 @@ namespace TUGraz.VectoCore.InputData.FileIO.XML.Declaration.DataProvider
 		public override IAngledriveInputData AngledriveInputData =>
 			ElementExists(XMLNames.Component_Angledrive) ? base.AngledriveInputData : null;
 		
-		public override IRetarderInputData RetarderInputData =>
-			ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
+		//public override IRetarderInputData RetarderInputData =>
+		//	ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
 
-		public override IAirdragDeclarationInputData AirdragInputData =>
-			ElementExists(XMLNames.Component_AirDrag) ? base.AirdragInputData : null;
+		//public override IAirdragDeclarationInputData AirdragInputData =>
+		//	ElementExists(XMLNames.Component_AirDrag) ? base.AirdragInputData : null;
 
 		#endregion
 	}
@@ -801,11 +801,11 @@ namespace TUGraz.VectoCore.InputData.FileIO.XML.Declaration.DataProvider
 		public override IBusAuxiliariesDeclarationData BusAuxiliaries  => _busAuxiliariesDeclarationInputData ??
 																 	 (_busAuxiliariesDeclarationInputData = ComponentReader.BusAuxiliariesInputData);
 
-		public override IAngledriveInputData AngledriveInputData =>
-			ElementExists(XMLNames.Component_Angledrive) ? base.AngledriveInputData : null;
+		//public override IAngledriveInputData AngledriveInputData =>
+		//	ElementExists(XMLNames.Component_Angledrive) ? base.AngledriveInputData : null;
 
-		public override IRetarderInputData RetarderInputData =>
-			ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
+		//public override IRetarderInputData RetarderInputData =>
+		//	ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
 
 		#endregion
 	}
@@ -838,14 +838,14 @@ namespace TUGraz.VectoCore.InputData.FileIO.XML.Declaration.DataProvider
 
 		#region Overrides of XMLDeclarationComponentsDataProviderV10
 
-		public override IRetarderInputData RetarderInputData =>
-			ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
+		//public override IRetarderInputData RetarderInputData =>
+		//	ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
 
 		public override IAxleGearInputData AxleGearInputData =>
 			ElementExists(XMLNames.Component_Axlegear) ? base.AxleGearInputData : null;
 
-		public override IAirdragDeclarationInputData AirdragInputData =>
-			ElementExists(XMLNames.Component_AirDrag) ? base.AirdragInputData : null;
+		//public override IAirdragDeclarationInputData AirdragInputData =>
+		//	ElementExists(XMLNames.Component_AirDrag) ? base.AirdragInputData : null;
 
 		#endregion
 	}
@@ -863,8 +863,8 @@ namespace TUGraz.VectoCore.InputData.FileIO.XML.Declaration.DataProvider
 
 		#region Overrides of XMLDeclarationComponentsDataProviderV10
 
-		public override IRetarderInputData RetarderInputData =>
-			ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
+		//public override IRetarderInputData RetarderInputData =>
+		//	ElementExists(XMLNames.Component_Retarder) ? base.RetarderInputData : null;
 
 		public override IAxleGearInputData AxleGearInputData =>
 			ElementExists(XMLNames.Component_Axlegear) ? base.AxleGearInputData : null;
