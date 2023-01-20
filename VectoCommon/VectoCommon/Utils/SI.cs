0c6dcf7e1f8a129a64e81e5f4d0815a5a7d5a2f2
VKMTHD\haraldmartini <harald.martini@student.tugraz.at>
2023-01-20 16:28:15 +0100

    re-added debuggerhidden attribute in SI

--- VectoCommon/VectoCommon/Utils/SI.cs
+++ VectoCommon/VectoCommon/Utils/SI.cs
@@ -1479,7 +1479,7 @@ namespace TUGraz.VectoCommon.Utils
 		/// <param name="val">The value.</param>
 		/// <param name="unitFactor"></param>
 		/// <param name="units">The units.</param>
-		//[DebuggerHidden]
+		[DebuggerHidden]
 		protected SI(double val, double unitFactor, int[] units)
 		{
 			Val = val;
