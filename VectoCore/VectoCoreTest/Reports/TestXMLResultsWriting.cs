424819df0081654af61f3ed22ed1d3b441aa33ad
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2023-01-25 15:38:26 +0100

    adding testcase for PEV range calculation

--- VectoCore/VectoCoreTest/Reports/TestXMLResultsWriting.cs
+++ VectoCore/VectoCoreTest/Reports/TestXMLResultsWriting.cs
@@ -459,6 +459,33 @@ public class TestXMLResultsWriting
 
 	}
 
+	// ---------
+
+	[TestCase()]
+	public void TestCalculatePEVRanges(params FuelType[] fuels)
+	{
+		var jobType = VectoSimulationJobType.ParallelHybridVehicle;
+		var vehicleCategory = VehicleCategory.RigidTruck;
+		var ovcmode = VectoRunData.OvcHevMode.ChargeDepleting;
+		var runData = GetMockRunData(vehicleCategory, jobType, true, false, ovcmode, fuels);
+		var modData = GetMockModData(VectoRun.Status.Success, fuels, ovcmode);
+
+		//var result = GetResultEntry(runData);
+		//result.SetResultData(runData, modData, 1);
+
+
+		var weighted = DeclarationData.CalculateElectricRangesPEV(runData, modData);
+
+		Console.WriteLine($"{weighted.ActualChargeDepletingRange.Value().ToXMLFormat(3)} {weighted.EquivalentAllElectricRange.Value().ToXMLFormat(3)} {weighted.ZeroCO2EmissionsRange.Value().ToXMLFormat(3)}");
+
+		//1518.750 1366.875 1366.875 0.004 797877.345 30.890 20.000
+
+		Assert.AreEqual(1518.750, weighted.ActualChargeDepletingRange.Value(), 1e-3);
+		Assert.AreEqual(1518.750, weighted.EquivalentAllElectricRange.Value(), 1e-3);
+		Assert.AreEqual(1518.750, weighted.ZeroCO2EmissionsRange.Value(), 1e-3);
+
+	}
+
 	// ===================================
 
 	private static void WriteToConsole(XDocument doc)
