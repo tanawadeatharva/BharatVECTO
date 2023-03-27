0b8a62d3ec06c3102e0950789477090bb4f0bd58
VKMTHD\haraldmartini <harald.martini@student.tugraz.at>
2023-03-27 11:24:45 +0200

    removed SourceTypeVersion property from DataSource

--- VectoCommon/VectoCommon/InputData/DeclarationInputData.cs
+++ VectoCommon/VectoCommon/InputData/DeclarationInputData.cs
@@ -80,16 +80,16 @@ namespace TUGraz.VectoCommon.InputData
 
 		public string SourceFile { get; set; }
 
+		/// <summary>
+		/// returns the version of the namespace uri
+		/// </summary>
 		public string SourceVersion { get; set; }
 
 		/// <summary>
 		/// In case of components loaded from XML files, this is the name of the XSD Type
 		/// </summary>
 		public string Type { get; set; }
-
-		public string SourceTypeVersion { get; set; }
-
-
+		
 
 		public string SourcePath => SourceFile != null ? Path.GetDirectoryName(Path.GetFullPath(SourceFile)) : null;
 	}
