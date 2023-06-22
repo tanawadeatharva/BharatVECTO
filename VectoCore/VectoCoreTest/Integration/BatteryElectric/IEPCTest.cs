bdec1d2b1d7719d5189e6a15d14224667c9e9a59
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2023-06-22 16:40:19 +0200

    additional testcases for IEPC with torque/speed limitations

--- VectoCore/VectoCoreTest/Integration/BatteryElectric/IEPCTest.cs
+++ VectoCore/VectoCoreTest/Integration/BatteryElectric/IEPCTest.cs
@@ -35,8 +35,11 @@ namespace TUGraz.VectoCore.Tests.Integration.BatteryElectric
 
 		protected const string IEPC_Gbx1SpeedWhl2 = @"TestData\BatteryElectric\GenericIEPC\IEPC_Gbx1Speed-Whl2\IEPC_ENG_Gbx1Whl2.vecto";
 
+		protected const string IEPC_Gbx3Speed_TqLimit = @"TestData\BatteryElectric\GenericIEPC\IEPC_Gbx3Speed\IEPC_ENG_Gbx3_TqLimit.vecto";
+		protected const string IEPC_Gbx3Speed_SpeedLimit = @"TestData\BatteryElectric\GenericIEPC\IEPC_Gbx3Speed\IEPC_ENG_Gbx3_SpeedLimit.vecto";
+		protected const string IEPC_Gbx3Speed_SpeedTqLimit = @"TestData\BatteryElectric\GenericIEPC\IEPC_Gbx3Speed\IEPC_ENG_Gbx3_SpeedTqLimit.vecto";
 
-		[OneTimeSetUp]
+        [OneTimeSetUp]
 		public void RunBeforeAnyTests()
 		{
 			Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
@@ -156,7 +159,11 @@ namespace TUGraz.VectoCore.Tests.Integration.BatteryElectric
 		TestCase(IEPC_Gbx1SpeedWhl2, 6, TestName = "IEPC Gbx 1speed DTWheel-2 Job SU"),
 		TestCase(IEPC_Gbx1SpeedWhl2, 7, TestName = "IEPC Gbx 1speed DTWheel-2 Job U"),
 		TestCase(IEPC_Gbx1SpeedWhl2, 8, TestName = "IEPC Gbx 1speed DTWheel-2 Job UD"),
-		]
+
+		TestCase(IEPC_Gbx3Speed_TqLimit, 0, TestName = "IEPC Gbx 3speed Job LH TqLimit"),
+		TestCase(IEPC_Gbx3Speed_SpeedLimit, 0, TestName = "IEPC Gbx 3speed Job LH SpeedLimit"),
+		TestCase(IEPC_Gbx3Speed_SpeedTqLimit, 0, TestName = "IEPC Gbx 3speed Job LH SpeedTqLimit"),
+        ]
 		public void IEPCRunJob(string jobFile, int cycleIdx)
 		{
 			var inputProvider = JSONInputDataFactory.ReadJsonJob(jobFile);
