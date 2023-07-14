7438ce0cf84c878256f96d6427469ee58b5f1368
VKMTHD\haraldmartini <martini@ivt.tugraz.at>
2023-07-14 14:45:07 +0200

    added testcase for Si unit WattPerSecond and fixed constructor

--- VectoCommon/VectoCommon/Utils/SI.cs
+++ VectoCommon/VectoCommon/Utils/SI.cs
@@ -793,7 +793,11 @@ namespace TUGraz.VectoCommon.Utils
 	public class WattPerSecond : SIBase<WattPerSecond>
 	{
 		private static readonly int[] Units = { 1, 2, -4, 0, 0, 0, 0 };
-        public WattPerSecond(double value) : base(value, Units) { }
+
+		private WattPerSecond(double val) : base(val, Units)
+		{
+
+		}
 		public override string UnitString => "W/s";
 	}
 
