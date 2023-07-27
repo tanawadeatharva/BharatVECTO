ed13a128db803e09285180833bc48c308794dfe3
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2023-07-27 14:04:08 +0200

    removed confidential testcase

--- VectoCore/VectoCoreTest/Integration/Declaration/PrimaryBus/PrimaryBusSimulation.cs
+++ VectoCore/VectoCoreTest/Integration/Declaration/PrimaryBus/PrimaryBusSimulation.cs
@@ -374,78 +374,7 @@ public class PrimaryBusSimulation
 		SerializeRunData(runsFactoryFinal, outputPath);
     }
 
-
-	[TestCase(-1, TestName = "CodeEU60_Voith_P1Hybrid - ALL"),
-	TestCase(2, TestName = "CodeEU60_Voith_P1Hybrid - FCMap Interpolation failed"),
-	TestCase(6, TestName = "CodeEU60_Voith_P1Hybrid - Battery Object reference not set")
-	]
-	public void CodeEU60_Voith_P1Hybrid(int cycleIdx)
-	{
-		RunSimulationPrimary(@"E:\QUAM\Downloads\CodeEU-60\Test_P1_MH.xml", cycleIdx);
-	}
-
-	[TestCase()]
-	public void CodeEU60_P1HybridCompleted()
-	{
-		CreateCompletedVIF(@"E:\QUAM\Downloads\CodeEU-60\A3_2023_07_03_Iveco_DIWA8_MH.vecto");
-	}
-
-	[NonParallelizable]
-	[TestCase(MissionType.HeavyUrban, LoadingType.LowLoading),
-	TestCase(MissionType.HeavyUrban, LoadingType.ReferenceLoad),
-	TestCase(MissionType.Urban, LoadingType.LowLoading),
-	TestCase(MissionType.Urban, LoadingType.ReferenceLoad),
-	TestCase(MissionType.Urban, LoadingType.ReferenceLoad),
-	TestCase(MissionType.Interurban, LoadingType.ReferenceLoad),]
-	public void CodeEU60_Voith_P1Hybrid_Completed(MissionType mission, LoadingType loading)
-	{
-		TestMissionFilter();
-		Kernel.Rebind<IMissionFilter>().To<TestMissionFilter>().InSingletonScope();
-		var missionFilter = Kernel.Get<IMissionFilter>() as TestMissionFilter;
-        missionFilter?.SetMissions((mission, loading));
-
-        string vif = @"E:\QUAM\Downloads\CodeEU-60\Test_P1_MH.RSLT_VIF.xml";
-		string completed = @"E:\QUAM\Downloads\CodeEU-60\Test_HEV_Interim_Input.xml";
-
-		var completedJob = GenerateJsonJobCompletedBus(vif, completed);
-
-		var finalVif = CreateCompletedVIF(completedJob);
-
-
-        //      var completedJob = GenerateJsonJobCompletedBus(Path.Combine(BASE_DIR, vifFile), Path.Combine(BASE_DIR, completed));
-
-        //var missionFilter = TestMissionFilter();
-        //missionFilter?.SetMissions((MissionType.Coach, LoadingType.ReferenceLoad));
-
-        //var finalVif = CreateCompletedVIF(completedJob);
-    }
-
-	[TestCase(@"E:\QUAM\Downloads\CodeEU-60\Test_P1_MH.RSLT_VIF.VIF_Report_2.xml", MissionType.Interurban, RefL)]
-	public void CodeEU60_Voith_P1Hybrid_VIF(string filePath, MissionType mission, LoadingType loading)
-	{
-		TestMissionFilter();
-		Kernel.Rebind<IMissionFilter>().To<TestMissionFilter>().InSingletonScope();
-		var missionFilter = Kernel.Get<IMissionFilter>() as TestMissionFilter;
-		missionFilter?.SetMissions((mission, loading));
-
-		var dataProvider = _xmlReader.CreateDeclaration(filePath);
-		var fileWriter = new FileOutputWriter(filePath);
-		var simFactory = Kernel.Get<ISimulatorFactoryFactory>();
-		var runsFactory = simFactory.Factory(ExecutionMode.Declaration, new XMLDeclarationVIFInputData(dataProvider as IMultistepBusInputDataProvider, null), fileWriter, null, null);
-		runsFactory.SerializeVectoRunData = true;
-		runsFactory.WriteModalResults = true;
-		//runsFactory.SerializeVectoRunData = true;
-		var jobContainer = new JobContainer(new SummaryDataContainer(fileWriter)) { };
-
-		jobContainer.AddRuns(runsFactory);
-
-		jobContainer.Execute(multithreaded: false);
-		jobContainer.WaitFinished();
-		Assert.IsTrue(jobContainer.AllCompleted);
-		Assert.IsTrue(jobContainer.Runs.TrueForAll(runEntry => runEntry.Success));
-    }
-
-    private static void SerializeRunData(ISimulatorFactory runsFactorySingle, string outputPath)
+	private static void SerializeRunData(ISimulatorFactory runsFactorySingle, string outputPath)
 	{
 		var jsonSerializerSettings = new JsonSerializerSettings();
 		jsonSerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
