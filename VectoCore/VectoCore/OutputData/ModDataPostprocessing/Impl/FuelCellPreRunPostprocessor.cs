f8afa5ca33303b917e894ffc7bbdfee459af1bf4
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2023-07-31 17:29:25 +0200

    starting implementation of mod-data window iterator

--- VectoCore/VectoCore/OutputData/ModDataPostprocessing/Impl/FuelCellPreRunPostprocessor.cs
+++ VectoCore/VectoCore/OutputData/ModDataPostprocessing/Impl/FuelCellPreRunPostprocessor.cs
@@ -1,8 +1,105 @@
-﻿namespace TUGraz.VectoCore.OutputData.ModDataPostprocessing.Impl
+﻿using System;
+using System.Data;
+using TUGraz.VectoCommon.Exceptions;
+using TUGraz.VectoCommon.Utils;
+using TUGraz.VectoCore.Models.Simulation.Data;
+
+namespace TUGraz.VectoCore.OutputData.ModDataPostprocessing.Impl
 {
 	public class FuelCellPreRunPostprocessor
 	{
+
+		protected ModDataWindowIterator WindowIterator;
+
+
+		public FuelCellPreRunPostprocessor(IModalDataContainer modData)
+		{
+			WindowIterator = new ModDataWindowIterator((modData as ModalDataContainer).Data, 300.SI<Meter>());
+		}
+
+
+	}
+
+	public class ModDataWindowIterator
+	{
+		public ModalResults ModData { get; set; }
+
+		protected DataColumn DistanceColumn { get; }
+
+		//protected ModDataAsRingBuffer RingBuffer { get; }
+
+		protected Meter WindowDistance { get; }
+
+		protected int NumRows;
+		protected int PreStart;
+		protected int Start;
+		protected int End;
+		protected int PostEnd;
+
+        public ModDataWindowIterator(ModalResults results, Meter windowDistance)
+		{
+			ModData = results;
+			NumRows = ModData.Rows.Count;
+			
+			WindowDistance = windowDistance;
+
+			DistanceColumn = results.Columns[ModalResultField.dist.GetCaption()];
+
+			Start = 0;
+			var endDistance = (Meter)ModData.Rows[Start][DistanceColumn] + windowDistance;
+			End = MoveITeratorForward(Start, r => (Meter)(ModData.Rows[r][DistanceColumn]) <= endDistance);
+		} 
+
+		protected int MoveITeratorForward(int startIdx, Func<int, bool> action)
+		{
+			var retVal = startIdx;
+			var initialValue = action(startIdx);
+			do {
+				retVal = GetNextRow(retVal);
+				if (action(retVal) != initialValue) {
+					return retVal;
+				}
+
+				if (retVal == startIdx) {
+					throw new VectoException("Failed to move iterator forward - iterated all elements");
+                }
+			} while (true);
+
+
+			//while (!action(retVal)) {
+			//	retVal = GetNextRow(retVal);
+			//	if (retVal == startIdx) {
+			//		throw new VectoException("Failed to move iterator forward - iterated all elements");
+			//	}
+			//}
+			//return retVal;
+		}
+
+		protected int GetNextRow(int rowIdx)
+		{
+			return (rowIdx + 1) % NumRows;
+
+		}
+
+		protected int GetPreviousRow(int rowIdx)
+		{
+			return  (rowIdx - 1) % NumRows;
+		}
+
+    }
+
+	public class ModDataAsRingBuffer
+	{
+		public ModalResults ModData { get; }
+
+		protected int NumRows;
 		
-		//public 
+		public ModDataAsRingBuffer(ModalResults modData)
+		{
+			ModData = modData;
+			NumRows = modData.Rows.Count;
+		}
+
+		
 	}
 }
--- VectoCore/VectoCoreTest/Models/Simulation/FuelCellPreRunPostprocessingTest.cs
+++ VectoCore/VectoCoreTest/Models/Simulation/FuelCellPreRunPostprocessingTest.cs
@@ -21,13 +21,14 @@ using TUGraz.VectoCore.Models.SimulationComponent;
 using TUGraz.VectoCore.Models.SimulationComponent.Data;
 using TUGraz.VectoCore.OutputData;
 using TUGraz.VectoCore.OutputData.FileIO;
+using TUGraz.VectoCore.OutputData.ModDataPostprocessing.Impl;
 using TUGraz.VectoCore.Utils;
 
 namespace TUGraz.VectoCore.Tests.Models.Simulation;
 
 
 [TestFixture]
-public class FuelCellPreRunPostprocessing
+public class FuelCellPreRunPostprocessingT
 {
 
 	[TestCase()]
@@ -42,7 +43,14 @@ public class FuelCellPreRunPostprocessing
 		string jobFile = "TestData/H2_FCV/GenericVehicleE2 - FCHV/FCHV_singleFc.vecto";
 		int cycleIdx = 0;
 
-        var inputProvider = JSONInputDataFactory.ReadJsonJob(jobFile);
+        var modData = RunFCHV_PEV_Simulation(jobFile, cycleIdx);
+
+		var fcPostProcessor = new FuelCellPreRunPostprocessor(modData);
+	}
+
+	private static IModalDataContainer RunFCHV_PEV_Simulation(string jobFile, int cycleIdx)
+	{
+		var inputProvider = JSONInputDataFactory.ReadJsonJob(jobFile);
 
 		var writer = new FileOutputWriter(jobFile);
 		var factory = SimulatorFactory.CreateSimulatorFactory(ExecutionMode.Engineering, inputProvider, writer);
@@ -65,157 +73,9 @@ public class FuelCellPreRunPostprocessing
 
 		run.Run();
 		Assert.IsTrue(run.FinishedWithoutErrors);
-    }
 
-
-	protected VectoRunData GetVectoRunData()
-	{
-		var mockCycle = new Mock<IDrivingCycleData>();
-		mockCycle.Setup(x => x.CycleType).Returns(CycleType.DistanceBased);
-
-		return new VectoRunData() {
-			Cycle = mockCycle.Object,
-		};
-    }
-
-
-	public class ModalResultsTest : ModalResults
-	{
-		private const string Delimiter = ",";
-		private const string Comment = "#";
-		private static readonly Regex HeaderFilter = new Regex(@"\[.*?\]|\<|\>", RegexOptions.Compiled);
-
-        public ModalResultsTest(string modDataFile)
-		{
-			var xml = Path.ChangeExtension(modDataFile, ".vmod.xml");
-			if (File.Exists(xml)) {
-				ReadXml(XmlTextReader.Create(xml));
-				return;
-			}
-
-
-            TableName = $"ModData - {modDataFile}";
-			using (var fs = File.OpenText(modDataFile)) {
-				var p = new TextFieldParser(fs) {
-					TextFieldType = FieldType.Delimited,
-					Delimiters = new[] { Delimiter },
-					CommentTokens = new[] { Comment },
-					HasFieldsEnclosedInQuotes = true,
-					TrimWhiteSpace = true
-				};
-
-				var hdrFields = p.ReadFields();
-				if (hdrFields == null) {
-					throw new CSVReadException("CSV Read Error: File was empty.");
-				}
-
-				var colsWithoutComment = hdrFields
-					.Select(l => l.Contains(Comment) ? l.Substring(0, l.IndexOf(Comment, StringComparison.Ordinal)) : l)
-					.ToArray();
-
-				var columns = colsWithoutComment
-					.Select(l =>  HeaderFilter.Replace(l, ""))
-					.Select(l => l.Trim())
-					.Where(col => !double.TryParse(col, NumberStyles.Any, CultureInfo.InvariantCulture, out _))
-					.Distinct()
-					.ToList();
-
-				var firstLineIsData = columns.Count == 0;
-
-				if (firstLineIsData) {
-					LogManager.GetLogger(typeof(VectoCSVFile).FullName)
-						.Warn("No valid Data Header found. Interpreting the first line as data line.");
-					// set the validColumns to: {"0", "1", "2", "3", ...} for all columns in first line.
-					columns = colsWithoutComment.Select((_, i) => i.ToString()).ToList();
-				}
-
-				var mrfCols = new List<object>();
-				var colNames = EnumHelper.GetValues<ModalResultField>().Select(x => Tuple.Create(x, HeaderFilter.Replace(x.GetCaption(), "").Trim())).ToArray();
-				//var missing = new List<string>();
-                foreach (var column in columns) {
-					var mrf = colNames.FirstOrDefault(x => x.Item2.Equals(column, StringComparison.CurrentCultureIgnoreCase));
-					if (mrf != null) {
-						mrfCols.Add(mrf.Item1);
-					} else {
-						mrfCols.Add(column);
-						//	missing.Add(column);
-					}
-				}
-
-				foreach (object col in mrfCols) {
-					if (col is ModalResultField field) {
-						CreateColumns(new [] {field});
-					} else {
-						Columns.Add(col.ToString());
-					}
-				}
-				//missing.ForEach(col => Columns.Add(col));
-
-				var lineNumber = 1;
-				while (!p.EndOfData) {
-					object[] cells = { };
-					if (firstLineIsData) {
-						cells = colsWithoutComment;
-					} else {
-						var fields = p.ReadFields();
-						if (fields != null) {
-							cells = fields.Select(l =>
-									l.Contains(Comment)
-										? l.Substring(0, l.IndexOf(Comment, StringComparison.Ordinal))
-										: l)
-								.Select(s => s.Trim())
-								.ToArray();
-
-							cells = cells.Select((t, i) => ConvertToValue(t, mrfCols[i])).ToArray();
-						}
-					}
-
-					firstLineIsData = false;
-					if (false && Columns.Count != cells.Length) {
-						throw new CSVReadException(
-							$"Line {lineNumber}: The number of values is not correct. " +
-							$"Expected {Columns.Count} Columns, Got {cells.Length} Columns");
-					}
-
-					try {
-						// ReSharper disable once CoVariantArrayConversion
-						Rows.Add(cells);
-					} catch (InvalidCastException e) {
-						throw new CSVReadException(
-							$"Line {lineNumber}: The data format of a value is not correct. {e.Message}", e);
-					}
-
-					lineNumber++;
-				}
-			}
-		}
-
-		private object ConvertToValue(object cell, object mrfCol)
-		{
-			var unitRegex = new Regex(@"[^\]]*\[([^]]+)\]", RegexOptions.Compiled);
-			if (mrfCol is ModalResultField mrf) {
-				var unit = unitRegex.Replace(mrf.GetCaption(), "$1");
-				switch (unit) {
-					case "s": return cell.ToString().ToDouble().SI<Second>();
-					case "km/h": return cell.ToString().ToDouble().KMPHtoMeterPerSecond();
-					case "m": return cell.ToString().ToDouble().SI<Meter>();
-					case "m/s^2": return cell.ToString().ToDouble().SI<MeterPerSquareSecond>();
-					case "%": return (cell.ToString().ToDouble() / 100).SI<Scalar>();
-					case "1/min": return cell.ToString().ToDouble().RPMtoRad();
-					case "Nm": return cell.ToString().ToDouble().SI<NewtonMeter>();
-					case "kW": return (cell.ToString().ToDouble() * 1000).SI<Watt>();
-					case "g/h": return cell.ToString().ToDouble().SI(Unit.SI.Gramm.Per.Hour).Cast<KilogramPerSecond>();
-					case "V": return cell.ToString().ToDouble().SI<Volt>();
-					case "A": return cell.ToString().ToDouble().SI<Ampere>();
-					default:
-						return cell;
-						//return cell.ToString().ToDouble().SI<Scalar>();
-				}
-			}
-
-			//if (mrfCol is string colName) {
-				return cell;
-			//}
-		}
+		return run.GetContainer().ModalData;
 	}
+
+
 }
