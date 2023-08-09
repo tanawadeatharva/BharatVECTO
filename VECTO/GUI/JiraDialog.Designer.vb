79dd3fb74bd37c77ea4475fd2a71945941e9b353
Stefanos Doumpoulakis <dubulak@gmail.com>
2023-08-09 17:24:14 +0300

    chore: replace Jira links with codeu

--- VECTO/GUI/JiraDialog.Designer.vb
+++ VECTO/GUI/JiraDialog.Designer.vb
@@ -26,113 +26,134 @@ Partial Class JiraDialog
 	'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
 	<DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
-		Me.OK_Button = New System.Windows.Forms.Button()
-		Me.Label1 = New System.Windows.Forms.Label()
-		Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
-		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-		Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
-		Me.Label2 = New System.Windows.Forms.Label()
-		Me.Button1 = New System.Windows.Forms.Button()
-		Me.GroupBox1.SuspendLayout()
-		Me.SuspendLayout()
-		'
-		'OK_Button
-		'
-		Me.OK_Button.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-		Me.OK_Button.Location = New System.Drawing.Point(356, 205)
-		Me.OK_Button.Name = "OK_Button"
-		Me.OK_Button.Size = New System.Drawing.Size(67, 23)
-		Me.OK_Button.TabIndex = 0
-		Me.OK_Button.Text = "Close"
-		'
-		'Label1
-		'
-		Me.Label1.AutoSize = True
-		Me.Label1.Location = New System.Drawing.Point(6, 16)
-		Me.Label1.Name = "Label1"
-		Me.Label1.Size = New System.Drawing.Size(269, 13)
-		Me.Label1.TabIndex = 1
-		Me.Label1.Text = "You need a CITnet user account to create a new issue."
-		'
-		'LinkLabel1
-		'
-		Me.LinkLabel1.AutoSize = True
-		Me.LinkLabel1.Location = New System.Drawing.Point(6, 65)
-		Me.LinkLabel1.Name = "LinkLabel1"
-		Me.LinkLabel1.Size = New System.Drawing.Size(141, 13)
-		Me.LinkLabel1.TabIndex = 2
-		Me.LinkLabel1.TabStop = True
-		Me.LinkLabel1.Text = "JIRA Quick Start Guide (pdf)"
-		'
-		'GroupBox1
-		'
-		Me.GroupBox1.Controls.Add(Me.LinkLabel3)
-		Me.GroupBox1.Controls.Add(Me.Label1)
-		Me.GroupBox1.Controls.Add(Me.Label2)
-		Me.GroupBox1.Controls.Add(Me.LinkLabel1)
-		Me.GroupBox1.Location = New System.Drawing.Point(12, 86)
-		Me.GroupBox1.Name = "GroupBox1"
-		Me.GroupBox1.Size = New System.Drawing.Size(411, 100)
-		Me.GroupBox1.TabIndex = 3
-		Me.GroupBox1.TabStop = False
-		Me.GroupBox1.Text = "Help"
-		'
-		'LinkLabel3
-		'
-		Me.LinkLabel3.AutoSize = True
-		Me.LinkLabel3.Location = New System.Drawing.Point(246, 33)
-		Me.LinkLabel3.Name = "LinkLabel3"
-		Me.LinkLabel3.Size = New System.Drawing.Size(122, 13)
-		Me.LinkLabel3.TabIndex = 18
-		Me.LinkLabel3.TabStop = True
-		Me.LinkLabel3.Text = "JRC-VECTO@ec.europa.eu"
-		'
-		'Label2
-		'
-		Me.Label2.AutoSize = True
-		Me.Label2.Location = New System.Drawing.Point(6, 33)
-		Me.Label2.Name = "Label2"
-		Me.Label2.Size = New System.Drawing.Size(241, 13)
-		Me.Label2.TabIndex = 3
-		Me.Label2.Text = "If you don't have one yet please contact support: "
-		'
-		'Button1
-		'
-		Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.Button1.Location = New System.Drawing.Point(12, 12)
-		Me.Button1.Name = "Button1"
-		Me.Button1.Size = New System.Drawing.Size(411, 54)
-		Me.Button1.TabIndex = 4
-		Me.Button1.Text = "Create JIRA Issue"
-		Me.Button1.UseVisualStyleBackColor = True
-		'
-		'JiraDialog
-		'
-		Me.AcceptButton = Me.OK_Button
-		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(435, 240)
-		Me.Controls.Add(Me.Button1)
-		Me.Controls.Add(Me.GroupBox1)
-		Me.Controls.Add(Me.OK_Button)
-		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
-		Me.MaximizeBox = False
-		Me.MinimizeBox = False
-		Me.Name = "JiraDialog"
-		Me.ShowInTaskbar = False
-		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-		Me.Text = "Report Issue via CITnet"
-		Me.GroupBox1.ResumeLayout(False)
-		Me.GroupBox1.PerformLayout()
-		Me.ResumeLayout(False)
+        Me.OK_Button = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'OK_Button
+        '
+        Me.OK_Button.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.OK_Button.Location = New System.Drawing.Point(475, 252)
+        Me.OK_Button.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.OK_Button.Name = "OK_Button"
+        Me.OK_Button.Size = New System.Drawing.Size(89, 28)
+        Me.OK_Button.TabIndex = 0
+        Me.OK_Button.Text = "Close"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 20)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(384, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "You need a code.europa.eu user account to create a new issue."
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(317, 79)
+        Me.LinkLabel1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(176, 16)
+        Me.LinkLabel1.TabIndex = 2
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "JRC-VECTO@ec.europa.eu"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.LinkLabel3)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.LinkLabel1)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 106)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(548, 123)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Help"
+        '
+        'LinkLabel3
+        '
+        Me.LinkLabel3.AutoSize = True
+        Me.LinkLabel3.Location = New System.Drawing.Point(256, 41)
+        Me.LinkLabel3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LinkLabel3.Name = "LinkLabel3"
+        Me.LinkLabel3.Size = New System.Drawing.Size(82, 16)
+        Me.LinkLabel3.TabIndex = 18
+        Me.LinkLabel3.TabStop = True
+        Me.LinkLabel3.Text = "VECTO Wiki"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 41)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(227, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "If you want to create an account go to "
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(16, 15)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(548, 66)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Create Issue"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(8, 79)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(288, 16)
+        Me.Label3.TabIndex = 19
+        Me.Label3.Text = "To contact us directly use the functional mailbox"
+        '
+        'JiraDialog
+        '
+        Me.AcceptButton = Me.OK_Button
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(580, 295)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.OK_Button)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "JiraDialog"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Report Issue via code.europa.eu"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
 
-	End Sub
-	Friend WithEvents OK_Button As Button
+    End Sub
+    Friend WithEvents OK_Button As Button
 	Friend WithEvents Label1 As Label
 	Friend WithEvents LinkLabel1 As LinkLabel
 	Friend WithEvents GroupBox1 As GroupBox
 	Friend WithEvents Label2 As Label
 	Friend WithEvents LinkLabel3 As LinkLabel
 	Friend WithEvents Button1 As Button
-
+    Friend WithEvents Label3 As Label
 End Class
