be35c70936e4e79da05e9f0fed9dc5c8924482cc
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2023-08-23 12:55:25 +0200

    bugfix: voltage level was overwritten in case of IEPC

--- VectoCore/VectoCore/InputData/Reader/DataObjectAdapter/SimulationComponents/ElectricMachinesDataAdapter.cs
+++ VectoCore/VectoCore/InputData/Reader/DataObjectAdapter/SimulationComponents/ElectricMachinesDataAdapter.cs
@@ -297,8 +297,8 @@ namespace TUGraz.VectoCore.InputData.Reader.DataObjectAdapter.SimulationComponen
 				});
 			}
 
-			voltageLevels.First().Voltage = 1.SI<Volt>();
-			voltageLevels.Last().Voltage = 10E9.SI<Volt>();
+			//voltageLevels.First().Voltage = 1.SI<Volt>();
+			//voltageLevels.Last().Voltage = 10E9.SI<Volt>();
 
 
 			var dragCurves = new Dictionary<uint, DragCurve>();
