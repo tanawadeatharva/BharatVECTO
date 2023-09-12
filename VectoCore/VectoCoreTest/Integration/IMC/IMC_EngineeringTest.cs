5e30266dc6fd5b1d653cab4ab5678b608bf58b62
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2023-09-12 15:03:27 +0200

    adding testcase and testdata xEV IMC

---
+++ VectoCore/VectoCoreTest/Integration/IMC/IMC_EngineeringTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using TUGraz.VectoCommon.Models;
+using TUGraz.VectoCore.InputData.FileIO.JSON;
+using TUGraz.VectoCore.Models.Simulation.Impl;
+using TUGraz.VectoCore.Models.Simulation.Impl.SimulatorFactory;
+using TUGraz.VectoCore.OutputData;
+using TUGraz.VectoCore.OutputData.FileIO;
+
+namespace TUGraz.VectoCore.Tests.Integration.IMC;
+
+public class IMC_EngineeringTest
+{
+    [TestCase]
+    public void RunJob_PHEV_IMC_Engineering()
+    {
+        var jobFile = "TestData/IMC/P2_PHEV_Group5_2030LH_rl_ENG/P2_Group5LH_rl.vecto";
+        var inputProvider = JSONInputDataFactory.ReadJsonJob(jobFile);
+        var writer = new FileOutputWriter(jobFile);
+        var factory = SimulatorFactory.CreateSimulatorFactory(ExecutionMode.Engineering, inputProvider, writer);
+        factory.Validate = false;
+        factory.WriteModalResults = true;
+        factory.SumData = new SummaryDataContainer(writer);
+        var run = factory.SimulationRuns().ToArray()[0];
+        var modData = ((ModalDataContainer)((VehicleContainer)run.GetContainer()).ModData).Data;
+
+        run.Run();
+        Assert.IsTrue(run.FinishedWithoutErrors);
+    }
+}
