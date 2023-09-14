aad120a2c61cc423c387d47a1e951832864f45ba
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2023-09-14 07:28:18 +0200

    extending IMC engineering testcases

--- VectoCore/VectoCoreTest/Integration/IMC/IMC_EngineeringTest.cs
+++ VectoCore/VectoCoreTest/Integration/IMC/IMC_EngineeringTest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 using TUGraz.VectoCommon.Models;
 using TUGraz.VectoCore.InputData.FileIO.JSON;
 using TUGraz.VectoCore.Models.Simulation.Impl;
@@ -11,27 +12,33 @@ namespace TUGraz.VectoCore.Tests.Integration.IMC;
 
 public class IMC_EngineeringTest
 {
-	private const string PEV_IMC_Grp5 = "TestData/IMC/BEV_E2_Group5_2030LH_rl_Electr_Gear/BEV_Group5LH_rl_Electr_Gear.vecto";
+	private const string PEV_IMC_Grp5_HW = "TestData/IMC/BEV_E2_Group5_2030LH_rl_Electr_Gear/BEV_Group5LH_rl_Electr_Gear_IMC-HW.vecto";
+	private const string PEV_IMC_Grp5_all = "TestData/IMC/BEV_E2_Group5_2030LH_rl_Electr_Gear/BEV_Group5LH_rl_Electr_Gear_IMC-all.vecto";
 
-    [TestCase]
-    public void RunJob_PHEV_IMC_Engineering()
-    {
-        var jobFile = "TestData/IMC/P2_PHEV_Group5_2030LH_rl_ENG/P2_Group5LH_rl.vecto";
+	private const string PHEV_IMC_Grp5_HW = "TestData/IMC/P2_PHEV_Group5_2030LH_rl_ENG/P2_Group5LH_rl_IMC-HW.vecto";
+	private const string PHEV_IMC_Grp5_all = "TestData/IMC/P2_PHEV_Group5_2030LH_rl_ENG/P2_Group5LH_rl_IMC-all.vecto";
+
+    [TestCase(PHEV_IMC_Grp5_HW, 0, TestName = "P-HEV IMC HW Engineering Grp5 LH")]
+	[TestCase(PHEV_IMC_Grp5_HW, 1, TestName = "P-HEV IMC HW Engineering Grp5 RD")]
+	[TestCase(PHEV_IMC_Grp5_all, 1, TestName = "P-HEV IMC All Engineering Grp5 RD")]
+    public void RunJob_PHEV_IMC_Engineering(string jobFile, int runIdx)
+    { 
         var inputProvider = JSONInputDataFactory.ReadJsonJob(jobFile);
         var writer = new FileOutputWriter(jobFile);
         var factory = SimulatorFactory.CreateSimulatorFactory(ExecutionMode.Engineering, inputProvider, writer);
         factory.Validate = false;
         factory.WriteModalResults = true;
         factory.SumData = new SummaryDataContainer(writer);
-        var run = factory.SimulationRuns().ToArray()[0];
+        var run = factory.SimulationRuns().ToArray()[runIdx];
         var modData = ((ModalDataContainer)((VehicleContainer)run.GetContainer()).ModData).Data;
 
         run.Run();
         Assert.IsTrue(run.FinishedWithoutErrors);
     }
 
-	[TestCase (PEV_IMC_Grp5, 0, TestName = "PEV IMC Engineering Grp5 LH")]
-	[TestCase(PEV_IMC_Grp5, 1, TestName = "PEV IMC Engineering Grp5 RD")]
+	[TestCase (PEV_IMC_Grp5_HW, 0, TestName = "PEV IMC HW Engineering Grp5 LH")]
+	[TestCase(PEV_IMC_Grp5_HW, 1, TestName = "PEV IMC HW Engineering Grp5 RD")]
+	[TestCase(PEV_IMC_Grp5_all, 1, TestName = "PEV IMC All Engineering Grp5 RD")]
 	public void RunJob_PEV_IMC_Engineering(string jobFile, int runIdx)
 	{
 		var inputProvider = JSONInputDataFactory.ReadJsonJob(jobFile);
