a5e59bca301191f22a7f138f98a57f3f6cb997e0
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2023-10-03 14:49:13 +0200

    writing airdrag data to MRF, also in case of standard values. updating MRF XML Schema: Airdrag component no longer optional updating writing MRF

--- VectoCore/VectoCore/Models/Declaration/DeclarationData.cs
+++ VectoCore/VectoCore/Models/Declaration/DeclarationData.cs
@@ -206,8 +206,46 @@ namespace TUGraz.VectoCore.Models.Declaration
 		}
 
 
+		public static SegmentLookup GetTruckSegment(IVehicleDeclarationInputData vehicle, bool batteryElectric = false)
+			{
+                var allowVocational = true;
+			var ng = vehicle.ExemptedVehicle ? false : vehicle.Components.EngineInputData?.EngineModes.Any(e =>
+				e.Fuels.Any(f => f.FuelType.IsOneOf(FuelType.LPGPI, FuelType.NGCI, FuelType.NGPI))) ?? false;
+			var ovcHev = vehicle.ExemptedVehicle ? false : vehicle.OvcHev;
+			Segment segment;
+			try {
+				segment = DeclarationData.TruckSegments.Lookup(
+					vehicle.VehicleCategory, batteryElectric, vehicle.AxleConfiguration, vehicle.GrossVehicleMassRating,
+					vehicle.CurbMassChassis,
+					vehicle.VocationalVehicle, ng, ovcHev);
+			} catch (VectoException) {
+				allowVocational = false;
+				segment = DeclarationData.TruckSegments.Lookup(
+					vehicle.VehicleCategory, batteryElectric, vehicle.AxleConfiguration, vehicle.GrossVehicleMassRating,
+					vehicle.CurbMassChassis,
+					false, ng, ovcHev);
+			}
 
-		public static WeightingGroup GetVehicleGroupCO2StandardsGroup(IVehicleDeclarationInputData vehicleData)
+			if (!segment.Found) {
+				throw new VectoException(
+					"no segment found for vehicle configuration: vehicle category: {0}, axle configuration: {1}, GVMR: {2}",
+					vehicle.VehicleCategory, vehicle.AxleConfiguration,
+					vehicle.GrossVehicleMassRating);
+			}
+
+			return new SegmentLookup() { Segment = segment, AllowVocational = allowVocational };
+		}
+
+
+		public struct SegmentLookup
+		{
+			public Segment Segment { get; set; }
+
+			public bool AllowVocational { get; set; }
+		}
+
+
+        public static WeightingGroup GetVehicleGroupCO2StandardsGroup(IVehicleDeclarationInputData vehicleData)
 		{
 			switch (vehicleData.VehicleCategory) {
 				case VehicleCategory.Van:
