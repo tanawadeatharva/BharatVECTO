9e02584a0710caf2708062ef82d264e54a8343a8
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2023-12-04 15:27:08 +0100

    use the current element to lookup namespace prefix definition instead of root element of tyre component

--- VectoCore/VectoCore/OutputData/XML/DeclarationReports/VehicleInformationFile/VehicleInformationFile_0_1/PrimaryComponents/VIFAxleWheelsType.cs
+++ VectoCore/VectoCore/OutputData/XML/DeclarationReports/VehicleInformationFile/VehicleInformationFile_0_1/PrimaryComponents/VIFAxleWheelsType.cs
@@ -72,7 +72,7 @@ namespace TUGraz.VectoCore.OutputData.XML.DeclarationReports.VehicleInformationF
 					if (defaultNsAttr != null) {
 						defaultNsAttr.Value = xmlTyre.GetXmlNode.GetNamespaceOfPrefix(parts.First());
 					} else {
-						ptr.Parent.Add(new XAttribute("xmlns", xmlTyre.GetXmlNode.GetNamespaceOfPrefix(parts.First())));
+						ptr.Parent.Add(new XAttribute("xmlns", ptr.Parent.GetNamespaceOfPrefix(parts.First())));
 					}
 				}
 				ptr = ptr.NextAttribute;
