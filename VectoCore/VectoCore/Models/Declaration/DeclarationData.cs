18f565cd72e5d44c9b2bf9c3bd23dfe29f3b3777
Stefanos Doumpoulakis <dubulak@gmail.com>
2023-12-11 17:07:10 +0200

    fix #189: reconsider inertia for smaller engines
    Problem: High engine inertia, compared to vehicle total mass, causes
    simulation to fail during gear shifts for many medium lorries.
    
    Solution: Different way of calculating inertia based on engine
    displacement and gearbox type.

--- VectoCore/VectoCore/Models/Declaration/DeclarationData.cs
+++ VectoCore/VectoCore/Models/Declaration/DeclarationData.cs
@@ -829,7 +829,46 @@ namespace TUGraz.VectoCore.Models.Declaration
 			public const double TorqueLimitGearboxFactor = 0.9;
 			public const double TorqueLimitVehicleFactor = 0.95;
 
+			public static readonly SI SmallEnginesBound = (3.2 / 1000).SI<CubicMeter>();
+			public static readonly SI MidEnginesBound = (5.0 / 1000).SI<CubicMeter>();
+
+			public static readonly KilogramSquareMeter ManualBaseInertia = 1.885.SI<KilogramSquareMeter>();
+			public static readonly KilogramSquareMeter ATBaseInertia = 1.707.SI<KilogramSquareMeter>();
+
+			public static readonly SI SmallEngineDisplacementInertia = (0.4 * 1000).SI(Unit.SI.Kilo.Gramm.Per.Meter);
+			public static readonly SI MidEngineATDisplacementInertia = (0.933 * 1000).SI(Unit.SI.Kilo.Gramm.Per.Meter);
+			public static readonly SI MidEngineManualDisplacementInertia = (0.989 * 1000).SI(Unit.SI.Kilo.Gramm.Per.Meter);
+
 			public static KilogramSquareMeter EngineInertia(VectoSimulationJobType jobType, CubicMeter displacement, GearboxType? gbxType)
+			{
+				if (displacement <= SmallEnginesBound) {
+					return ComputeSmallEngineInertia(displacement);
+				}
+				else if ((displacement > SmallEnginesBound) && (displacement <= MidEnginesBound)) {
+					return ComputeMidEngineInertia(displacement, gbxType);
+				}
+				else {
+					return ComputeBigEngineInertia(jobType, displacement, gbxType);
+				}
+			}
+
+			private static KilogramSquareMeter ComputeSmallEngineInertia(CubicMeter displacement)
+			{ 
+				return (SmallEngineDisplacementInertia * displacement).Cast<KilogramSquareMeter>();
+			}
+
+			private static KilogramSquareMeter ComputeMidEngineInertia(CubicMeter displacement, GearboxType? gbxType)
+			{ 
+				if (!gbxType.HasValue) {
+					throw new VectoException("Gearbox type must be provided!");
+				}
+
+				return gbxType.Value.AutomaticTransmission()
+					? (MidEngineATDisplacementInertia * displacement) - ATBaseInertia
+					: (MidEngineManualDisplacementInertia * displacement) - ManualBaseInertia;
+			}
+
+			private static KilogramSquareMeter ComputeBigEngineInertia(VectoSimulationJobType jobType, CubicMeter displacement, GearboxType? gbxType)
 			{
 				// VB Code:    Return 1.3 + 0.41 + 0.27 * (Displ / 1000)
 				KilogramSquareMeter clutchPlateTc;
