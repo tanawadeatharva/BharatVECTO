a89925367416608754d2e5565125088e059edd51
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2023-12-11 09:50:08 +0100

    adding mock input data for battery electric vehicle (needed to calculate reference propulsion power)

--- VectoCore/VectoCoreTest/Integration/BatteryElectric/BatteryElectricTest.cs
+++ VectoCore/VectoCoreTest/Integration/BatteryElectric/BatteryElectricTest.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Moq;
 using NUnit.Framework;
 using TUGraz.VectoCommon.Exceptions;
 using TUGraz.VectoCommon.InputData;
@@ -1113,6 +1114,8 @@ namespace TUGraz.VectoCore.Tests.Integration.BatteryElectric
 				},
 			};
 			var wheels = DeclarationData.Wheels.Lookup("275/70 R22.5");
+			var inputdata = new Mock<IVehicleDeclarationInputData>();
+			inputdata.Setup(v => v.VehicleType).Returns(VectoSimulationJobType.BatteryElectricVehicle);
 			return new VehicleData {
 				AirDensity = DeclarationData.AirDensity,
 				AxleConfiguration = AxleConfiguration.AxleConfig_4x2,
@@ -1121,6 +1124,7 @@ namespace TUGraz.VectoCore.Tests.Integration.BatteryElectric
 				DynamicTyreRadius = wheels.DynamicTyreRadius,
 				AxleData = axles,
 				SavedInDeclarationMode = false,
+				InputData = inputdata.Object,
 			};
 		}
 
