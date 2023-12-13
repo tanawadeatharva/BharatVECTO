fd9084caab4356234c0cf4a9c81a840a374a4019
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2023-12-13 17:47:07 +0100

    use tabs for intendation

--- VectoCore/VectoCore/Models/Declaration/DeclarationData.cs
+++ VectoCore/VectoCore/Models/Declaration/DeclarationData.cs
@@ -112,7 +112,7 @@ namespace TUGraz.VectoCore.Models.Declaration
 		public const double HEV_EquivalenceFactor_Max = 4.0;
 		//public static readonly HEVStrategyParameters InitEquivalenceFactorsBus = new HEVStrategyParametersBus();
 
-        public static readonly VehicleOperationLookup VehicleOperation = new VehicleOperationLookup();
+		public static readonly VehicleOperationLookup VehicleOperation = new VehicleOperationLookup();
 
 		public static readonly double ElectricMachineDefaultMechanicalTransmissionEfficiency = 1;
 		//public static MeterPerSecond CycleSpeedLimit;
@@ -136,7 +136,7 @@ namespace TUGraz.VectoCore.Models.Declaration
 		public static readonly Volt SuperCapReferenceVoltage = 2.7.SI<Volt>();
 		public const double SuperCapInternalResistanceStdValuesCorrection = 0.25;
 
-        public const double ElectricMachineDefaultEfficiencyFallback = 0.98;
+		public const double ElectricMachineDefaultEfficiencyFallback = 0.98;
 
 		public static readonly Watt MinDepotChgPwr = 10.SI(Unit.SI.Kilo.Watt).Cast<Watt>();
 		public static readonly Second DepotChargingDuration = 6.SI(Unit.SI.Hour).Cast<Second>();
@@ -214,7 +214,7 @@ namespace TUGraz.VectoCore.Models.Declaration
 
 		public static SegmentLookup GetTruckSegment(IVehicleDeclarationInputData vehicle, bool batteryElectric = false)
 			{
-                var allowVocational = true;
+				var allowVocational = true;
 			var ng = vehicle.ExemptedVehicle ? false : vehicle.Components.EngineInputData?.EngineModes.Any(e =>
 				e.Fuels.Any(f => f.FuelType.IsOneOf(FuelType.LPGPI, FuelType.NGCI, FuelType.NGPI))) ?? false;
 			var ovcHev = vehicle.ExemptedVehicle ? false : vehicle.OvcHev;
@@ -251,7 +251,7 @@ namespace TUGraz.VectoCore.Models.Declaration
 		}
 
 
-        public static WeightingGroup GetVehicleGroupCO2StandardsGroup(IVehicleDeclarationInputData vehicleData)
+		public static WeightingGroup GetVehicleGroupCO2StandardsGroup(IVehicleDeclarationInputData vehicleData)
 		{
 			switch (vehicleData.VehicleCategory) {
 				case VehicleCategory.Van:
@@ -761,15 +761,15 @@ namespace TUGraz.VectoCore.Models.Declaration
 				return new EngineStopStartLorry();
 			}
 
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <param name="hdvClass"></param>
-            /// <param name="jobType">only used for buses</param>
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="hdvClass"></param>
+			/// <param name="jobType">only used for buses</param>
 			/// <param name="arch">only used for buses</param>
-            /// <param name="compressorDrive">only used for buses</param>
-            /// <returns></returns>
-            public static IEngineStopStart GetEngineStopStart(VehicleClass hdvClass, VectoSimulationJobType? jobType = null,  ArchitectureID? arch = null,
+			/// <param name="compressorDrive">only used for buses</param>
+			/// <returns></returns>
+			public static IEngineStopStart GetEngineStopStart(VehicleClass hdvClass, VectoSimulationJobType? jobType = null,  ArchitectureID? arch = null,
 				CompressorDrive? compressorDrive = null)
 			{
 
@@ -1071,7 +1071,7 @@ namespace TUGraz.VectoCore.Models.Declaration
 				return limitedFld;
 			}
 
-            private static List<ShiftPolygon.ShiftPolygonEntry> DownshiftLineDrive(
+			private static List<ShiftPolygon.ShiftPolygonEntry> DownshiftLineDrive(
 				ElectricMotorFullLoadCurve fullLoadCurve, ElectricMotorFullLoadCurve fullLoadCurveOrig,
 				PerSecond nMin, PerSecond nMax)
 			{
@@ -1535,7 +1535,7 @@ namespace TUGraz.VectoCore.Models.Declaration
 						TorqueRatio = 0.85,
 						Torque =  -4.1 * first.Torque
 					},
-                };
+				};
 				foreach (var torqueConverterEntry in characteristicTorque) {
 					torqueConverterEntry.SpeedRatio = torqueConverterEntry.SpeedRatio * ratio;
 					torqueConverterEntry.TorqueRatio = torqueConverterEntry.TorqueRatio / ratio;
@@ -1679,8 +1679,8 @@ namespace TUGraz.VectoCore.Models.Declaration
 		{
 			var vehicleOperation = VehicleOperation.LookupVehicleOperation(cdResult.VehicleClass, cdResult.Mission);
 			var chgEfficiency = CalculateChargingEfficiencyOVCHEV(cdResult.MaxChargingPower, vehicleOperation, cdResult.BatteryData);
-            CalculateChargingEfficiencyOVCHEV(cdResult.MaxChargingPower, vehicleOperation, cdResult.BatteryData);
-            return CalculateWeightedResult(cdResult, csResult, vehicleOperation, chgEfficiency);
+			CalculateChargingEfficiencyOVCHEV(cdResult.MaxChargingPower, vehicleOperation, cdResult.BatteryData);
+			return CalculateWeightedResult(cdResult, csResult, vehicleOperation, chgEfficiency);
 		}
 
 		public static IWeightedResult CalculateWeightedResultCompletedBus(IResultEntry cdResult, IResultEntry csResult)
@@ -1711,7 +1711,7 @@ namespace TUGraz.VectoCore.Models.Declaration
 			var D24_realWorldFactorChargeDuringMission = vehicleOperation.RealWorldUsageFactors.StationaryChargingDuringMission;
 
 			var (etaChgBatDepot, etaChgBatInMission, etaChtBatWeighted) = chargingEfficiency;
-            
+			
 			var D9_maxStatChargingPower = cdResult.MaxChargingPower;
 
 			var D11_energyConsumptionCdMode = cdResult.ElectricEnergyConsumption / cdResult.Distance;
@@ -1802,7 +1802,7 @@ namespace TUGraz.VectoCore.Models.Declaration
 			var connectorLossDepot = currentEstDepot * batteryData.ConnectionSystemResistance *
 								currentEstDepot;
 
-            var etaChgBatDepot = 1 - ((respChgBatDepot.LossPower + connectorLossDepot) / respChgBatDepot.PowerDemand).Value();
+			var etaChgBatDepot = 1 - ((respChgBatDepot.LossPower + connectorLossDepot) / respChgBatDepot.PowerDemand).Value();
 			var etaChgBatInMission = 1 - ((respChgBatInMission.LossPower + connectorLossInMission) / respChgBatInMission.PowerDemand).Value();
 
 
@@ -1893,7 +1893,7 @@ namespace TUGraz.VectoCore.Models.Declaration
 				PassengerCount = entries.All(e => e.ChargeDepletingResult.PassengerCount != null) ? entries.Sum(e => e.ChargeDepletingResult.PassengerCount.Value * e.ChargeDepletingResult.WeightingFactor) : (double?)null,
 				FuelConsumption = fuels.Select(f => Tuple.Create(f,
 						entries.Sum(e =>
-                            e.Weighted.FuelConsumption[f] * e.ChargeDepletingResult.WeightingFactor)))
+							e.Weighted.FuelConsumption[f] * e.ChargeDepletingResult.WeightingFactor)))
 					.ToDictionary(x => x.Item1, x => x.Item2),
 				ElectricEnergyConsumption = entries.Sum(e => e.Weighted.ElectricEnergyConsumption * e.ChargeDepletingResult.WeightingFactor),
 				CO2Total = entries.Sum(e => e.Weighted.CO2Total * e.ChargeDepletingResult.WeightingFactor),
