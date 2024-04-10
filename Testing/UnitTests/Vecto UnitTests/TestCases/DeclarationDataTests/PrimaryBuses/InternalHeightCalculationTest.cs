f11a23987f6f32be991e8d6d05e57ef755db5eda
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2024-04-10 18:36:44 +0200

    migrating more testcases to unit test project

---
+++ Testing/UnitTests/Vecto UnitTests/TestCases/DeclarationDataTests/PrimaryBuses/InternalHeightCalculationTest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using TUGraz.VectoCommon.Models;
+using TUGraz.VectoCommon.Utils;
+using TUGraz.VectoCore.Models.Declaration;
+using Assert = NUnit.Framework.Assert;
+
+namespace TUGraz.Vecto.UnitTests.TestCases.DeclarationDataTests.PrimaryBuses;
+
+public class InternalHeightCalculationTest {
+
+	[TestCase(VehicleCode.CE, RegistrationClass.I, 2.134, 2.134),
+	TestCase(VehicleCode.CE, RegistrationClass.II, 2.134, 2.134),
+	TestCase(VehicleCode.CE, RegistrationClass.I_II, 2.134, 2.134),
+	TestCase(VehicleCode.CE, RegistrationClass.A, 2.134, 2.134),
+
+	TestCase(VehicleCode.CF, RegistrationClass.I, 2.134, 1.8),
+	TestCase(VehicleCode.CF, RegistrationClass.II, 2.134, 1.8),
+	TestCase(VehicleCode.CF, RegistrationClass.I_II, 2.134, 1.8),
+	TestCase(VehicleCode.CF, RegistrationClass.A, 2.134, 1.8),
+
+	TestCase(VehicleCode.CI, RegistrationClass.I, 2.134, 2.134),
+	TestCase(VehicleCode.CI, RegistrationClass.II, 2.134, 2.134),
+	TestCase(VehicleCode.CI, RegistrationClass.I_II, 2.134, 2.134),
+	TestCase(VehicleCode.CI, RegistrationClass.A, 2.134, 2.134),
+	TestCase(VehicleCode.CI, RegistrationClass.II_III, 2.134, 2.134),
+	TestCase(VehicleCode.CI, RegistrationClass.III, 2.134, 2.134),
+	TestCase(VehicleCode.CI, RegistrationClass.B, 2.134, 2.134),
+
+	TestCase(VehicleCode.CJ, RegistrationClass.I, 2.134, 1.8),
+	TestCase(VehicleCode.CJ, RegistrationClass.II, 2.134, 1.8),
+	TestCase(VehicleCode.CJ, RegistrationClass.I_II, 2.134, 1.8),
+	TestCase(VehicleCode.CJ, RegistrationClass.A, 2.134, 1.8),
+	TestCase(VehicleCode.CJ, RegistrationClass.II_III, 2.134, 1.8),
+	TestCase(VehicleCode.CJ, RegistrationClass.III, 2.134, 1.8),
+	TestCase(VehicleCode.CJ, RegistrationClass.B, 2.134, 1.8),
+
+	TestCase(VehicleCode.CA, RegistrationClass.II, 2.134, 2.134 - 0.5),
+	TestCase(VehicleCode.CA, RegistrationClass.II_III, 2.134, 2.134 - 0.5),
+	TestCase(VehicleCode.CA, RegistrationClass.II_III, 3.134, 1.8),
+	TestCase(VehicleCode.CA, RegistrationClass.III, 2.134, 1.8),
+	TestCase(VehicleCode.CA, RegistrationClass.B, 2.134, 1.8),
+
+	TestCase(VehicleCode.CB, RegistrationClass.II, 2.134, 1.8),
+	TestCase(VehicleCode.CB, RegistrationClass.II_III, 2.134, 1.8),
+	TestCase(VehicleCode.CB, RegistrationClass.III, 2.134, 1.8),
+	TestCase(VehicleCode.CB, RegistrationClass.B, 2.134, 1.8),
+	]
+	public void TestInternalHeightCalculation(
+		VehicleCode vehicleCode, RegistrationClass regClass, double bodyHeight, double expectedInternalHeight)
+	{
+		var internalHeight =
+			DeclarationData.BusAuxiliaries.CalculateInternalHeight(vehicleCode, regClass, bodyHeight.SI<Meter>());
+
+		Assert.AreEqual(expectedInternalHeight, internalHeight.Value(), 1e-6);
+	}
+}
