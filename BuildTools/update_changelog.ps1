2fd45fac88c9d55e9cc16005a988b671d4927a34
David Amo <david.amo-gonzalez@ext.ec.europa.eu>
2024-10-03 13:25:03 +0100

    feat(notes): support Gitlab issue closing pattern

--- BuildTools/update_changelog.ps1
+++ BuildTools/update_changelog.ps1
@@ -67,7 +67,11 @@ function Update-BuildPropsVersion([string]$version) {
 
 function Remove-CliffArtifacts([string] $filename){
     # When git trailer/footers are empty git cliff produces undesired artifacts that need removal.
-    $(Get-Content $filename).Replace("description::", ":").Replace("CodeEU : ", "") | Set-Content $filename
+    $ArtifactFreeChangelog = $(Get-Content $filename).Replace("description::", ":").Replace("CodeEU : ", "")
+
+    # Gitlab issue clossing pattern: https://code.europa.eu/help/user/project/issues/managing_issues.md#default-closing-pattern
+    $GITLAB_ISSUE_CLOSING_REGEX = "\b((?:[Cc]los(?:e[sd]?|ing)|\b[Ff]ix(?:e[sd]|ing)?|\b[Rr]esolv(?:e[sd]?|ing)|\b[Ii]mplement(?:s|ed|ing)?)(:?)) "
+    $ArtifactFreeChangelog -replace $GITLAB_ISSUE_CLOSING_REGEX | Set-Content $filename
 }
 
 if(!$RELEASE_VERSION){
