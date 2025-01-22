9b36cfc1c8a634a8e6307e8d232bb04ebfb3c996
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2025-01-22 15:08:52 +0100

    move cycleshiftstrategy to outerscope and a separate file

--- VectoCore/VectoCore/Models/SimulationComponent/Impl/CycleGearbox.cs
+++ VectoCore/VectoCore/Models/SimulationComponent/Impl/CycleGearbox.cs
@@ -581,37 +581,6 @@ namespace TUGraz.VectoCore.Models.SimulationComponent.Impl
 			public WattSecond PowershiftLosses { get; set; }
 		}
 
-		public class CycleShiftStrategy : BaseShiftStrategy
-		{
-			public CycleShiftStrategy(IVehicleContainer container) : base(container) { }
-
-			public override IGearbox Gearbox { get; set; }
-
-			protected override bool DoCheckShiftRequired(Second absTime, Second dt, NewtonMeter outTorque,
-				PerSecond outAngularVelocity, NewtonMeter inTorque, PerSecond inAngularVelocity, GearshiftPosition gear,
-				Second lastShiftTime, IResponse response)
-			{
-				return false;
-			}
-
-			public override GearshiftPosition InitGear(Second absTime, Second dt, NewtonMeter torque, PerSecond outAngularVelocity)
-			{
-				throw new NotImplementedException();
-			}
-
-			public override GearshiftPosition Engage(Second absTime, Second dt, NewtonMeter outTorque, PerSecond outAngularVelocity)
-			{
-				throw new NotImplementedException();
-			}
-
-			public override void Disengage(Second absTime, Second dt, NewtonMeter outTorque, PerSecond outAngularVelocity)
-			{
-				throw new NotImplementedException();
-			}
-
-			public override GearshiftPosition NextGear => throw new NotImplementedException();
-
-			
-		}
+		
 	}
 }
---
+++ VectoCore/VectoCore/Models/SimulationComponent/Impl/Shiftstrategies/CycleShiftStrategy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TUGraz.VectoCommon.Models;
+using TUGraz.VectoCommon.Utils;
+using TUGraz.VectoCore.Models.Simulation;
+
+namespace TUGraz.VectoCore.Models.SimulationComponent.Impl.Shiftstrategies
+{
+    public class CycleShiftStrategy : BaseShiftStrategy<CycleGearbox>
+    {
+        public CycleShiftStrategy(IVehicleContainer container) : base(container) { }
+
+        //public override IGearbox Gearbox { get; set; }
+
+        protected override bool DoCheckShiftRequired(Second absTime, Second dt, NewtonMeter outTorque,
+            PerSecond outAngularVelocity, NewtonMeter inTorque, PerSecond inAngularVelocity, GearshiftPosition gear,
+            Second lastShiftTime, IResponse response)
+        {
+            return false;
+        }
+
+        public override GearshiftPosition InitGear(Second absTime, Second dt, NewtonMeter torque, PerSecond outAngularVelocity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override GearshiftPosition Engage(Second absTime, Second dt, NewtonMeter outTorque, PerSecond outAngularVelocity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Disengage(Second absTime, Second dt, NewtonMeter outTorque, PerSecond outAngularVelocity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override GearshiftPosition NextGear => throw new NotImplementedException();
+
+
+    }
+}
