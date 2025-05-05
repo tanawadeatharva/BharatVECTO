f560b182faa5ad520d78501b262c12a96f0b6484
Markus Quaritsch <quaritsch@ivt.tugraz.at>
2025-05-05 09:14:19 +0200

    fix running PEV/IEPC JSON jobs

--- VectoCore/VectoCore/InputData/FileIO/JSON/JSONSubComponent.cs
+++ VectoCore/VectoCore/InputData/FileIO/JSON/JSONSubComponent.cs
@@ -966,7 +966,7 @@ namespace TUGraz.VectoCore.InputData.FileIO.JSON
 
 		public IMCTechnology Technology => Body["InMotionCharging"]?["Technology"] != null
 			? Body["InMotionCharging"].GetEx<string>("Technology").ParseEnum<IMCTechnology>()
-			: IMCTechnology.NotApplicable;
+			: IMCTechnology.None;
 
 		#endregion
 	}
