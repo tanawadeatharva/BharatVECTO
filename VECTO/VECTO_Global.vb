f46699686b18400dc9d21fa0364f7a71e4779760
David Amo <david.amo-gonzalez@ext.ec.europa.eu>
2025-10-06 08:06:13 +0100

    fix: VECTO main version in dialogs

--- VECTO/VECTO_Global.vb
+++ VECTO/VECTO_Global.vb
@@ -13,8 +13,8 @@ Imports System.IO
 Imports System.Text
 
 Public Module VECTO_Global
-	Public Const VECTOvers As String = "4"
-	Public COREvers As String = "NOT FOUND"
+    Public Const VECTOvers As String = "5"
+    Public COREvers As String = "NOT FOUND"
 
 	Public Const LicSigAppCode As String = "VECTO-Release-0093C61E0A2E4BFA9A7ED7E729C56AE4"
 	public InstallModeInstalled As Boolean = False
